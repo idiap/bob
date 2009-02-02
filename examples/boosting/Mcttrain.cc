63b98ff0475ba6e3b4660531e407f87b3fc513a0
cosmin <cosmin@cosmin-laptop.(none)>
2009-02-02 10:10:19 +0100

    Moved FileBinDataSet from src/trainers to src/core

--- examples/boosting/Mcttrain.cc
+++ examples/boosting/Mcttrain.cc
@@ -5,38 +5,19 @@
 #include "MctLbpTrainer.h"
 #include "LBPMachine.h"
 #include "ipLBP.h"
+
 using namespace Torch;
+
 int main()
 {
+	FileBinDataSet fb;
+	fb.setData("bindata.list", Tensor::Short,Tensor::Short, 19, 19);
 
+	MctLbpTrainer mct_trainer;
+	mct_trainer.setData(&fb);
+	mct_trainer.setnRounds(10,3);
+	mct_trainer.setLbpParameters(LBPMachine::LBP8RAverageAddBit, 1);
+	mct_trainer.train();
 
-
-    FileBinDataSet *FB = new FileBinDataSet();
-   FB->setData("bindata.list",Tensor::Short,Tensor::Short,19,19);
-
-    Tensor *T1 = new ShortTensor();
-    T1= FB->getExample(3);
-    print("dimension of T1 is %d\n",T1->nDimension());
-    Tprint((ShortTensor*)T1);
-    T1=FB->getTarget(3);
-    Tprint((ShortTensor*)T1);
-    print("%d\n",(*FB->short_example)(5,5,5));
-
-    LBPMachine *lbp = new LBPMachine();
-//    lbp->LBPType lbpt;
-//    lbpt = LBP8RAverageAddBit;
-    MctLbpTrainer *M1 = new MctLbpTrainer();
-    M1->setData(FB);
-    //M1->setnRounds(100,10);
-    M1->setnRounds(10,3);
-    M1->setLbpParameters(LBPMachine::LBP8RAverageAddBit,1);
-    M1->train();
-
-    delete FB;
-    delete M1;
-    delete lbp;
-
-    return 0;
-
-
+	return 0;
 }
