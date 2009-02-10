419a48470c00518dafb12a7f2f27302bcd0d452a
Sebastien Marcel <marcel@idiap.ch>
2009-02-10 17:27:11 +0100

    modified:   src/core/Trainer.cc 	modified:   src/core/Trainer.h

--- src/core/Trainer.h
+++ src/core/Trainer.h
@@ -13,21 +13,26 @@ namespace Torch {
 	public:
 
 		/// Constructor
-		Trainer(Machine& machine, DataSet& dataset);
+		Trainer();
 
 		/// Destructor
 		virtual ~Trainer();
 
-		/// Train the machine on the given dataset
-		virtual bool 		train() = 0;
+		/// Train the given machine on the given dataset
+		virtual bool 	train() = 0;
+
+		/// Set the DataSet to train with
+		bool 		setData(DataSet *m_dataset_);
+
+		/// Set the Machine to train
+		bool 		setMachine(Machine *m_machine_);
 
 	protected:
-
 		////////////////////////////////////////////////////
 		/// Attributes
 
-		Machine&		m_machine;	// The machine that will be trained
-		DataSet&		m_dataset;	// The dataset used to train the machine
+		Machine		*m_machine;	// The machine that will be trained
+		DataSet		*m_dataset;	// The dataset used to train the machine
 	};
 
 }
