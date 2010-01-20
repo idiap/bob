2f7a18cd21f17f7ff54c61460d442d1bef9deb5e
Venkatesh Bala <bvenka@cicatrix01.idiap.ch>
2010-01-20 15:21:15 +0100

    Updated BoostingRoundLBPTrainer and FTrainer for CascadeTraining with Scanning images to fetch  -ve patterns

--- src/boosting/FTrainer.cc
+++ src/boosting/FTrainer.cc
@@ -12,6 +12,10 @@ FTrainer::FTrainer() : Trainer()
 FTrainer::~FTrainer()
 {
 }
-
+float FTrainer::forwardScan(const Tensor &example_,TensorRegion &tregion)
+{
+	print("Ftrainer::Should not be here\n");
+	return 1.0;
+}
 }
 
