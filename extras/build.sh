0d2abe4013c87dfcc81f8bce80b84ca9548a149a
André Anjos <andre.dos.anjos@gmail.com>
2010-06-29 11:12:35 +0200

    Simplified build system with scripts that can be called to build and install specific architectures

--- extras/build.sh
+++ extras/build.sh
@@ -1,11 +1,44 @@
 #!/bin/bash 
 # Andre Anjos <andre.dos.anjos@gmail.com>
-# Mon 28 Jun 2010 17:16:56 CEST
+# Tue 29 Jun 2010 10:54:41 CEST
 
-# Builds all projects using cmake
-dir=build
-[ ! -d ${dir} ] && mkdir ${dir}
-cd ${dir}
-cmake ..
-make all
-make install
+# This is a generic script to build a certain release configuration. Do NOT
+# call this script directly. Create symlinks to it using the configuration type
+# as script name and call the symlink instead.
+
+bname=`basename $0 .sh`
+if [ "${bname}" = "build" ]; then
+  echo "usage: `dirname $0`/<symlink.sh>"
+  echo "  Do NOT call this program directly, use the symlinks!"
+  echo "  If none exists, create one pointing to this script. Example:"
+  echo "  $ ln -s ./build.sh ./release.sh"
+  echo "  Please note that the names of the symlinks should be valid CMAKE"
+  echo "  build types in lowercase (e.g. debug.sh or release.sh)."
+  exit 1
+fi
+
+# Here we compute all the relevant directories needed for our build and the
+# variables that need to be taken under consideration while building and
+# installing the projects.
+build_type=`basename $0 .sh | tr 'a-z' 'A-Z'` #uppercase transformation
+platform=`uname -s`-`uname -m`-${build_type}
+platform=`echo ${platform} | tr 'A-Z' 'a-z'` #lowercase transformation
+prefix=`pwd`
+build_dir=${prefix}/build/${platform}
+install_dir=${prefix}/install/${platform}
+include_dir=${prefix}/install/include
+
+# Print out some stuff so the user knows what we are doing
+echo "Build type: ${build_type}"
+echo "Platform: ${platform}"
+echo "Prefix: ${prefix}"
+echo "Build directory: ${build_dir}"
+echo "Install directory: ${install_dir}"
+echo "Includes directory: ${include_dir}"
+
+# Here we create a directory for the build and call cmake followed by a make
+# all and install.
+[ ! -d ${build_dir} ] && mkdir -p ${build_dir}
+cd ${build_dir}
+cmake -DCMAKE_BUILD_TYPE=${build_type} -DPLATFORM=${platform} -DINSTALL_DIR=${install_dir} -DINCLUDE_DIR=${include_dir} ${prefix}
+make all install
--- extras/mrproper.sh
+++ extras/mrproper.sh
@@ -2,6 +2,7 @@
 # Andre Anjos <andre.dos.anjos@gmail.com>
 # Mon 28 Jun 2010 15:47:43 CEST
 find . -name '*~' -print0 | xargs -0 rm -vf
-rm -rf linux*
+rm -rf install*
 rm -rf include
-rm -rf build 
+rm -rf build/*
+[ -d build ] && rmdir build
