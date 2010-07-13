f7ce7e8fb0a311bb303fae63e564fe1264004859
André Anjos <andre.anjos@idiap.ch>
2010-07-13 18:39:54 +0200

    Working example of fdvideo in python

--- python/scripts/fdvideo.py
+++ python/scripts/fdvideo.py
@@ -9,18 +9,23 @@
 import os, sys
 import torch
 
+def draw_rectangle(i, pat, line_width, color):
+  """Draws a rectangle on an image in a certain position."""
+  for k in range(line_width):
+    i.drawRect(pat.x-k, pat.y-k, pat.width+(2*k), pat.height+(2*k), color) 
+
 def main():
   if len(sys.argv) == 1:
     print '%s - Detects faces in videos' % os.path.basename(sys.argv[0])
     print 'usage: %s <input-video> <facefinder-params> [<output-video>]' \
         % sys.argv[0]
+    sys.exit(1)
 
-  output_video = None
-  if len(sys.argv) >= 4: output_video = sys.argv[3]
-
-  input = torch.ip.Video(sys.argv[0])
-  finder = torch.scanning.FaceFinder(sys.argv[1])
+  input = torch.ip.Video(sys.argv[1])
+  finder = torch.scanning.FaceFinder(sys.argv[2])
   buffer = torch.ip.Image(1, 1, 1) #forces gray-scale conversion
+  output = None
+  if len(sys.argv) >= 4: output = torch.ip.Video(sys.argv[3], input)
 
   frame = 0
   while (input.read(buffer)):
@@ -29,10 +34,21 @@ def main():
       raise RuntimeError, 'FaceFinder could not process image'
     detections = finder.getPatterns()
 
-    if len(detections) > 1:
-      print 'Detected multiple faces on frame %d'
+    print 'frame: %d, detections: %d' % (frame, len(detections))
+    for k in detections: print k
 
+    finder.getScanner().deleteAllROIs()
+    if not finder.getScanner().addROIs(buffer, 0.3):
+      raise RuntimeError, 'Scanner could not set ROIs'
 
+    if output:
+      color_buffer = torch.ip.Image(buffer.getWidth(), buffer.getHeight(), 3)
+      color_buffer.copyFromImage(buffer)
+      for k in detections: draw_rectangle(color_buffer, k, 3, torch.ip.red)
+      output.write(color_buffer)
+
+  input.close()
+  if output: output.close()
 
 if __name__ == '__main__':
   main()
