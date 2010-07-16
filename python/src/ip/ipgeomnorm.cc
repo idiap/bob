a02f40c88652da0c3bd2c6307bf20aae11e195e9
André Anjos <andre.anjos@idiap.ch>
2010-07-16 11:32:11 +0200

    Remove spurious print out

--- python/src/ip/ipgeomnorm.cc
+++ python/src/ip/ipgeomnorm.cc
@@ -6,9 +6,11 @@
  */
 
 #include <boost/python.hpp>
+#include <boost/shared_ptr.hpp>
 
 #include "ip/ipGeomNorm.h"
 #include "trainer/GTFile.h"
+#include "ip/Image.h"
 
 using namespace boost::python;
 
@@ -19,12 +21,27 @@ static list ipgn_get_points(const Torch::ipGeomNorm& n) {
   return retval;
 }
 
+static boost::shared_ptr<Torch::ipGeomNorm> make_geomnorm(const char* cfg)
+{
+  boost::shared_ptr<Torch::ipGeomNorm> retval(new Torch::ipGeomNorm());
+  if (retval->loadCfg(cfg)) return retval;
+  return boost::shared_ptr<Torch::ipGeomNorm>();
+}
+
+static boost::shared_ptr<Torch::Image> ipgn_get_image(const Torch::ipGeomNorm& gn, int index) 
+{
+  boost::shared_ptr<Torch::Image> retval(new Torch::Image);
+  retval->setTensor(&gn.getOutput(index));
+  return retval;
+}
+
 void bind_ip_ipgeomnorm()
 {
-  class_<Torch::ipGeomNorm, bases<Torch::ipCore> >("ipGeomNorm", init<>())
-    .def("loadCfg", &Torch::ipGeomNorm::loadCfg)
+  class_<Torch::ipGeomNorm, boost::shared_ptr<Torch::ipGeomNorm>, bases<Torch::ipCore> >("ipGeomNorm", no_init)
+    .def("__init__", make_constructor(&make_geomnorm))
     .def("setGTFile", &Torch::ipGeomNorm::setGTFile)
     .def("getGTFile", &Torch::ipGeomNorm::getGTFile, return_internal_reference<>())
     .def("getNMPoints", &ipgn_get_points)
+    .def("getOutputImage", &ipgn_get_image, with_custodian_and_ward_postcall<0, 1>())
     ;
 }
