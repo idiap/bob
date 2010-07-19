1847568de5f10c9ac1710c595bc0fb89891df39a
André Anjos <andre.dos.anjos@gmail.com>
2010-07-19 22:56:12 +0200

    Finalize first working version of the first test for VideoTensors

--- python/tests/test_videotensor.py
+++ python/tests/test_videotensor.py
@@ -8,7 +8,7 @@
 
 # These are some global parameters for the test.
 INPUT_VIDEO = '/idiap/group/vision/visidiap/databases/banca/english/videos/1001_f_g1_04_1001_en.avi'
-OUTPUT_VIDEO = 'out.avi'
+OUTPUT_VIDEO = '/tmp/out.avi'
 
 import unittest
 import torch
@@ -22,30 +22,24 @@ class VideoTensorTest(unittest.TestCase):
     frames = 100
     vt = torch.ip.VideoTensor(width, height, 3, frames)
     pattern = torch.ip.Image(width, height, 3)
-    pattern.save("start.jpg")
     colors = (
               torch.ip.white, 
               torch.ip.blue, 
               torch.ip.red, 
               torch.ip.yellow, 
-              #torch.ip.green,
-              #torch.ip.cyan, 
-              #torch.ip.black, 
-              #torch.ip.pink
+              torch.ip.green,
+              torch.ip.cyan, 
+              torch.ip.black, 
+              torch.ip.pink
              )
     bar_width = width/len(colors)
     for i, c in enumerate(colors):
       for k in range(bar_width):
         pattern.drawLine(k+(i*bar_width), 0, k+(i*bar_width), height-1, c)
-    pattern.save("test.jpg")
-    import sys; exit(1)
     for i in range(100): vt.setFrame(pattern, i)
     out_video = torch.ip.Video(OUTPUT_VIDEO, width, height, 150000, 20, 5)
     self.assertEqual(vt.save(out_video), True)
     out_video.close()
-
-  def test02_CanCopyUsingTensor(self):
-    input_video = torch.ip.Video(INPUT_VIDEO)
 
 if __name__ == '__main__':
   import sys
