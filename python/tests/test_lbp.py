340c0794a1846944e02858ddb08ba0dd57ae3def
André Anjos <andre.dos.anjos@gmail.com>
2010-07-25 21:53:50 +0200

    More tests

--- python/tests/test_lbp.py
+++ python/tests/test_lbp.py
@@ -35,6 +35,10 @@ def generate_3x3_image(image, values):
   image.set(1, 0, 0, int(values[8]))
   #image.save('img_%s.pgm' % values)
 
+def calculate_lbp8r_rotinvariant_value(v):
+  """Calculates the rotation invariant LBP code for a certain 8-bit value"""
+   
+
 class Processor:
 
   def __init__(self, operator, generator, center):
@@ -86,85 +90,92 @@ class LBPTest(unittest.TestCase):
     op.setBOption("RotInvariant", True)
     proc = Processor(op, generate_3x3_image, (1,1))
     #torch's implementation start labelling the patterns from 1
-    self.assertEqual(proc('100000000'), 0x1)
-    self.assertEqual(proc('102000000'), 0x2)
-    self.assertEqual(proc('100020000'), 0x2)
-    self.assertEqual(proc('100000200'), 0x2)
-    self.assertEqual(proc('100000002'), 0x2)
-    self.assertEqual(proc('102020000'), 0x3)
-    self.assertEqual(proc('100020200'), 0x3)
-    self.assertEqual(proc('100000202'), 0x3)
-    self.assertEqual(proc('102000002'), 0x3)
-    self.assertEqual(proc('102020200'), 0x4)
-    self.assertEqual(proc('100020202'), 0x4)
-    self.assertEqual(proc('102000202'), 0x4)
-    self.assertEqual(proc('102020002'), 0x4)
-    #torch has this label's wrong
-    #self.assertEqual(proc('100020002'), 0x5)
-    #self.assertEqual(proc('102000200'), 0x5)
-    #self.assertEqual(proc('102020202'), 0x6)
+    self.assertEqual(proc('100000000'), 0x1) #0x0
+    self.assertEqual(proc('102000000'), 0x2) #0x1
+    self.assertEqual(proc('100020000'), 0x2) #0x1
+    self.assertEqual(proc('100000200'), 0x2) #0x1
+    self.assertEqual(proc('100000002'), 0x2) #0x1
+    self.assertEqual(proc('102020000'), 0x3) #0x3
+    self.assertEqual(proc('100020200'), 0x3) #0x3
+    self.assertEqual(proc('100000202'), 0x3) #0x3
+    self.assertEqual(proc('102000002'), 0x3) #0x3
+    self.assertEqual(proc('102020200'), 0x4) #0x7
+    self.assertEqual(proc('100020202'), 0x4) #0x7
+    self.assertEqual(proc('102000202'), 0x4) #0x7
+    self.assertEqual(proc('102020002'), 0x4) #0x7
+    #torch has these labels wrong
+    #self.assertEqual(proc('100020002'), 0x5) #0x5
+    #self.assertEqual(proc('102000200'), 0x5) #0x5
+    #self.assertEqual(proc('102020202'), 0x6) #0xf
     #self.assertEqual(op.max_label, 0xf) this is set to 16!
 
   def test03_u2_4p1r(self):
     op = torch.ip.ipLBP4R(1)
     op.setBOption("Uniform", True)
     proc = Processor(op, generate_3x3_image, (1,1))
-    self.assertEqual(proc('100000000'), 0x1)
-    self.assertEqual(proc('102000000'), 0x8)
-    self.assertEqual(proc('100020000'), 0x4)
-    self.assertEqual(proc('100000200'), 0x2)
-    self.assertEqual(proc('100000002'), 0x1)
-    self.assertEqual(proc('102020000'), 0xc)
-    self.assertEqual(proc('100020200'), 0x6)
-    self.assertEqual(proc('100000202'), 0x3)
-    self.assertEqual(proc('102000002'), 0x9)
-    self.assertEqual(proc('100020002'), 0x0)
-    self.assertEqual(proc('102000200'), 0x0)
-    self.assertEqual(proc('102020200'), 0xe)
-    self.assertEqual(proc('100020202'), 0x7)
-    self.assertEqual(proc('102000202'), 0xb)
-    self.assertEqual(proc('102020002'), 0xd)
-    self.assertEqual(proc('102020202'), 0xf)
+    self.assertEqual(proc('100000000'), 0x1) #0x0
+    self.assertEqual(proc('102000000'), 0x2) #0x8
+    self.assertEqual(proc('100020000'), 0x3) #0x4
+    self.assertEqual(proc('100000200'), 0x4) #0x2
+    self.assertEqual(proc('100000002'), 0x5) #0x1
+    self.assertEqual(proc('102020000'), 0x6) #0xc
+    self.assertEqual(proc('100020200'), 0x7) #0x6
+    self.assertEqual(proc('100000202'), 0x8) #0x3
+    self.assertEqual(proc('102000002'), 0x9) #0x9 (by chance!)
+    self.assertEqual(proc('102020200'), 0xa) #0xe
+    self.assertEqual(proc('100020202'), 0xb) #0x7
+    self.assertEqual(proc('102000202'), 0xc) #0xb
+    self.assertEqual(proc('102020002'), 0xd) #0xd (by chance!)
+    self.assertEqual(proc('100020002'), 0x0) #non-uniform(2) => 0x0
+    self.assertEqual(proc('102000200'), 0x0) #non-uniform(2) => 0x0
+    self.assertEqual(proc('102020202'), 0xe) #0xf
+
+  def test04_rotinvariant_u2_4p1r(self):
+    op = torch.ip.ipLBP4R(1)
+    op.setBOption("Uniform", True)
+    op.setBOption("RotInvariant", True)
+    proc = Processor(op, generate_3x3_image, (1,1))
+    self.assertEqual(proc('100000000'), 0x1) #0x0
+    self.assertEqual(proc('102000000'), 0x2) #0x8
+    self.assertEqual(proc('100020000'), 0x2) #0x4
+    self.assertEqual(proc('100000200'), 0x2) #0x2
+    self.assertEqual(proc('100000002'), 0x2) #0x1
+    self.assertEqual(proc('102020000'), 0x3) #0xc
+    self.assertEqual(proc('100020200'), 0x3) #0x6
+    self.assertEqual(proc('100000202'), 0x3) #0x3
+    #self.assertEqual(proc('102000002'), 0x3) #0x9 #missing from torch
+    self.assertEqual(proc('102020200'), 0x4) #0xe
+    self.assertEqual(proc('100020202'), 0x4) #0x7
+    #self.assertEqual(proc('102000202'), 0x4) #0xb #missing from torch
+    #self.assertEqual(proc('102020002'), 0x4) #0xd #missing from torch
+    self.assertEqual(proc('100020002'), 0x0) #non-uniform(2) => 0x0
+    self.assertEqual(proc('102000200'), 0x0) #non-uniform(2) => 0x0
+    self.assertEqual(proc('102020202'), 0x5) #0xf
+
+  def test05_vanilla_4p1r_toaverage(self):
+    op = torch.ip.ipLBP4R(1)
+    proc = Processor(op, generate_3x3_image, (1,1))
+    op.setBOption("ToAverage", True)
+    self.assertEqual(proc('100000000'), 0x0) #average is 0.2
+    self.assertEqual(proc('102000000'), 0x8) #average is 0.6
+    self.assertEqual(proc('100020000'), 0x4) #average is 0.6
+    self.assertEqual(proc('100000200'), 0x2) #average is 0.6
+    self.assertEqual(proc('100000002'), 0x1) #average is 0.6
+    self.assertEqual(proc('102020000'), 0xc) #average is 1.0
+    self.assertEqual(proc('100020200'), 0x6) #average is 1.0
+    self.assertEqual(proc('100000202'), 0x3) #average is 1.0
+    self.assertEqual(proc('102000002'), 0x9) #average is 1.0
+    self.assertEqual(proc('102020200'), 0xe) #average is 1.4
+    self.assertEqual(proc('100020202'), 0x7) #average is 1.4
+    self.assertEqual(proc('102000202'), 0xb) #average is 1.4
+    self.assertEqual(proc('102020002'), 0xd) #average is 1.4
+    self.assertEqual(proc('100020002'), 0x5) #average is 1.0
+    self.assertEqual(proc('102000200'), 0xa) #average is 1.0
+    self.assertEqual(proc('102020202'), 0xf) #average is 1.8
+    #self.assertEqual(op.max_label, 0xf) this is set to 16!
 
 if __name__ == '__main__':
   import os, sys
   sys.argv.append('-v')
   unittest.main()
 
-"""
-test01_4p1r  (4 points, radius = 1 pixel):
-
- +--------+----------+----------+----------------+----------+
- | center | outside  |    lbp   | rot. invariant |    u2    |
- +--------+----------+----------+----------------+----------+
- | pixel0 | 12345678 |          |                |          |
- +--------+----------+----------+----------------+----------+
- |   0    | 11111111 |   1111   |      1111      |   1111   |
- |   1    | 00000000 |   0000   |      0000      |   0000   |
- |   1    | 10000000 |   0000   |      0000      |   0000   |
- |   1    | 11000000 |   1000   |      0001      |   1000   |
- |   1    | 11100000 |   1000   |      0001      |   1000   |
- |   1    | 11110000 | 11110000 |    00001111    | 11110000 |
- |   1    | 11111000 | 11111000 |    00011111    | 11111000 |
- |   1    | 11111100 | 11111100 |    00111111    | 11111100 |
- |   1    | 11111110 | 11111110 |    01111111    | 11111110 |
- |   1    | 11111111 | 11111111 |    11111111    | 11111111 |
- |   1    | 10111111 | 10111111 |    01111111    | 10111111 |
- |   1    | 10011111 | 10011111 |    00111111    | 10011111 |
- |   1    | 10001111 | 10001111 |    00011111    | 10001111 |
- |   1    | 10000111 | 10000111 |    00001111    | 10000111 |
- |   1    | 10000011 | 10000011 |    00000111    | 10000011 |
- |   1    | 10000001 | 10000001 |    00000011    | 10000001 |
- |   1    | 10000001 | 10000001 |    00000011    | 10000001 |
- |        |          |          |                |          |
- |        |          |          |                |          |
- |        |          |          |                |          |
- |        |          |          |                |          |
- |        |          |          |                |          |
- |        |          |          |                |          |
- |        |          |          |                |          |
- |        |          |          |                |          |
- |        |          |          |                |          |
- +--------+----------+----------+----------------+----------+
-
-"""
