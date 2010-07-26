cc0fe683767421c3bc968fa734ece8a69ddc4018
André Anjos <andre.anjos@idiap.ch>
2010-07-26 18:38:58 +0200

    Only compiles ffmpeg support in if found

--- python/src/ip/main.cc
+++ python/src/ip/main.cc
@@ -22,8 +22,10 @@ BOOST_PYTHON_MODULE(libpytorch_ip) {
   bind_ip_color();
   bind_ip_vision();
   bind_ip_image();
+#ifdef HAVE_FFMPEG
   bind_ip_video();
   bind_ip_videotensor();
+#endif
   bind_ip_ipgeomnorm();
   bind_ip_lbp();
 }
