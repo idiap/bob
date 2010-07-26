0e28657e9888ed7d39f2ee664ea0f3f63f633830
André Anjos <andre.anjos@idiap.ch>
2010-07-26 18:22:30 +0200

    Remove module line

--- extras/ffmpegConfig.cmake
+++ extras/ffmpegConfig.cmake
@@ -2,5 +2,4 @@
 # Andre Anjos - 25.june.2010
 
 include(FindPkgConfig)
-set(ffmpeg_MODULES "libavformat;libavcodec;libswscale;libavutil" CACHE INTERNAL "modules")
 pkg_check_modules(ffmpeg REQUIRED libavformat>=52 libavcodec>=52 libswscale>=0.11 libavutil>=50)
