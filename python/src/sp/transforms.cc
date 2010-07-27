4c5f93a7793ac5882086e7eefbaf1f901dda6ed3
André Anjos <andre.anjos@idiap.ch>
2010-07-27 12:15:32 +0200

    Include DCT and FFT into the python mix

---
+++ python/src/sp/transforms.cc
@@ -0,0 +1,23 @@
+/**
+ * @file src/python/ip/color.cc 
+ * @author <a href="mailto:andre.anjos@idiap.ch">Andre Anjos</a> 
+ *
+ * @brief Binds Color to python 
+ */
+
+#include <boost/python.hpp>
+
+#include "core/Tensor.h"
+#include "core/spCore.h"
+#include "sp/spDCT.h"
+#include "sp/spFFT.h"
+
+using namespace boost::python;
+
+void bind_sp_transforms()
+{
+  class_<Torch::spDCT, bases<Torch::spCore> >("spDCT", "This class is designed to perform a DCT (or inverse DCT) over an input tensor. The result is a tensor of the same storage type.", init<optional<bool> >(arg("inverse"), "Creates an new DCT operator."))
+    ;
+  class_<Torch::spFFT, bases<Torch::spCore> >("spFFT", "This class is designed to perform an FFT (or inverse FFT) over an input tensor. The result is a tensor of the same storage type.", init<optional<bool> >(arg("inverse"), "Creates an new FFT operator."))
+    ;
+}
--- python/torch/__init__.py
+++ python/torch/__init__.py
@@ -1,8 +1,9 @@
 """Torch is a toolkit of biometric utilities for face and speech processing.
 """
 import core
+import sp
 import scanning
 import ip
 import trainer
 
-__all__ = ['core', 'scanning', 'ip', 'trainer'] 
+__all__ = ['core', 'sp', 'scanning', 'ip', 'trainer'] 
