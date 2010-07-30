f60b536bdeea6009e283d7014cca5955001412fa
André Anjos <andre.anjos@idiap.ch>
2010-07-30 14:45:15 +0200

    Add core tensor benchmarks

---
+++ benchmark/core/tensor.cc
@@ -0,0 +1,251 @@
+/**
+ * @file tensor.cc
+ * @author <a href="mailto:andre.anjos@idiap.ch">Andre Anjos</a> 
+ *
+ * @brief Extensive Tensor benchmark tests 
+ */
+
+#include <iostream>
+#include <stdint.h>
+#include <boost/timer.hpp>
+#include <boost/format.hpp>
+#include "core/Tensor.h"
+
+const char* type2string(const Torch::Tensor& t) {
+  static const char* d = "Double";
+  static const char* f = "Float";
+  static const char* l = "Long";
+  static const char* i = "Int";
+  static const char* s = "Short";
+  static const char* c = "Char";
+  static const char* u = "Unknown";
+  switch (t.getDatatype()) {
+    case Torch::Tensor::Double:
+      return d;
+      break;
+    case Torch::Tensor::Float:
+      return f;
+      break;
+    case Torch::Tensor::Long:
+      return l;
+      break;
+    case Torch::Tensor::Int:
+      return i;
+      break;
+    case Torch::Tensor::Short:
+      return s;
+      break;
+    case Torch::Tensor::Char:
+      return c;
+      break;
+  }
+  return u;
+}
+
+template<typename T> void benchmark_allocation
+(const unsigned times, const unsigned s) {
+  short v = 0;
+  boost::timer timer;
+  for (unsigned n=0; n<times; ++n) {
+    Torch::Tensor* t = new T(s, s, s, s);
+    delete t;
+  }
+  double elapsed = timer.elapsed();
+  T tmp;
+  float tsize = s*s*s*s*tmp.typeSize();
+  boost::format value("%.2f b");
+  if (tsize > 1024) {
+    tsize /= 1024;
+    value = boost::format("%.2f kb");
+  }
+  if (tsize > 1024) {
+    tsize /= 1024;
+    value = boost::format("%.2f Mb");
+  }
+  if (tsize > 1024) {
+    tsize /= 1024;
+    value = boost::format("%.2f Gb");
+  }
+
+  std::cout << "total: " << elapsed << " s; tensor size: "
+            << value % tsize << "; per alloc.: " << (1000000*elapsed)/times
+            << " us";
+}
+
+void benchmark_set(const int times, Torch::Tensor& t) {
+  short v = 0;
+  const unsigned S[] = {t.size(0), t.size(1), t.size(2), t.size(3)};
+  boost::timer timer;
+  for (unsigned n=0; n<times; ++n) 
+    for(unsigned int i=0; i<S[0]; ++i)  
+      for(unsigned int j=0; j<S[1]; ++j)  
+        for(unsigned int k=0; k<S[2]; ++k)  
+          for(unsigned int l=0; l<S[3]; ++l) {
+            t.set(i, j, k, l, v++);
+          }
+  double elapsed = timer.elapsed();
+  uint64_t total = times * t.sizeAll();
+  std::cout << "total: " << elapsed << " s; per element: "
+            << ((1000000.0)*(elapsed/total)) << " us";
+}
+
+template<typename T> double benchmark_get(const int times, const T& t) {
+  double v = 0;
+  const unsigned S[] = {t.size(0), t.size(1), t.size(2), t.size(3)};
+  boost::timer timer;
+  for (unsigned n=0; n<times; ++n) 
+    for(unsigned int i=0; i<S[0]; ++i)  
+      for(unsigned int j=0; j<S[1]; ++j)  
+        for(unsigned int k=0; k<S[2]; ++k)  
+          for(unsigned int l=0; l<S[3]; ++l) {
+            v += t.get(i, j, k, l);
+          }
+  double elapsed = timer.elapsed();
+  uint64_t total = times * t.sizeAll();
+  std::cout << "total: " << elapsed << " s; per element: "
+            << ((1000000.0)*(elapsed/total)) << " us";
+  return v;
+}
+
+template<typename T> void benchmark_narrow(const int times, const T& t) {
+  const unsigned S[] = {t.size(0), t.size(1), t.size(2), t.size(3)};
+  T narrowed;
+  boost::timer timer;
+  for (unsigned n=0; n<times; ++n) narrowed.narrow(&t, 0, 0, S[0]/2);
+  double elapsed = timer.elapsed();
+  std::cout << "total: " << elapsed << " s; per operation: "
+            << ((1000000.0)*(elapsed/times)) << " us";
+}
+
+template<typename T> void 
+benchmark_add(const int times, const T& t1, const T& t2) {
+  const unsigned S[] = {t1.size(0), t1.size(1), t1.size(2), t1.size(3)};
+  T result(S[0], S[1], S[2], S[3]);
+  boost::timer timer;
+  for (unsigned n=0; n<times; ++n) 
+    for(unsigned int i=0; i<S[0]; ++i)  
+      for(unsigned int j=0; j<S[1]; ++j)  
+        for(unsigned int k=0; k<S[2]; ++k)  
+          for(unsigned int l=0; l<S[3]; ++l) {
+            result(i, j, k, l) = t1.get(i, j, k, l) + t2.get(i, j, k, l);
+          }
+  double elapsed = timer.elapsed();
+  std::cout << "total: " << elapsed << " s; per time: "
+            << ((1000.0)*(elapsed/times)) << " ms";
+}
+
+int main(int argc, char** argv) {
+  boost::format H("%19s | ");
+  
+  //Allocation tests
+  const unsigned TS[] = {1, 10, 100};
+  const unsigned AR[] = {2500000, 2500000, 200000};
+  for (unsigned k=0; k<3; ++k) {
+    std::cout << H % "Alloc (Double/4d)";
+    benchmark_allocation<Torch::DoubleTensor>(AR[k], TS[k]);
+    std::cout << std::endl;
+    std::cout << H % "Alloc (Float/4d)";
+    benchmark_allocation<Torch::FloatTensor>(AR[k], TS[k]);
+    std::cout << std::endl;
+    std::cout << H % "Alloc (Long/4d)";
+    benchmark_allocation<Torch::LongTensor>(AR[k], TS[k]);
+    std::cout << std::endl;
+    std::cout << H % "Alloc (Int/4d)";
+    benchmark_allocation<Torch::IntTensor>(AR[k], TS[k]);
+    std::cout << std::endl;
+    std::cout << H % "Alloc (Short/4d)";
+    benchmark_allocation<Torch::ShortTensor>(AR[k], TS[k]);
+    std::cout << std::endl;
+    std::cout << H % "Alloc (Char/4d)";
+    benchmark_allocation<Torch::CharTensor>(AR[k], TS[k]);
+    std::cout << std::endl;
+  }
+
+  //Set tests //would narrowing be slower?
+  const unsigned N = 20;
+  const unsigned S[] = {100, 100, 10, 10};
+  std::cout << H % "Set (4d)";
+  Torch::FloatTensor dt4(S[0], S[1], S[2], S[3]);
+  benchmark_set(N, dt4);
+  std::cout << std::endl;
+  std::cout << H % "Set (4d/narrowed)";
+  Torch::FloatTensor dt4_narrow;
+  dt4_narrow.narrow(&dt4, 3, 0, 5);
+  benchmark_set(2*N, dt4_narrow);
+  std::cout << std::endl;
+  std::cout << H % "Set (3d)";
+  Torch::FloatTensor dt3(S[0], S[1], S[2]);
+  benchmark_set(250*N, dt3);
+  std::cout << std::endl;
+  std::cout << H % "Set (3d/narrowed)";
+  Torch::FloatTensor dt3_narrow;
+  dt3_narrow.narrow(&dt3, 2, 0, 5);
+  benchmark_set(400*N, dt3_narrow);
+  std::cout << std::endl;
+  std::cout << H % "Set (2d)";
+  Torch::FloatTensor dt2(S[0], S[1]);
+  benchmark_set(2500*N, dt2);
+  std::cout << std::endl;
+  std::cout << H % "Set (2d/narrowed)";
+  Torch::FloatTensor dt2_narrow;
+  dt2_narrow.narrow(&dt2, 1, 0, 50);
+  benchmark_set(4000*N, dt2_narrow);
+  std::cout << std::endl;
+  std::cout << H % "Set (1d)";
+  Torch::FloatTensor dt1(S[0]);
+  benchmark_set(250000*N, dt1);
+  std::cout << std::endl;
+  std::cout << H % "Set (1d/narrowed)";
+  Torch::FloatTensor dt1_narrow;
+  dt1_narrow.narrow(&dt1, 0, 0, 50);
+  benchmark_set(250000*N, dt1_narrow);
+  std::cout << std::endl;
+
+  //Retrieval tests
+  std::cout << H % "Get (4d)";
+  benchmark_get(N, dt4);
+  std::cout << std::endl;
+  std::cout << H % "Get (4d/narrowed)";
+  benchmark_get(2*N, dt4_narrow);
+  std::cout << std::endl;
+  std::cout << H % "Get (3d)";
+  benchmark_get(250*N, dt3);
+  std::cout << std::endl;
+  std::cout << H % "Get (3d/narrowed)";
+  benchmark_get(400*N, dt3_narrow);
+  std::cout << std::endl;
+  std::cout << H % "Get (2d)";
+  benchmark_get(2500*N, dt2);
+  std::cout << std::endl;
+  std::cout << H % "Get (2d/narrowed)";
+  benchmark_get(4000*N, dt2_narrow);
+  std::cout << std::endl;
+  std::cout << H % "Get (1d)";
+  benchmark_get(250000*N, dt1);
+  std::cout << std::endl;
+  std::cout << H % "Get (1d/narrowed)";
+  benchmark_get(250000*N, dt1_narrow);
+  std::cout << std::endl;
+
+  //Narrow tests
+  std::cout << H % "Narrow (4d)";
+  benchmark_narrow(10000000, dt4);
+  std::cout << std::endl;
+  std::cout << H % "Narrow (3d)";
+  benchmark_narrow(10000000, dt3);
+  std::cout << std::endl;
+  std::cout << H % "Narrow (2d)";
+  benchmark_narrow(10000000, dt2);
+  std::cout << std::endl;
+  std::cout << H % "Narrow (1d)";
+  benchmark_narrow(10000000, dt1);
+  std::cout << std::endl;
+
+  //Operation tests
+  boost::format fsize("Add (%d,%d,%d,%d)");
+  std::string size = str(fsize % dt4.size(0) % dt4.size(1) % dt4.size(2) 
+      % dt4.size(3));
+  std::cout << H % size;
+  benchmark_add(10, dt4, dt4);
+  std::cout << std::endl;
+}
