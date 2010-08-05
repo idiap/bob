f9ae63ee18f31b11a01895027df6faa636b65b65
André Anjos <andre.anjos@idiap.ch>
2010-08-05 10:14:50 +0200

    Fixes on the compilation system; Do not attempt to build complex static libraries

--- extras/cblasConfig.cmake
+++ extras/cblasConfig.cmake
@@ -4,17 +4,11 @@ set(cblas_LIBDIRS /usr/lib;/usr/lib/atlas CACHE INTERNAL "libdirs")
 find_path(cblas_INCLUDE NAMES cblas.h)
 find_path(lapack_INCLUDE NAMES clapack.h)
 
-find_library(lapack_LIBRARY NAMES clapack lapack PATHS ${cblas_LIBDIRS})
+find_library(lapack_LIBRARY NAMES lapack lapack PATHS ${cblas_LIBDIRS})
 find_library(cblas_LIBRARY NAMES cblas)
 find_library(atlas_LIBRARY NAMES atlas)
 set(cblas_LIBRARIES ${lapack_LIBRARY};${cblas_LIBRARY};${atlas_LIBRARY} CACHE
     INTERNAL "libraries")
-set(cblas_ARCHIVES "" CACHE INTERNAL "archives")
-
-foreach(a ${cblas_LIBRARIES})
-	STRING(REGEX REPLACE "(.+)\\.(so|dylib)$" "\\1.a" tmp "${a}")
-  set(cblas_ARCHIVES ${cblas_ARCHIVES};${tmp} CACHE INTERNAL "archives")
-endforeach(a ${cblas_LIBRARIES})
 
 if(cblas_INCLUDE AND lapack_INCLUDE AND cblas_LIBRARY AND lapack_LIBRARY)
   set(cblas_FOUND "YES" CACHE INTERNAL "package")
