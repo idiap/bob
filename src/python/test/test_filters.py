beee6cf1152eea3f4e473ca7d46672543c978352
André Anjos <andre.anjos@idiap.ch>
2010-08-25 16:34:44 +0200

    Add SmoothGaussian tests

--- src/python/test/test_filters.py
+++ src/python/test/test_filters.py
@@ -249,7 +249,7 @@ class FilterTest(unittest.TestCase):
           self.assertEqual(processed.get(j, i, k), reference.get(j, i, k))
 
   def test13_shift(self):
-    v = torch.ip.Image(1, 1, 1) #histo equalization only works on grayscaled
+    v = torch.ip.Image(1, 1, 1)
     v.load(INPUT_IMAGE)
     f = torch.ip.ipShift()
     self.assertEqual(f.setIOption('shiftx', 10), True)
@@ -262,6 +262,26 @@ class FilterTest(unittest.TestCase):
     # compare to our model
     reference = torch.ip.Image(1, 1, 1)
     reference.load('shift.ppm')
+    for i in range(reference.width):
+      for j in range(reference.height):
+        for k in range(reference.nplanes):
+          self.assertEqual(processed.get(j, i, k), reference.get(j, i, k))
+
+  def test14_smoothGaussian(self):
+    v = torch.ip.Image(1, 1, 3)
+    v.load(INPUT_IMAGE)
+    f = torch.ip.ipSmoothGaussian()
+    self.assertEqual(f.setIOption('RadiusX', 5), True)
+    self.assertEqual(f.setIOption('RadiusY', 5), True)
+    self.assertEqual(f.setDOption('Sigma', 5.0), True)
+    self.assertEqual(f.process(v), True)
+    self.assertEqual(f.getNOutputs(), 1)
+    self.assertEqual(f.getOutput(0).getDatatype(), torch.core.Type.Short)
+    processed = torch.ip.Image(f.getOutput(0))
+    #processed.save('smoothgauss.ppm') #use this to save another reference image
+    # compare to our model
+    reference = torch.ip.Image(1, 1, 3)
+    reference.load('smoothgauss.ppm')
     for i in range(reference.width):
       for j in range(reference.height):
         for k in range(reference.nplanes):
