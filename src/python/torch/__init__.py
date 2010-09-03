3bfa30741adc385dc829930a2f9f62494a7768f9
André Anjos <andre.anjos@idiap.ch>
2010-09-03 13:25:14 +0200

    Use ctypes instead of dl as it is more portable

--- src/python/torch/__init__.py
+++ src/python/torch/__init__.py
@@ -4,9 +4,9 @@
 # This is a fix to the problem of using templates/static/exceptions/dynamic
 # casts with boost.python. It makes all symbols loaded by python from this
 # point onwards global
-import sys, dl
+import sys, ctypes
 default_flags = sys.getdlopenflags()
-sys.setdlopenflags(dl.RTLD_NOW | dl.RTLD_GLOBAL)
+sys.setdlopenflags(default_flags|ctypes.RTLD_GLOBAL)
 
 import core
 import sp
