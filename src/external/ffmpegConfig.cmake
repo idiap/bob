06f85fef396d3ecf9cadd1fdca692c436fda507e
André Anjos <andre.anjos@idiap.ch>
2010-10-28 10:35:53 +0200

    Move externals configuration into the externals dir

--- src/external/ffmpegConfig.cmake
+++
@@ -1,6 +0,0 @@
-# Tries to find a local version of ffmpeg/developement installed
-# Andre Anjos - 25.june.2010
-
-include(FindPkgConfig)
-pkg_check_modules(ffmpeg REQUIRED libavformat>=52 libavcodec>=52 libavutil>=49 libswscale>=0.10)
-link_directories(${ffmpeg_LIBRARY_DIRS})
