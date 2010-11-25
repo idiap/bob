fc909fb6c122cb308e1b3d9363237095bd9b72c5
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2010-11-25 21:12:39 +0100

    Remove oourafft and related functions

--- src/sp/python/src/transforms.cc
+++ src/sp/python/src/transforms.cc
@@ -12,10 +12,8 @@
 #include "sp/spCore.h"
 #include "sp/spDCT.h"
 #include "sp/spFCT.h"
-#include "sp/spFCT_oourafft.h"
 #include "sp/spDFT.h"
 #include "sp/spFFT.h"
-#include "sp/spFFT_oourafft.h"
 
 using namespace boost::python;
 
@@ -25,12 +23,8 @@ void bind_sp_transforms()
     ;
   class_<Torch::spFCT, bases<Torch::spCore> >("spFCT", "This class is designed to perform a Fast Cosine Transform (FCT) (or inverse FCT) using the FFTPACK 4.1 library over an input tensor. The ouput is a FloatTensor, and is supposed to be identical to the output of spDCT.", init<optional<bool> >(arg("inverse"), "Creates a new FCT operator."))
     ;
-  class_<Torch::spFCT_oourafft, bases<Torch::spCore> >("spFCT_oourafft", "This class is designed to perform a Fast Cosine Transform (FCT) (or inverse FCT) over an input tensor using the oourafft library. It only allows power of two length. The output is a FloatTensor, and is supposed to be identical to the output of spDCT.", init<optional<bool> >(arg("inverse"), "Creates a new FCT operator."))
-    ;
   class_<Torch::spDFT, bases<Torch::spCore> >("spDFT", "This class is designed to perform a Discrete Fourier Transform (DFT) (or inverse DFT) over an input tensor using a naive algorithm. The output is a FloatTensor. A more efficient alternative is to used spFFT.", init<optional<bool> >(arg("inverse"), "Creates a new DFT operator."))
     ;
   class_<Torch::spFFT, bases<Torch::spCore> >("spFFT", "This class is designed to perform a Fast Fourier Transform (FFT) (or inverse FFT) over an input tensor using the FFTPACK 4.1 library. The output is a FloatTensor, and is supposed to be identical to the output of spDFT.", init<optional<bool> >(arg("inverse"), "Creates a new FFT operator."))
-    ;
-  class_<Torch::spFFT_oourafft, bases<Torch::spCore> >("spFFT_oourafft", "This class is designed to perform a Fast Fourier Transform (FFT) (or inverse FFT) over an input tensor using the oourafft library. It only allows power of two length. The output is a FloatTensor, and is supposed to be identical to the output of spDFT.", init<optional<bool> >(arg("inverse"), "Creates an new FFT operator."))
     ;
 }
