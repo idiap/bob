f643714bf1ea6beded5323f1d1b7cbe1676f5599
André Anjos <andre.anjos@idiap.ch>
2010-12-03 15:54:10 +0100

    Implement python logging

--- src/core/python/core/__init__.py
+++ src/core/python/core/__init__.py
@@ -2,6 +2,25 @@ from libpytorch_core import *
 from . import array
 __all__ = dir()
 
+#this will setup divergence from C++ into python.logging correctly
+import logging
+from . import colorlog
+
+#this configures our core logger
+logger = logging.getLogger("torch")
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter("%(name)s@%(asctime)s|%(levelname)s: %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+cxx_logger = logging.getLogger('torch.cxx')
+debug.reset(PythonLoggingOutputDevice(cxx_logger.debug))
+info.reset(PythonLoggingOutputDevice(cxx_logger.info))
+warn.reset(PythonLoggingOutputDevice(cxx_logger.warn))
+error.reset(PythonLoggingOutputDevice(cxx_logger.error))
+
 def variables(self):
   """Returns all available variables in a list."""
   retval = []
