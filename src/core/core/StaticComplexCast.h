eb4f43591d7e6f05fa82116ce422394a7fd150da
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2010-12-13 10:43:23 +0100

    Add doxygen documentation to StaticComplexCast

--- src/core/core/StaticComplexCast.h
+++ src/core/core/StaticComplexCast.h
@@ -1,12 +1,29 @@
-#ifndef TORCH5SPRO_STATIC_COMPLEX_CAST_H
-#define TORCH5SPRO_STATIC_COMPLEX_CAST_H 1
+/**
+ * @file src/core/core/StaticComplexCast.h
+ * @author <a href="mailto:Laurent.El-Shafey@idiap.ch">Laurent El Shafey</a> 
+ *
+ * @brief This file defines functions which add std::complex support to the 
+ * static_cast function.
+ */
+
+#ifndef TORCH5SPRO_CORE_STATIC_COMPLEX_CAST_H
+#define TORCH5SPRO_CORE_STATIC_COMPLEX_CAST_H 1
 
 #include <complex>
 
 namespace Torch {
-
+/**
+ * \ingroup libcore_api
+ * @{
+ *
+ */
   namespace core {
 
+    /**
+     * @brief Functions which add std::complex support to the static_cast
+     * function. This is done by considering the real part only of any
+     * complex number.
+     */
     template<typename T, typename U> 
     void static_complex_cast(const U& in, T& out) {
       out = static_cast<T>(in);
@@ -30,7 +47,10 @@ namespace Torch {
     }
 
   }
+/**
+ * @}
+ */
 }
 
-#endif /* TORCH5SPRO_STATIC_COMPLEX_CAST_H */
+#endif /* TORCH5SPRO_CORE_STATIC_COMPLEX_CAST_H */
 
