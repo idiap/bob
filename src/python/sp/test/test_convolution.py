b21ef00bc05b219291f0374bdb26789f25483202
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2011-01-28 21:28:53 +0100

    Convolution: add some more python tests

--- src/python/sp/test/test_convolution.py
+++ src/python/sp/test/test_convolution.py
@@ -21,6 +21,23 @@ b1_4 = torch.core.array.float64_1([0.2551, 0.5060, 0.6991, 0.8909], (4,));
 b1_5 = torch.core.array.float64_1([0.9593, 0.5472, 0.1386, 0.1493, 0.2575],
   (5,));
 
+A2_5 = torch.core.array.float64_2([
+  0.8407, 0.3500, 0.3517, 0.2858, 0.0759,
+  0.2543, 0.1966, 0.8308, 0.7572, 0.0540,
+  0.8143, 0.2511, 0.5853, 0.7537, 0.5308,
+  0.2435, 0.6160, 0.5497, 0.3804, 0.7792,
+  0.9293, 0.4733, 0.9172, 0.5678, 0.9340], (5,5));
+
+b2_2 = torch.core.array.float64_2([
+  0.1299, 0.4694,
+  0.5688, 0.0119], (2,2));
+
+b2_3 = torch.core.array.float64_2([
+  0.3371, 0.3112, 0.6020,
+  0.1622, 0.5285, 0.2630,
+  0.7943, 0.1656, 0.6541], (3,3));
+
+
 RES_A1_10_b1_3_full = torch.core.array.float64_1([0.4152, 0.6005, 0.8634, 
   1.0266, 0.7429, 0.7524, 0.5982, 0.4405, 0.7626, 0.7884, 0.7972, 0.2557], 
   (12,));
@@ -49,11 +66,53 @@ RES_A1_10_b1_5_same = torch.core.array.float64_1([0.7761, 1.0136, 1.3340,
 
 RES_A1_10_b1_5_valid = torch.core.array.float64_1([1.3340, 0.8442, 0.4665, 
   0.8386, 1.4028, 0.9804], (6,));
+
+RES_A2_5_b2_2_full = torch.core.array.float64_2([
+  0.1092, 0.4401, 0.2100, 0.2022, 0.1440, 0.0356,
+  0.5113, 0.3540, 0.4044, 0.6551, 0.4090, 0.0262,
+  0.2504, 0.5297, 0.6688, 0.8132, 0.4624, 0.2498,
+  0.4948, 0.3469, 0.6965, 0.7432, 0.5907, 0.3721,
+  0.2592, 0.8510, 0.6613, 0.7272, 0.8356, 0.4477,
+  0.5286, 0.2803, 0.5274, 0.3339, 0.5380, 0.0111], (6,6));
+
+RES_A2_5_b2_2_same = torch.core.array.float64_2([
+  0.3540, 0.4044, 0.6551, 0.4090, 0.0262,
+  0.5297, 0.6688, 0.8132, 0.4624, 0.2498,
+  0.3469, 0.6965, 0.7432, 0.5907, 0.3721,
+  0.8510, 0.6613, 0.7272, 0.8356, 0.4477,
+  0.2803, 0.5274, 0.3339, 0.5380, 0.0111], (5,5));
+
+RES_A2_5_b2_2_valid = torch.core.array.float64_2([
+  0.3540, 0.4044, 0.6551, 0.4090,
+  0.5297, 0.6688, 0.8132, 0.4624,
+  0.3469, 0.6965, 0.7432, 0.5907,
+  0.8510, 0.6613, 0.7272, 0.8356], (4,4));
+
+RES_A2_5_b2_3_full = torch.core.array.float64_2([
+  0.2834, 0.3796, 0.7336, 0.4165, 0.3262, 0.1957, 0.0457,
+  0.2221, 0.6465, 0.9574, 0.9564, 1.0098, 0.5879, 0.0524,
+  0.9835, 0.9216, 1.9583, 1.7152, 1.7309, 1.0461, 0.3833,
+  0.4161, 0.9528, 1.8242, 2.0354, 2.0621, 1.4545, 0.6439,
+  0.9995, 1.0117, 2.5338, 2.1359, 2.4450, 1.7253, 1.1143,
+  0.3441, 1.0976, 1.3412, 1.4975, 1.7343, 1.0209, 0.7553,
+  0.7381, 0.5299, 1.4147, 0.9125, 1.4358, 0.5261, 0.6109], (7,7));
+
+RES_A2_5_b2_3_same = torch.core.array.float64_2([
+  0.6465, 0.9574, 0.9564, 1.0098, 0.5879,
+  0.9216, 1.9583, 1.7152, 1.7309, 1.0461,
+  0.9528, 1.8242, 2.0354, 2.0621, 1.4545,
+  1.0117, 2.5338, 2.1359, 2.4450, 1.7253,
+  1.0976, 1.3412, 1.4975, 1.7343, 1.0209], (5,5));
+
+RES_A2_5_b2_3_valid = torch.core.array.float64_2([
+  1.9583, 1.7152, 1.7309,
+  1.8242, 2.0354, 2.0621,
+  2.5338, 2.1359, 2.4450], (3,3));
 #############################################################################
+
 
 def compare(v1, v2, width):
   return abs(v1-v2) <= width
-
 
 def test_convolve_1D_nopt(A, b, reference, obj):
   # Compute the convolution product
@@ -70,6 +129,24 @@ def test_convolve_1D(A, b, reference, obj, option):
   obj.assertEqual(res.shape(), reference.shape())
   for i in range(res.extent(0)):
     obj.assertTrue(compare(res[i], reference[i], eps))
+
+def test_convolve_2D_nopt(A, b, reference, obj):
+  # Compute the convolution product
+  res = torch.sp.convolve(A, b)
+  
+  obj.assertEqual(res.shape(), reference.shape())
+  for i in range(res.extent(0)):
+    for j in range(res.extent(1)):
+      obj.assertTrue(compare(res[i,j], reference[i,j], eps))
+
+def test_convolve_2D(A, b, reference, obj, option):
+  # Compute the convolution product
+  res = torch.sp.convolve(A, b, option)
+
+  obj.assertEqual(res.shape(), reference.shape())
+  for i in range(res.extent(0)):
+    for j in range(res.extent(1)):
+      obj.assertTrue(compare(res[i,j], reference[i,j], eps))
 
 
 ##################### Unit Tests ##################  
@@ -112,6 +189,30 @@ class ConvolutionTest(unittest.TestCase):
 
   def test_convolution_1D_10_5_V(self):
     test_convolve_1D( A10, b1_5, RES_A1_10_b1_5_valid, self, 2)
+
+  def test_convolution_2D_5_2_n(self):
+    test_convolve_2D_nopt( A2_5, b2_2, RES_A2_5_b2_2_full, self)
+
+  def test_convolution_2D_5_2_F(self):
+    test_convolve_2D( A2_5, b2_2, RES_A2_5_b2_2_full, self, 0)
+
+  def test_convolution_2D_5_2_S(self):
+    test_convolve_2D( A2_5, b2_2, RES_A2_5_b2_2_same, self, 1)
+
+  def test_convolution_2D_5_2_V(self):
+    test_convolve_2D( A2_5, b2_2, RES_A2_5_b2_2_valid, self, 2)
+
+  def test_convolution_2D_5_3_n(self):
+    test_convolve_2D_nopt( A2_5, b2_3, RES_A2_5_b2_3_full, self)
+
+  def test_convolution_2D_5_3_F(self):
+    test_convolve_2D( A2_5, b2_3, RES_A2_5_b2_3_full, self, 0)
+
+  def test_convolution_2D_5_3_S(self):
+    test_convolve_2D( A2_5, b2_3, RES_A2_5_b2_3_same, self, 1)
+
+  def test_convolution_2D_5_3_V(self):
+    test_convolve_2D( A2_5, b2_3, RES_A2_5_b2_3_valid, self, 2)
 
 
 ##################### Main ##################  
