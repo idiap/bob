0c19e66e9a4944871ff9363f89e8aa17227b248c
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2011-01-30 20:24:52 +0100

    Add complex_cast tests

--- src/cxx/core/test/blitzArray.cc
+++ src/cxx/core/test/blitzArray.cc
@@ -13,9 +13,12 @@
 #include <blitz/array.h>
 #include <stdint.h>
 
+#include "core/logging.h"
+
 
 struct T {
-  T() {}
+  double eps;
+  T():eps(1e-6) {}
   ~T() {}
 };
 
@@ -67,8 +70,55 @@ void checkBlitzAllocation( const int n_megabytes ) {
 #endif
 }
 
+template <typename T>
+void checkBlitzEqual( const blitz::Array<T,1> a, const blitz::Array<T,1> b ) {
+  BOOST_REQUIRE_EQUAL( a.extent(0), b.extent(0) );
+  for(int i=0; i<a.extent(0); ++i)
+    BOOST_CHECK_EQUAL( a(i+a.lbound(0)), b(i+b.lbound(0)) );
+}
+
+template <typename T>
+void checkBlitzEqual( const blitz::Array<T,2> a, const blitz::Array<T,2> b ) {
+  BOOST_REQUIRE_EQUAL( a.extent(0), b.extent(0) );
+  BOOST_REQUIRE_EQUAL( a.extent(1), b.extent(1) );
+  for(int i=0; i<a.extent(0); ++i)
+    for(int j=0; j<a.extent(1); ++j)
+      BOOST_CHECK_EQUAL( a(i+a.lbound(0),j+a.lbound(1)), 
+        b(i+b.lbound(0),j+b.lbound(1)) );
+}
+
+template <typename T>
+void checkBlitzEqual( const blitz::Array<T,3> a, const blitz::Array<T,3> b ) {
+  BOOST_REQUIRE_EQUAL( a.extent(0), b.extent(0) );
+  BOOST_REQUIRE_EQUAL( a.extent(1), b.extent(1) );
+  BOOST_REQUIRE_EQUAL( a.extent(2), b.extent(2) );
+  for(int i=0; i<a.extent(0); ++i)
+    for(int j=0; j<a.extent(1); ++j)
+      for(int k=0; k<a.extent(2); ++k)
+        BOOST_CHECK_EQUAL( a(i+a.lbound(0),j+a.lbound(1),k+a.lbound(2)), 
+          b(i+b.lbound(0),j+b.lbound(1),k+b.lbound(2)) );
+}
+
+template <typename T>
+void checkBlitzEqual( const blitz::Array<T,4> a, const blitz::Array<T,4> b ) {
+  BOOST_REQUIRE_EQUAL( a.extent(0), b.extent(0) );
+  BOOST_REQUIRE_EQUAL( a.extent(1), b.extent(1) );
+  BOOST_REQUIRE_EQUAL( a.extent(2), b.extent(2) );
+  BOOST_REQUIRE_EQUAL( a.extent(3), b.extent(3) );
+  for(int i=0; i<a.extent(0); ++i)
+    for(int j=0; j<a.extent(1); ++j)
+      for(int k=0; k<a.extent(2); ++k)
+        for(int l=0; l<a.extent(3); ++l)
+          BOOST_CHECK_EQUAL( 
+            a(i+a.lbound(0),j+a.lbound(1),k+a.lbound(2),l+a.lbound(3)), 
+            b(i+b.lbound(0),j+b.lbound(1),k+b.lbound(2),l+b.lbound(3)) );
+}
+
+
+
 BOOST_FIXTURE_TEST_SUITE( test_setup, T )
 
+/*************************** ALLOCATION TESTS ******************************/
 BOOST_AUTO_TEST_CASE( test_blitz_array_512MB )
 {
   checkBlitzAllocation( 512 );
@@ -97,6 +147,168 @@ BOOST_AUTO_TEST_CASE( test_blitz_array_2560MB )
 BOOST_AUTO_TEST_CASE( test_blitz_array_3072MB )
 {
   checkBlitzAllocation( 3072 );
+}
+
+
+/************************** COMPLEX_CAST TESTS *****************************/
+BOOST_AUTO_TEST_CASE( test_blitz_array_cast_simple_1D )
+{
+  blitz::Array<int32_t,1> X(4);
+  for(int i=0; i<4; ++i)
+    X(i) = i+1;
+
+  blitz::Array<double,1> Y(4), ref(4); 
+  blitz::complex_cast<double>(X, Y);
+
+  for(int i=0; i<4; ++i)
+    ref(i) = static_cast<double>(i+1);
+
+  checkBlitzEqual( Y, ref);
+}
+
+BOOST_AUTO_TEST_CASE( test_blitz_array_cast_tocomplex_1D )
+{
+  blitz::Array<int32_t,1> X(4);
+  for(int i=0; i<4; ++i)
+    X(i) = i+1;
+
+  blitz::Array<std::complex<double>,1> Y(4), ref(4); 
+  blitz::complex_cast<std::complex<double> >(X, Y);
+
+  for(int i=0; i<4; ++i)
+    ref(i) = std::complex<double>(i+1,0);
+
+  checkBlitzEqual( Y, ref);
+}
+
+BOOST_AUTO_TEST_CASE( test_blitz_array_cast_tocomplex_2D )
+{
+  blitz::Array<int32_t,2> X(4,4);
+  for(int i=0; i<4; ++i)
+    for(int j=0; j<4; ++j)
+      X(i,j) = i+j+1;
+
+  blitz::Array<std::complex<double>,2> Y(4,4), ref(4,4); 
+  blitz::complex_cast<std::complex<double> >(X, Y);
+
+  for(int i=0; i<4; ++i)
+    for(int j=0; j<4; ++j)
+      ref(i,j) = std::complex<double>(i+j+1,0);
+
+  checkBlitzEqual( Y, ref);
+}
+
+BOOST_AUTO_TEST_CASE( test_blitz_array_cast_tocomplex_3D )
+{
+  blitz::Array<int32_t,3> X(4,4,4);
+  for(int i=0; i<4; ++i)
+    for(int j=0; j<4; ++j)
+      for(int k=0; k<4; ++k)
+        X(i,j,k) = i+j+k+1;
+
+  blitz::Array<std::complex<double>,3> Y(4,4,4), ref(4,4,4); 
+  blitz::complex_cast<std::complex<double> >(X, Y);
+
+  for(int i=0; i<4; ++i)
+    for(int j=0; j<4; ++j)
+      for(int k=0; k<4; ++k)
+        ref(i,j,k) = std::complex<double>(i+j+k+1,0);
+
+  checkBlitzEqual( Y, ref);
+}
+
+BOOST_AUTO_TEST_CASE( test_blitz_array_cast_tocomplex_4D )
+{
+  blitz::Array<int32_t,4> X(4,4,4,4);
+  for(int i=0; i<4; ++i)
+    for(int j=0; j<4; ++j)
+      for(int k=0; k<4; ++k)
+        for(int l=0; l<4; ++l)
+          X(i,j,k,l) = i+j+k+l+1;
+
+  blitz::Array<std::complex<double>,4> Y(4,4,4,4), ref(4,4,4,4); 
+  blitz::complex_cast<std::complex<double> >(X, Y);
+
+  for(int i=0; i<4; ++i)
+    for(int j=0; j<4; ++j)
+      for(int k=0; k<4; ++k)
+        for(int l=0; l<4; ++l)
+          ref(i,j,k,l) = std::complex<double>(i+j+k+l+1,0);
+
+  checkBlitzEqual( Y, ref);
+}
+
+
+BOOST_AUTO_TEST_CASE( test_blitz_array_cast_fromcomplex_1D )
+{
+  blitz::Array<std::complex<double>,1> X(4);
+  for(int i=0; i<4; ++i)
+    X(i) = std::complex<double>(i+1,i);
+
+  blitz::Array<int32_t,1> Y(4), ref(4); 
+  blitz::complex_cast<int32_t >(X, Y);
+
+  for(int i=0; i<4; ++i)
+    ref(i) = i+1;
+
+  checkBlitzEqual( Y, ref);
+}
+
+BOOST_AUTO_TEST_CASE( test_blitz_array_cast_fromcomplex_2D )
+{
+  blitz::Array<std::complex<double>,2> X(4,4);
+  for(int i=0; i<4; ++i)
+    for(int j=0; j<4; ++j)
+      X(i,j) = std::complex<double>(i+j+1,i*j);
+
+  blitz::Array<int32_t,2> Y(4,4), ref(4,4); 
+  blitz::complex_cast<int32_t >(X, Y);
+
+  for(int i=0; i<4; ++i)
+    for(int j=0; j<4; ++j)
+      ref(i,j) = i+j+1;
+
+  checkBlitzEqual( Y, ref);
+}
+
+BOOST_AUTO_TEST_CASE( test_blitz_array_cast_fromcomplex_3D )
+{
+  blitz::Array<std::complex<double>,3> X(4,4,4);
+  for(int i=0; i<4; ++i)
+    for(int j=0; j<4; ++j)
+      for(int k=0; k<4; ++k)
+        X(i,j,k) = std::complex<double>(i+j+k+1,i*j*k);
+
+  blitz::Array<int32_t,3> Y(4,4,4), ref(4,4,4); 
+  blitz::complex_cast<int32_t >(X, Y);
+
+  for(int i=0; i<4; ++i)
+    for(int j=0; j<4; ++j)
+      for(int k=0; k<4; ++k)
+        ref(i,j,k) = i+j+k+1;
+
+  checkBlitzEqual( Y, ref);
+}
+
+BOOST_AUTO_TEST_CASE( test_blitz_array_cast_fromcomplex_4D )
+{
+  blitz::Array<std::complex<double>,4> X(4,4,4,4);
+  for(int i=0; i<4; ++i)
+    for(int j=0; j<4; ++j)
+      for(int k=0; k<4; ++k)
+        for(int l=0; l<4; ++l)
+          X(i,j,k,l) = std::complex<double>(i+j+k+l+1,i*j*k*l);
+
+  blitz::Array<int32_t,4> Y(4,4,4,4), ref(4,4,4,4); 
+  blitz::complex_cast<int32_t >(X, Y);
+
+  for(int i=0; i<4; ++i)
+    for(int j=0; j<4; ++j)
+      for(int k=0; k<4; ++k)
+        for(int l=0; l<4; ++l)
+          ref(i,j,k,l) = i+j+k+l+1;
+
+  checkBlitzEqual( Y, ref);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
