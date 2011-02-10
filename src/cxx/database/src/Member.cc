7968234f5fd5aeb16aafd02320bf576e3860893e
Andre Anjos <andre.anjos@idiap.ch>
2011-02-10 17:58:45 +0100

    Enable Member.cc compilation

--- src/cxx/database/src/Member.cc
+++ src/cxx/database/src/Member.cc
@@ -1,16 +1,20 @@
 /**
- * @file src/Member.cc
+ * @file database/src/Member.cc
  * @author <a href="mailto:andre.anjos@idiap.ch">Andre Anjos</a> 
  *
  * @brief  
  */
 
 #include "database/Member.h"
+#include "database/dataset_common.h"
 
-Member::Member(): 
-  m_array_id(0), m_arrayset_id(0) { }
+namespace db = Torch::database;
 
-Member::~Member() {
-  TDEBUG3("Member destructor (id: " << getArrayId() << "-" << 
-    getArraysetId() << ")");
+db::Member::Member(size_t arrayset_id, size_t array_id) :
+  m_arrayset_id(arrayset_id),
+  m_array_id(array_id)
+{
+  if (!m_arrayset_id) throw db::IndexError();
 }
+
+db::Member::~Member() { }
