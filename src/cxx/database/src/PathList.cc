aed7b744dc99af7d540984496a89b8357b4e7f59
Andre Anjos <andre.anjos@idiap.ch>
2011-03-01 20:50:05 +0100

    Fixes the PathList so resolutions work as intended in ticket:62

--- src/cxx/database/src/PathList.cc
+++ src/cxx/database/src/PathList.cc
@@ -60,25 +60,66 @@ db::PathList& db::PathList::existing() {
   return *this;
 }
 
+/**
+ * Removes the last component from the path, supposing it is complete. If it is
+ * only root_path(), just return it.
+ */
+static fs::path trim_one(const fs::path& p) {
+  if (p == p.root_path()) return p;
+
+  fs::path retval;
+  for (fs::path::iterator it = p.begin(); it!=p.end(); ++it) {
+    fs::path::iterator next = it;
+    ++next; //< for the lack of better support in boost::filesystem V2
+    if (next == p.end()) break; //< == skip the last bit
+    retval /= *it;
+  }
+  return retval;
+}
+
+/**
+ * We wrote this method because we are using boost::filesystem v2 and the name
+ * resolution in this version sucks. If you find yourself maintaining this
+ * method, just re-think about using boost::filesystem::absolute, if v3 is
+ * already available.
+ */
+static fs::path absolute(const fs::path& p) {
+  fs::path completed = fs::complete(p, fs::current_path());
+  fs::path retval;
+  for (fs::path::iterator it = completed.begin(); it != completed.end(); ++it) {
+    if (*it == "..") {
+      retval = trim_one(retval); 
+      continue;
+    }
+    if (*it == ".") { //ignore '.'
+      continue;
+    }
+    retval /= *it;
+  }
+  return retval;
+}
+
 fs::path db::PathList::locate(const fs::path& path) const {
   if (path.is_complete()) return path; //can only locate relative paths
   for (std::list<fs::path>::const_iterator 
       it=m_list.begin(); it!=m_list.end(); ++it) {
-    if (fs::exists(*it / path)) return fs::complete(*it / path);
+    if (fs::exists(*it / path)) return absolute(*it / path);
   }
   return fs::path(); //emtpy
 }
 
 static bool starts_with(const fs::path& input, const fs::path& path) {
-  return (input.string().find(fs::complete(path).string()) == 0);
+  return (input.string().find(path.string()) == 0);
 }
 
 fs::path db::PathList::reduce(const fs::path& input) const {
   if (!input.is_complete()) return input; //can only reduce absolute paths
+  fs::path abs_input = absolute(input);
   const fs::path* best_match = 0; //holds the best match so far
   for (std::list<fs::path>::const_iterator 
       it=m_list.begin(); it!=m_list.end(); ++it) {
-    if (starts_with(input, *it)) {
+    fs::path abs_path = absolute(*it);
+    if (starts_with(abs_input, abs_path)) {
       if (best_match) {
         if (it->string().size() > best_match->string().size()) best_match = &(*it);
         //otherwise, we prefer the first entry in the path list, naturally
@@ -92,5 +133,12 @@ fs::path db::PathList::reduce(const fs::path& input) const {
   if (!best_match) return input; //no match found
   
   //if you get to this point, you have found a match, return "input-best_match"
-  return input.string().substr(fs::complete(*best_match).string().size()+1);
+  size_t psize = absolute(*best_match).string().size();
+  if (absolute(*best_match) != abs_input.root_path()) {
+    //if we are anywhere but in the root directory, we need to remove the extra
+    //slash after the path name being clipped.
+    psize += 1;
+  }
+  const std::string& istr = abs_input.string();
+  return istr.substr(psize, istr.size()-psize);
 }
--- src/python/database/test/test_pathlist.py
+++ src/python/database/test/test_pathlist.py
@@ -73,6 +73,61 @@ class PathListTest(unittest.TestCase):
 
     self.assertEqual(relative, 'ls')
 
+  def test04_resolution(self):
+
+    # The paths in the PathList are not resolved until they are neded. Suppose
+    # for example I put '.' in the PathList, then if I switch to '/bin', I
+    # should be able to locate 'ls'. The use of locate() or reduce() will use
+    # always absolute paths
+    pl = torch.database.PathList('.')
+
+    # Make sure '.' is in there
+    self.assertTrue('.' in pl)
+
+    os.chdir('/bin')
+    
+    # Now I can resolve ls, since I have '.' in PathList and ls is in /bin,
+    # please note that the locate() method always returns full paths, even if
+    # it contains relative paths inside.
+    self.assertEqual(pl.locate('ls'), '/bin/ls')
+
+    # I can also reduce the path while in there
+    self.assertEqual(pl.reduce('/bin/ls'), 'ls')
+
+    # Using relative paths should get you correctly resolved
+    self.assertEqual(pl.locate('./ls'), '/bin/ls')
+    self.assertEqual(pl.locate('../bin/ls'), '/bin/ls')
+    
+    # If you use '..', we ignore anything that goes beyond root
+    self.assertEqual(pl.locate('../../../../../bin/ls'), '/bin/ls')
+
+    # You can use combinations of '.' and '..' as you wish
+    self.assertEqual(pl.locate('.././././.././../bin/ls'), '/bin/ls')
+
+    # The same as for reducing
+    self.assertEqual(pl.reduce('/bin/../bin/ls'), 'ls')
+    self.assertEqual(pl.reduce('/bin/.././../bin/ls'), 'ls')
+
+    # Reduction returns the input when it cannot resolve the name
+    self.assertEqual(pl.reduce('/does/not/exist'), '/does/not/exist')
+    self.assertEqual(pl.reduce('../test/blaBla'), '../test/blaBla')
+    self.assertEqual(pl.reduce('../../../test/blaBla'), '../../../test/blaBla')
+
+    # If I move to another directory that does not contain 'ls', the
+    # resolution stops working. This works as designed. It is important that
+    # locate() always returns an existing path, while reduce() leaves alone
+    # paths it cannot resolve for some reason.
+    os.chdir('/')
+    self.assertEqual(pl.locate('ls'), '')
+
+    # localisation will work again if I specify a bit more on the path
+    self.assertEqual(pl.locate('bin/ls'), '/bin/ls')
+    self.assertEqual(pl.locate('bin/../bin/.././bin/ls'), '/bin/ls')
+
+    # reduction will also work as expected, even if that means only removing
+    # the front slash
+    self.assertEqual(pl.reduce('/bin/ls'), 'bin/ls')
+
 if __name__ == '__main__':
   sys.argv.append('-v')
   if os.environ.has_key('TORCH_PROFILE') and \
