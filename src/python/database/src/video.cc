f7aa6484e4075ab4f1a322d74992d5aaa80ea433
Andre Anjos <andre.anjos@idiap.ch>
2011-03-28 13:39:30 +0200

    Copies const reference to strings to please boost::python

--- src/python/database/src/video.cc
+++ src/python/database/src/video.cc
@@ -60,16 +60,16 @@ void bind_database_video() {
 
   class_<db::VideoReader, boost::shared_ptr<db::VideoReader> >("VideoReader",
       "VideoReader objects can read data from video files. The current implementation uses FFMPEG which is a stable freely available implementation for these tasks. You can read an entire video in memory by using the 'load()' method or use video iterators to read frame-by-frame and avoid overloading your machine's memory. The maximum precision FFMPEG will output is a 24-bit (8-bit per band) representation of each pixel (32-bit with transparency when supported by Torch, which is not the case presently). So, the input of data using this class uses uint8_t as base element type. Output will be colored using the RGB standard, with each band varying between 0 and 255, with zero meaning pure black and 255, pure white (color).", init<const std::string&>((arg("filename")), "Initializes a new VideoReader object by giving the input file name to read"))
-    .add_property("filename", make_function(&db::VideoReader::filename, return_internal_reference<>()))
+    .add_property("filename", make_function(&db::VideoReader::filename, return_value_policy<copy_const_reference>()))
     .add_property("height", &db::VideoReader::height)
     .add_property("width", &db::VideoReader::width)
     .add_property("numberOfFrames", &db::VideoReader::numberOfFrames)
     .def("__len__", &db::VideoReader::numberOfFrames)
     .add_property("duration", &db::VideoReader::duration)
-    .add_property("codecName", make_function(&db::VideoReader::codecName, return_internal_reference<>()))
-    .add_property("codecLongName", make_function(&db::VideoReader::codecLongName, return_internal_reference<>()))
+    .add_property("codecName", make_function(&db::VideoReader::codecName, return_value_policy<copy_const_reference>()))
+    .add_property("codecLongName", make_function(&db::VideoReader::codecLongName, return_value_policy<copy_const_reference>()))
     .add_property("frameRate", &db::VideoWriter::frameRate)
-    .add_property("info", make_function(&db::VideoReader::info, return_internal_reference<>()))
+    .add_property("info", make_function(&db::VideoReader::info, return_value_policy<copy_const_reference>()))
     .def("load", &db::VideoReader::load, (arg("array")), "Loads all of the video stream in a blitz array organized in this way: (frames, color-bands, height, width). The 'data' parameter will be resized if required.")
     .def("__iter__", &db::VideoReader::begin)
     ;
@@ -77,14 +77,14 @@ void bind_database_video() {
   class_<db::VideoWriter, boost::shared_ptr<db::VideoWriter> >("VideoWriter",
      "Use objects of this class to create and write video files using FFMPEG.",
      init<const std::string&, size_t, size_t, optional<float, float, size_t> >((arg("filename"), arg("height"), arg("width"), arg("bitrate")=1500000, arg("framerate")=25, arg("gop")=12), "Creates a new output file given the input parameters. The codec to be used will be derived from the filename extension."))
-    .add_property("filename", make_function(&db::VideoWriter::filename, return_internal_reference<>()))
+    .add_property("filename", make_function(&db::VideoWriter::filename, return_value_policy<copy_const_reference>()))
     .add_property("height", &db::VideoWriter::height)
     .add_property("width", &db::VideoWriter::width)
     .add_property("numberOfFrames", &db::VideoWriter::numberOfFrames)
     .def("__len__", &db::VideoWriter::numberOfFrames)
     .add_property("duration", &db::VideoWriter::duration)
-    .add_property("codecName", make_function(&db::VideoWriter::codecName, return_internal_reference<>()))
-    .add_property("codecLongName", make_function(&db::VideoWriter::codecLongName, return_internal_reference<>()))
+    .add_property("codecName", make_function(&db::VideoWriter::codecName, return_value_policy<copy_const_reference>()))
+    .add_property("codecLongName", make_function(&db::VideoWriter::codecLongName, return_value_policy<copy_const_reference>()))
     .add_property("frameRate", &db::VideoWriter::frameRate)
     .add_property("bitRate", &db::VideoWriter::bitRate)
     .add_property("gop", &db::VideoWriter::gop)
