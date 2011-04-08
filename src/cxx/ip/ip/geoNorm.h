7da550757ca800ef4d6c0e19f766625eefa4a818
Niklas Johansson <niklas.johansson@idiap.ch>
2011-04-08 17:19:08 +0200

    Added geonorm

--- src/cxx/ip/ip/geoNorm.h
+++ src/cxx/ip/ip/geoNorm.h
@@ -13,6 +13,8 @@
 #include "core/logging.h"
 #include "ip/Exception.h"
 #include "core/common.h"
+#include "ip/rotate.h"
+#include "ip/scale.h"
 
 namespace Torch {
 /**
@@ -25,18 +27,18 @@ namespace Torch {
     template<typename T>
     void geoNorm(const blitz::Array<T,2>& src, blitz::Array<T,2>& dst, 
 		 const int left_h, const int left_w,
-		 const int right_h, const int right_h,
+		 const int right_h, const int right_w,
 		 const int target_distance_between_left_and_right,
 		 const int final_height, const int final_width)
     {
 	    //shift to center
-	    blitz::Array<T,2> dst_cnt = sameAs(src);
-	    torch::ip::shiftToCenterOfPoints(src, dst_cnt, left_h, left_w, right_h, right_w);
+	    blitz::Array<T,2> dst_cnt(src.shape());
+	    shiftToCenterOfPoints(src, dst_cnt, left_h, left_w, right_h, right_w);
 	    
 	    // rotate
-	    double angle = torch::ip::getRotateAngleToLevelOutHorizontal(left_h, left_w, right_h, right_w);
-	    blitz::Array<T,2> dst_rot(torch::ip::getShapeRotated(dst_cnt, angle));
-	    torch::ip::rotate(dst_cnt, dst_rot, angle);
+	    double angle = getRotateAngleToLevelOutHorizontal(left_h, left_w, right_h, right_w);
+	    blitz::Array<T,2> dst_rot(getShapeRotated(dst_cnt, angle));
+	    rotate(dst_cnt, dst_rot, angle);
 	    
 	    // scale the 2D tensor
 	    // old distance between points vs target distance between points
@@ -49,7 +51,7 @@ namespace Torch {
 	    const int new_width       = floor(src.extent(1) * scale_factor + 0.5);
 	    
 	    blitz::Array<T,2> dst_scl(new_height, new_width);
-	    torch::ip::scale(dst_rot, dst_scl, new_height, new_width);
+	    scale(dst_rot, dst_scl, new_height, new_width);
 
 	    // crop around the center WARNING, TODO find correct formula for the cropping
 	    blitz::Array<T,2> dst_crp(final_height, final_width);
