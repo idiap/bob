7f93acc173a15f21c9a21089bdd11659865306ed
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2011-04-19 15:27:16 +0200

    Add ArrayCodec for former TensorFile

---
+++ src/cxx/database/src/TensorFile.cc
@@ -0,0 +1,261 @@
+/**
+ * @file src/cxx/database/src/TensorFile.cc
+ * @author <a href="mailto:Laurent.El-Shafey@idiap.ch">Laurent El Shafey</a> 
+ *
+ * @brief This class can be used to store and load multiarrays into/from files.
+ */
+
+#include "core/logging.h"
+#include "core/array_type.h"
+
+#include "database/TensorFile.h"
+#include "database/dataset_common.h"
+
+namespace db = Torch::database;
+namespace core = Torch::core;
+
+db::TensorFile::TensorFile(const std::string& filename, db::TensorFile::openmode flag):
+  m_header_init(false),
+  m_current_array(0),
+  m_n_arrays_written(0),
+  m_openmode(flag)
+{
+  if((flag & db::TensorFile::out) && (flag & db::TensorFile::in)) {
+    m_stream.open(filename.c_str(), std::ios::in | std::ios::out | 
+        std::ios::binary);
+    if(m_stream)
+    {
+      m_header.read(m_stream);
+      m_header_init = true;
+      m_n_arrays_written = m_header.m_n_samples;
+
+      if (flag & db::TensorFile::append) {
+        m_stream.seekp(0, std::ios::end);
+        m_current_array = m_header.m_n_samples;
+      }
+    }
+  }
+  else if(flag & db::TensorFile::out) {  
+    if(m_stream && (flag & db::TensorFile::append)) {
+      m_stream.open(filename.c_str(), std::ios::out | std::ios::in);
+      m_header.read(m_stream);
+      m_header_init = true;
+      m_n_arrays_written = m_header.m_n_samples;
+      m_stream.seekp(0, std::ios::end);
+      m_current_array = m_header.m_n_samples;
+    }
+    else
+      m_stream.open(filename.c_str(), std::ios::out | std::ios::binary);
+  }
+  else if(flag & db::TensorFile::in) {
+    m_stream.open(filename.c_str(), std::ios::in | std::ios::binary);
+    if(m_stream) {
+      m_header.read(m_stream);
+      m_header_init = true;
+      m_n_arrays_written = m_header.m_n_samples;
+
+      if (flag & db::TensorFile::append) {
+        core::error << "Cannot append data in read only mode." << std::endl;
+        throw core::Exception();
+      }
+    }
+  }
+  else
+  {
+    core::error << "Invalid combination of flags." << std::endl;
+    throw core::Exception();
+  }
+}
+
+db::TensorFile::~TensorFile() {
+  close();
+}
+
+void db::TensorFile::close() {
+  // Rewrite the header and update the number of samples
+  m_header.m_n_samples = m_n_arrays_written;
+  if(m_openmode & db::TensorFile::out) m_header.write(m_stream);
+
+  m_stream.close();
+}
+
+void db::TensorFile::initHeader(const Torch::core::array::ElementType type, 
+    size_t ndim, const size_t* shape) {
+  // Check that data have not already been written
+  if (m_n_arrays_written > 0 ) {
+    Torch::core::error << "Cannot init the header of an output stream in which data" <<
+      " have already been written." << std::endl;
+    throw Torch::core::Exception();
+  }
+
+  // Initialize header
+  m_header.m_elem_type = type;
+  m_header.m_tensor_type = db::arrayTypeToTensorType(type);
+  m_header.setShape(ndim, shape);
+  m_header.write(m_stream);
+  m_header_init = true;
+}
+
+template <typename T>
+void write_inlined(const db::detail::InlinedArrayImpl& data, std::ostream& s) {
+
+  switch(data.getNDim()) {
+    case 1:
+      {
+        const blitz::Array<T,1>& bz = data.get<T,1>();
+        for (int i=0; i<(int)data.getShape()[0]; ++i) {
+          T val = bz(i);
+          s.write((const char*)&val, core::array::getElementSize<T>());
+        }
+        break;
+      }
+    case 2:
+      {
+        const blitz::Array<T,2>& bz = data.get<T,2>();
+        for (int i=0; i<(int)data.getShape()[0]; ++i)
+          for (int j=0; j<(int)data.getShape()[1]; ++j) {
+            T val = bz(i,j);
+            s.write((const char*)&val, core::array::getElementSize<T>());
+          }
+        break;
+      }
+    case 3:
+      {
+        const blitz::Array<T,3>& bz = data.get<T,3>();
+        for (int i=0; i<(int)data.getShape()[0]; ++i)
+          for (int j=0; j<(int)data.getShape()[1]; ++j)
+            for (int k=0; k<(int)data.getShape()[2]; ++k) {
+              T val = bz(i,j,k);
+              s.write((const char*)&val, core::array::getElementSize<T>());
+            }
+        break;
+      }
+    case 4:
+      {
+        const blitz::Array<T,4>& bz = data.get<T,4>();
+        for (int i=0; i<(int)data.getShape()[0]; ++i)
+          for (int j=0; j<(int)data.getShape()[1]; ++j)
+            for (int k=0; k<(int)data.getShape()[2]; ++k)
+              for (int l=0; l<(int)data.getShape()[3]; ++l) {
+                T val = bz(i,j,k,l);
+                s.write((const char*)&val, core::array::getElementSize<T>());
+              }
+        break;
+      }
+    default:
+      throw db::DimensionError(data.getNDim(), Torch::core::array::N_MAX_DIMENSIONS_ARRAY);
+  }
+}
+
+void db::TensorFile::write(const db::detail::InlinedArrayImpl& data) {
+  if(!m_header_init) {
+    //initializes the header
+    initHeader(data.getElementType(), data.getNDim(), data.getShape());
+  }
+  else {
+      //checks compatibility with previously written stuff
+      if (data.getNDim() != m_header.getNDim()) throw DimensionError(data.getNDim(), m_header.getNDim());
+      const size_t* p_shape = data.getShape();
+      const size_t* h_shape = m_header.getShape();
+      for (size_t i=0; i<data.getNDim(); ++i)
+        if(p_shape[i] != h_shape[i]) throw DimensionError(p_shape[i], h_shape[i]);
+  }
+
+  // copy the data into the output stream
+  switch(data.getElementType()) {
+    case Torch::core::array::t_bool: write_inlined<bool>(data, m_stream); break;
+    case Torch::core::array::t_int8: write_inlined<int8_t>(data, m_stream); break;
+    case Torch::core::array::t_int16: write_inlined<int16_t>(data, m_stream); break;
+    case Torch::core::array::t_int32: write_inlined<int32_t>(data, m_stream); break;
+    case Torch::core::array::t_int64: write_inlined<int64_t>(data, m_stream); break;
+    case Torch::core::array::t_uint8: write_inlined<uint8_t>(data, m_stream); break;
+    case Torch::core::array::t_uint16: write_inlined<uint16_t>(data, m_stream); break;
+    case Torch::core::array::t_uint32: write_inlined<uint32_t>(data, m_stream); break;
+    case Torch::core::array::t_uint64: write_inlined<uint64_t>(data, m_stream); break;
+    case Torch::core::array::t_float32: write_inlined<float>(data, m_stream); break;
+    case Torch::core::array::t_float64: write_inlined<double>(data, m_stream); break;
+    case Torch::core::array::t_float128: write_inlined<long double>(data, m_stream); break;
+    case Torch::core::array::t_complex64: write_inlined<std::complex<float> >(data, m_stream); break;
+    case Torch::core::array::t_complex128: write_inlined<std::complex<double> >(data, m_stream); break;
+    case Torch::core::array::t_complex256: write_inlined<std::complex<long double> >(data, m_stream); break;
+    default: throw TypeError(data.getElementType(), Torch::core::array::t_unknown);
+  }
+
+  // increment m_n_arrays_written and m_current_array
+  ++m_current_array;
+  if (m_current_array>m_n_arrays_written) ++m_n_arrays_written;
+}
+
+template <typename T>
+db::detail::InlinedArrayImpl read_inlined(size_t ndim, const size_t* shape,
+    std::istream& s) {
+  switch(ndim) {
+    case 1:
+      {
+        blitz::Array<T,1> bz(shape[0]);
+        s.read(reinterpret_cast<char*>(bz.data()), shape[0]*sizeof(T));
+        return bz;
+      }
+    case 2:
+      {
+        //arrays are always stored in C-style ordering
+        blitz::Array<T,2> bz(shape[0], shape[1]);
+        s.read(reinterpret_cast<char*>(bz.data()), shape[0]*shape[1]*sizeof(T));
+        return bz;
+      }
+    case 3:
+      {
+        //arrays are always stored in C-style ordering
+        blitz::Array<T,3> bz(shape[0], shape[1], shape[2]);
+        s.read(reinterpret_cast<char*>(bz.data()), shape[0]*shape[1]*shape[2]*sizeof(T));
+        return bz;
+      }
+    case 4:
+      {
+        //arrays are always stored in C-style ordering
+        blitz::Array<T,4> bz(shape[0], shape[1], shape[2], shape[3]);
+        s.read(reinterpret_cast<char*>(bz.data()), shape[0]*shape[1]*shape[2]*shape[3]*sizeof(T));
+        return bz;
+      }
+    default:
+      throw db::DimensionError(ndim, Torch::core::array::N_MAX_DIMENSIONS_ARRAY);
+  }
+}
+
+db::detail::InlinedArrayImpl db::TensorFile::read() {
+  if(!m_header_init) throw Uninitialized();
+  switch(getElementType()) {
+    case Torch::core::array::t_bool: return read_inlined<bool>(m_header.getNDim(), m_header.getShape(), m_stream);
+    case Torch::core::array::t_int8: return read_inlined<int8_t>(m_header.getNDim(), m_header.getShape(), m_stream);
+    case Torch::core::array::t_int16: return read_inlined<int16_t>(m_header.getNDim(), m_header.getShape(), m_stream);
+    case Torch::core::array::t_int32: return read_inlined<int32_t>(m_header.getNDim(), m_header.getShape(), m_stream);
+    case Torch::core::array::t_int64: return read_inlined<int64_t>(m_header.getNDim(), m_header.getShape(), m_stream);
+    case Torch::core::array::t_uint8: return read_inlined<uint8_t>(m_header.getNDim(), m_header.getShape(), m_stream);
+    case Torch::core::array::t_uint16: return read_inlined<uint16_t>(m_header.getNDim(), m_header.getShape(), m_stream);
+    case Torch::core::array::t_uint32: return read_inlined<uint32_t>(m_header.getNDim(), m_header.getShape(), m_stream);
+    case Torch::core::array::t_uint64: return read_inlined<uint64_t>(m_header.getNDim(), m_header.getShape(), m_stream);
+    case Torch::core::array::t_float32: return read_inlined<float>(m_header.getNDim(), m_header.getShape(), m_stream);
+    case Torch::core::array::t_float64: return read_inlined<double>(m_header.getNDim(), m_header.getShape(), m_stream);
+    case Torch::core::array::t_float128: return read_inlined<long double>(m_header.getNDim(), m_header.getShape(), m_stream);
+    case Torch::core::array::t_complex64: return read_inlined<std::complex<float> >(m_header.getNDim(), m_header.getShape(), m_stream);
+    case Torch::core::array::t_complex128: return read_inlined<std::complex<double> >(m_header.getNDim(), m_header.getShape(), m_stream);
+    case Torch::core::array::t_complex256: return read_inlined<std::complex<long double> >(m_header.getNDim(), m_header.getShape(), m_stream);
+    default: throw Torch::database::TypeError(getElementType(), Torch::core::array::t_unknown);
+  }
+  ++m_current_array;
+}
+
+db::detail::InlinedArrayImpl db::TensorFile::read (size_t index) {
+  // Check that we are reaching an existing array
+  if( index > m_header.m_n_samples ) {
+    core::error << "Trying to reach a non-existing array." << std::endl;
+    throw IndexError(index);
+  }
+
+  // Set the stream pointer at the correct position
+  m_stream.seekg( m_header.getArrayIndex(index) );
+  m_current_array = index;
+
+  // Put the content of the stream in the blitz array.
+  return read();
+}
---
+++ src/cxx/database/src/TensorFileHeader.cc
@@ -0,0 +1,175 @@
+/**
+ * @file src/cxx/database/database/TensorFileHeader.cc
+ * @author <a href="mailto:Laurent.El-Shafey@idiap.ch">Laurent El Shafey</a> 
+ *
+ * This class defines an header for storing multiarrays into .tensor files.
+ */
+
+#include "core/logging.h"
+
+#include "database/TensorFileHeader.h"
+#include "database/Exception.h"
+
+namespace db = Torch::database;
+namespace dbd = db::detail;
+namespace core = Torch::core;
+
+dbd::TensorFileHeader::TensorFileHeader()
+  : m_tensor_type(db::Char),
+    m_elem_type(core::array::t_unknown), 
+    m_n_samples(0),
+    m_n_dimensions(0), 
+    m_tensor_size(0)
+{
+  for (size_t i=0; i<core::array::N_MAX_DIMENSIONS_ARRAY; ++i) m_shape[i] = 0;
+}
+
+dbd::TensorFileHeader::~TensorFileHeader() { }
+
+size_t dbd::TensorFileHeader::getArrayIndex (size_t index) const {
+  size_t header_size = 7 * sizeof(int);
+  return header_size + index * m_tensor_size;
+}
+
+size_t dbd::TensorFileHeader::getSize(size_t dim_index) const {
+  if(dim_index >= m_n_dimensions) throw db::DimensionError(dim_index, m_n_dimensions);
+  return m_shape[dim_index]; 
+}
+
+void dbd::TensorFileHeader::read(std::istream& str) {
+  // Start reading at the beginning of the stream
+  str.seekg(std::ios_base::beg);
+
+  int val;
+  str.read( reinterpret_cast<char*>(&val), sizeof(int));
+  m_tensor_type = (db::TensorType)val;
+  m_elem_type = db::tensorTypeToArrayType(m_tensor_type);
+  str.read( reinterpret_cast<char*>(&val), sizeof(int));
+  m_n_samples = (size_t)val;
+  str.read( reinterpret_cast<char*>(&val), sizeof(int));
+  m_n_dimensions = (size_t)val;
+  str.read( reinterpret_cast<char*>(&val), sizeof(int));
+  m_shape[0] = (size_t)val;
+  str.read( reinterpret_cast<char*>(&val), sizeof(int));
+  m_shape[1] = (size_t)val;
+  str.read( reinterpret_cast<char*>(&val), sizeof(int));
+  m_shape[2] = (size_t)val;
+  str.read( reinterpret_cast<char*>(&val), sizeof(int));
+  m_shape[3] = (size_t)val;
+
+  if(!header_ok())
+    throw Torch::database::Exception();
+}
+
+void dbd::TensorFileHeader::write(std::ostream& str) const
+{
+  // Start writing at the beginning of the stream
+  str.seekp(std::ios_base::beg);
+
+  int val;
+  val = (int)m_tensor_type;
+  str.write( reinterpret_cast<char*>(&val), sizeof(int));
+  val = (int)m_n_samples;
+  str.write( reinterpret_cast<char*>(&val), sizeof(int));
+  val = (int)m_n_dimensions;
+  str.write( reinterpret_cast<char*>(&val), sizeof(int));
+  val = (int)m_shape[0];
+  str.write( reinterpret_cast<char*>(&val), sizeof(int));
+  val = (int)m_shape[1];
+  str.write( reinterpret_cast<char*>(&val), sizeof(int));
+  val = (int)m_shape[2];
+  str.write( reinterpret_cast<char*>(&val), sizeof(int));
+  val = (int)m_shape[3];
+  str.write( reinterpret_cast<char*>(&val), sizeof(int));
+}
+
+bool dbd::TensorFileHeader::header_ok()
+{
+  // Check the type
+  switch (m_tensor_type)
+  {
+    // supported tensor types
+    case db::Char:
+    case db::Short:
+    case db::Int:
+    case db::Long:
+    case db::Float:
+    case db::Double:
+      break;
+    // error
+    default:
+      return false;
+  }
+
+  // Check the number of samples and dimensions
+  if( m_n_samples < 0 || m_n_dimensions < 1 ||
+      m_n_dimensions > 4)
+    return false;
+
+  // OK
+  update();
+  return true;
+}
+
+void dbd::TensorFileHeader::update()
+{
+  size_t base_size = 0;
+  switch (m_tensor_type)
+  {
+    case db::Char:    base_size = sizeof(char); break;
+    case db::Short:   base_size = sizeof(short); break;
+    case db::Int:     base_size = sizeof(int); break;
+    case db::Long:    base_size = sizeof(long); break;
+    case db::Float:   base_size = sizeof(float); break;
+    case db::Double:  base_size = sizeof(double); break;
+  }
+
+  size_t tsize = 1;
+  for(size_t i = 0; i < m_n_dimensions; ++i)
+    tsize *= m_shape[i];
+
+  m_tensor_size = tsize * base_size;
+}
+
+
+db::TensorType db::arrayTypeToTensorType(Torch::core::array::ElementType eltype)
+{
+  switch(eltype)
+  {
+    case Torch::core::array::t_int8:
+      return db::Char;
+    case Torch::core::array::t_int16:
+      return db::Short;
+    case Torch::core::array::t_int32:
+      return db::Int;
+    case Torch::core::array::t_int64:
+      return db::Long;
+    case Torch::core::array::t_float32:
+      return db::Float;
+    case Torch::core::array::t_float64:
+      return db::Double;
+    default:
+      throw Torch::database::UnsupportedTypeError(Torch::core::array::t_unknown);
+  }
+}
+  
+Torch::core::array::ElementType db::tensorTypeToArrayType(db::TensorType tensortype)
+{
+  switch(tensortype)
+  {
+    case db::Char:
+      return Torch::core::array::t_int8;
+    case db::Short:
+      return Torch::core::array::t_int16;
+    case db::Int:
+      return Torch::core::array::t_int32;
+    case db::Long:
+      return Torch::core::array::t_int64;
+    case db::Float:
+      return Torch::core::array::t_float32;
+    case db::Double:
+      return Torch::core::array::t_float64;
+    default:
+      throw Torch::database::UnsupportedTypeError(Torch::core::array::t_unknown);
+  }
+}
