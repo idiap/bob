40cd4001516196211a35b4c308e7b72e494d4244
Francois Moulin <Francois.Moulin@idiap.ch>
2011-05-18 12:22:41 +0200

    Config: add "cd" function to Configuration

--- src/cxx/config/src/Configuration.cc
+++ src/cxx/config/src/Configuration.cc
@@ -14,7 +14,7 @@ namespace conf = Torch::config;
 namespace bp = boost::python;
 
 conf::Configuration::Configuration(const std::string& s):
-  m_dict()
+  m_current_path("/"), m_dict()
 {
   boost::filesystem::path p(s);
   //based on the extension, choose how to populate internal dictionary
@@ -24,12 +24,12 @@ conf::Configuration::Configuration(const std::string& s):
 }
 
 conf::Configuration::Configuration(): 
-  m_dict()
+  m_current_path("/"), m_dict()
 {
 }
 
 conf::Configuration::Configuration(const conf::Configuration& other):
-  m_dict(other.m_dict)
+  m_current_path("/"), m_dict(other.m_dict)
 {
 }
 
@@ -40,6 +40,7 @@ conf::Configuration::~Configuration ()
 conf::Configuration& conf::Configuration::operator= (const conf::Configuration& other) {
   m_dict = bp::dict();
   m_dict.update(other.m_dict);
+  m_current_path = other.m_current_path;
   return *this;
 }
 
@@ -51,7 +52,49 @@ void conf::Configuration::save (const std::string& s) const {
   else throw conf::NotImplemented("save", p.extension());
 }
 
+// Function adapted from
+// http://stackoverflow.com/questions/1746136/how-do-i-normalize-a-pathname-using-boostfilesystem/1750710#1750710
+static boost::filesystem::path normalize(const boost::filesystem::path& p) {
+  boost::filesystem::path result;
+  for(boost::filesystem::path::iterator it=p.begin(); it!=p.end(); ++it) {
+    if(*it == "..") {
+      result = result.parent_path();
+    }
+    else if(*it == ".") {
+      // Ignore
+    }
+    else {
+      // Just cat other path entries
+      result /= *it;
+    }
+  }
+  
+  return result;
+}
+
 conf::Configuration& conf::Configuration::update (const conf::Configuration& other) {
   m_dict.update(other.m_dict);
   return *this;
+}
+
+void conf::Configuration::cd(std::string path) {
+  if (!path.empty() && path.at(0) == '/') {
+    // Path is absolute
+    m_current_path = normalize(boost::filesystem::path(path));
+  }
+  else {
+    // Path is relative
+    m_current_path = normalize(m_current_path / path);
+  }
+}
+
+std::string conf::Configuration::getAbsolutePath(const std::string path) const {
+  if (!path.empty() && path.at(0) == '/') {
+    // Path is absolute
+    return normalize(boost::filesystem::path(path)).string().substr(1);
+  }
+  else {
+    // Path is relative
+    return normalize(m_current_path / path).string().substr(1);
+  }
 }
