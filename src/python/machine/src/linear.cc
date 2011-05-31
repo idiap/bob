e80f2cfffb7d7a1c8810e8b542eac20427a983b4
Andre Anjos <andre.anjos@idiap.ch>
2011-05-31 13:33:31 +0200

    Framework for LinearMachine python bindings

---
+++ src/python/machine/src/linear.cc
@@ -0,0 +1,15 @@
+/**
+ * @author Andre Anjos <andre.anjos@idiap.ch>
+ * @date Tue 31 May 2011 13:29:08 CEST
+ *
+ * @brief Bindings for a LinearMachine
+ */
+
+#include <boost/python.hpp>
+#include "machine/LinearMachine.h"
+
+namespace bp = boost::python;
+namespace mach = Torch::machine;
+
+void bind_machine_linear() {
+}
--- src/python/machine/src/main.cc
+++ src/python/machine/src/main.cc
@@ -5,6 +5,7 @@ using namespace boost::python;
 void bind_machine_base();
 void bind_machine_exception();
 void bind_machine_eigenmachine();
+void bind_machine_linear();
 
 BOOST_PYTHON_MODULE(libpytorch_machine)
 {
@@ -17,4 +18,5 @@ BOOST_PYTHON_MODULE(libpytorch_machine)
   bind_machine_base();
   bind_machine_exception();
   bind_machine_eigenmachine();
+  bind_machine_linear();
 }
