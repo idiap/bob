892d5facca3a2c7911b41cad7c8ded7211ed4520
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2011-06-27 21:23:05 +0200

    Add python bindings to cgsolve

--- src/python/math/src/linsolve.cc
+++ src/python/math/src/linsolve.cc
@@ -8,11 +8,13 @@
 #include <boost/python.hpp>
 
 #include "math/linsolve.h"
+#include "math/cgsolve.h"
 
 using namespace boost::python;
 
 static const char* LINSOLVE_DOC = "Solve the linear system A*x=b and return the result as a blitz array. The solver is from the LAPACK library.";
 static const char* LINSOLVE_SYMPOS_DOC = "Solve the linear system A*x=b, where A is symmetric definite positive, and return the result as a blitz array. The solver is from the LAPACK library.";
+static const char* CGSOLVE_SYMPOS_DOC = "Solve the linear system A*x=b via conjugate gradients, where A is symmetric definite positive, and return the result as a blitz array.";
 
 static blitz::Array<double,1> script_linsolve(const blitz::Array<double,2>& A, const blitz::Array<double,1>& b)
 {
@@ -28,6 +30,13 @@ static blitz::Array<double,1> script_linsolveSympos(const blitz::Array<double,2>
   return res;
 }
 
+static blitz::Array<double,1> script_cgsolveSympos(const blitz::Array<double,2>& A, const blitz::Array<double,1>& b, const double acc, const int max_iter)
+{
+  blitz::Array<double,1> res(b.extent(0));
+  Torch::math::cgsolveSympos( A, res, b, acc, max_iter);
+  return res;
+}
+
 
 void bind_math_linsolve()
 {
@@ -36,5 +45,7 @@ void bind_math_linsolve()
   def("linsolve", (void (*)(const blitz::Array<double,2>& A, blitz::Array<double,1>& res, const blitz::Array<double,1>& b))&Torch::math::linsolve, (arg("A"),arg("output"),arg("b")), LINSOLVE_DOC);
   def("linsolveSympos", (blitz::Array<double,1> (*)(const blitz::Array<double,2>& A,const blitz::Array<double,1>& b))&script_linsolveSympos, (arg("A"), arg("b")), LINSOLVE_SYMPOS_DOC);
   def("linsolveSympos", (void (*)(const blitz::Array<double,2>& A, blitz::Array<double,1>& res, const blitz::Array<double,1>& b))&Torch::math::linsolveSympos, (arg("A"),arg("output"),arg("b")), LINSOLVE_SYMPOS_DOC);
+  def("cgsolveSympos", (blitz::Array<double,1> (*)(const blitz::Array<double,2>& A,const blitz::Array<double,1>& b, const double acc, const int max_iter))&script_linsolveSympos, (arg("A"), arg("b"), arg("acc"), arg("max_iter")), CGSOLVE_SYMPOS_DOC);
+  def("cgsolveSympos", (void (*)(const blitz::Array<double,2>& A, blitz::Array<double,1>& res, const blitz::Array<double,1>& b, const double acc, const int max_iter))&Torch::math::linsolveSympos, (arg("A"), arg("output"), arg("b"), arg("acc"), arg("max_iter")), CGSOLVE_SYMPOS_DOC);
 }
 
