806e84808932fd13f96845d0d6c9fc067ba9bb9e
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2011-06-27 20:18:20 +0200

    Update python bindings

--- src/python/math/src/linsolve.cc
+++ src/python/math/src/linsolve.cc
@@ -7,17 +7,24 @@
 
 #include <boost/python.hpp>
 
-#include "core/logging.h"
 #include "math/linsolve.h"
 
 using namespace boost::python;
 
 static const char* LINSOLVE_DOC = "Solve the linear system A*x=b and return the result as a blitz array. The solver is from the LAPACK library.";
+static const char* LINSOLVE_SYMPOS_DOC = "Solve the linear system A*x=b, where A is symmetric definite positive, and return the result as a blitz array. The solver is from the LAPACK library.";
 
 static blitz::Array<double,1> script_linsolve(const blitz::Array<double,2>& A, const blitz::Array<double,1>& b)
 {
-  blitz::Array<double,1> res;
+  blitz::Array<double,1> res(b.extent(0));
   Torch::math::linsolve( A, res, b);
+  return res;
+}
+
+static blitz::Array<double,1> script_linsolveSympos(const blitz::Array<double,2>& A, const blitz::Array<double,1>& b)
+{
+  blitz::Array<double,1> res(b.extent(0));
+  Torch::math::linsolveSympos( A, res, b);
   return res;
 }
 
@@ -27,5 +34,7 @@ void bind_math_linsolve()
   // Linear system solver
   def("linsolve", (blitz::Array<double,1> (*)(const blitz::Array<double,2>& A,const blitz::Array<double,1>& b))&script_linsolve, (arg("A"), arg("b")), LINSOLVE_DOC);
   def("linsolve", (void (*)(const blitz::Array<double,2>& A, blitz::Array<double,1>& res, const blitz::Array<double,1>& b))&Torch::math::linsolve, (arg("A"),arg("output"),arg("b")), LINSOLVE_DOC);
+  def("linsolveSympos", (blitz::Array<double,1> (*)(const blitz::Array<double,2>& A,const blitz::Array<double,1>& b))&script_linsolveSympos, (arg("A"), arg("b")), LINSOLVE_SYMPOS_DOC);
+  def("linsolveSympos", (void (*)(const blitz::Array<double,2>& A, blitz::Array<double,1>& res, const blitz::Array<double,1>& b))&Torch::math::linsolveSympos, (arg("A"),arg("output"),arg("b")), LINSOLVE_SYMPOS_DOC);
 }
 
