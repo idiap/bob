e34141ee0600b6ddda4af01ebba1a8d8f58bd811
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2011-07-05 19:53:44 +0200

    Refactor query for BANCA db

--- src/python/db/lib/banca/query.py
+++ src/python/db/lib/banca/query.py
@@ -145,6 +145,134 @@ class Database(object):
     else:
       return q.first().path
 
+  def objects(self, directory=None, extension=None, protocol=None,
+      purposes=None, model_ids=None, groups=None, classes=None, 
+      languages=None):
+    """Returns a set of filenames for the specific query by the user.
+
+    Keyword Parameters:
+
+    directory
+      A directory name that will be prepended to the final filepath returned
+
+    extension
+      A filename extension that will be appended to the final filepath returned
+
+    protocol
+      One of the BANCA protocols ("P", "G", "Mc", "Md", "Ma", "Ud", "Ua").
+
+    purposes
+      The purposes required to be retrieved ("enrol", "probe") or a tuple
+      with several of them. If 'None' is given (this is the default), it is 
+      considered the same as a tuple with all possible values. This field is
+      ignored for the data from the "world" group.
+
+    model_ids
+      Only retrieves the files for the provided list of model ids (claimed 
+      client id).  If 'None' is given (this is the default), no filter over 
+      the model_ids is performed.
+
+    groups
+      One of the groups ("g1", "g2", "world") or a tuple with several of them. 
+      If 'None' is given (this is the default), it is considered the same as a 
+      tuple with all possible values.
+      Note that 'dev' is an alias to 'g1' and 'test' an alias to 'g2'
+
+    classes
+      The classes (types of accesses) to be retrieved ('client', 'impostor') 
+      or a tuple with several of them. If 'None' is given (this is the 
+      default), it is considered the same as a tuple with all possible values.
+
+    languages
+      The language spoken by the clients ("en")
+      TODO: only English is currently supported
+      If 'None' is given (this is the default), it is considered the same as a 
+      tuple with all possible values.
+
+    Returns: A dictionary containing:
+      - 0: the resolved filenames 
+      - 1: the model id
+      - 2: the claimed id attached to the model
+      - 3: the real id
+      - 4: the "stem" path (basename of the file)
+    considering allthe filtering criteria. The keys of the dictionary are 
+    unique identities for each file in the BANCA database. Conserve these 
+    numbers if you wish to save processing results later on.
+    """
+
+    def make_path(stem, directory, extension):
+      import os
+      if not extension: extension = ''
+      if directory: return os.path.join(directory, stem + extension)
+      return stem + extension
+
+    groups = self.__group_replace_alias__(groups)
+    VALID_PROTOCOLS = ('Mc', 'Md', 'Ma', 'Ud', 'Ua', 'P', 'G')
+    VALID_PURPOSES = ('enrol', 'probe')
+    VALID_GROUPS = ('g1', 'g2', 'world')
+    VALID_LANGUAGES = ('en', 'fr', 'sp')
+    VALID_CLASSES = ('client', 'impostor')
+
+    protocol = self.__check_validity__(protocol, "protocol", VALID_PROTOCOLS)
+    purposes = self.__check_validity__(purposes, "purpose", VALID_PURPOSES)
+    groups = self.__check_validity__(groups, "group", VALID_GROUPS)
+    languages = self.__check_validity__(languages, "language", VALID_LANGUAGES)
+    classes = self.__check_validity__(classes, "class", VALID_CLASSES)
+    retval = {}
+
+    if(isinstance(model_ids,str)):
+      model_ids = (model_ids,)
+    
+    if 'world' in groups:
+      q = self.session.query(File).join(Client).\
+            filter(Client.sgroup == 'world').\
+            filter(Client.language.in_(languages))
+      if model_ids:
+        q = q.filter(File.claimed_id.in_(model_ids))
+      q = q.order_by(File.real_id, File.session_id, File.claimed_id, File.shot) 
+      for k in q:
+        retval[k.id] = (make_path(k.path, directory, extension), k.claimed_id, k.claimed_id, k.real_id, k.path)
+    
+    if ('g1' in groups or 'g2' in groups):
+      if('enrol' in purposes):
+        q = self.session.query(File).join(Client).join(Session).join(Protocol).\
+              filter(File.claimed_id == File.real_id).\
+              filter(Client.sgroup.in_(groups)).\
+              filter(Client.language.in_(languages)).\
+              filter(Protocol.name.in_(protocol)).\
+              filter(Protocol.purpose == 'enrol')
+        if model_ids:
+          q = q.filter(File.claimed_id.in_(model_ids))
+        q = q.order_by(File.claimed_id, File.session_id, File.real_id, File.shot)
+        for k in q:
+          retval[k.id] = (make_path(k.path, directory, extension), k.claimed_id, k.claimed_id, k.real_id, k.path)
+      if('probe' in purposes):
+        if('client' in classes):
+          q = self.session.query(File).join(Client).join(Session).join(Protocol).\
+                filter(File.claimed_id == File.real_id).\
+                filter(Client.sgroup.in_(groups)).\
+                filter(Client.language.in_(languages)).\
+                filter(Protocol.name.in_(protocol)).\
+                filter(Protocol.purpose == 'probe')
+          if model_ids:
+            q = q.filter(File.claimed_id.in_(model_ids))
+          q = q.order_by(File.claimed_id, File.session_id, File.real_id, File.shot)
+          for k in q:
+            retval[k.id] = (make_path(k.path, directory, extension), k.claimed_id, k.claimed_id, k.real_id, k.path)
+        if('impostor' in classes):
+          q = self.session.query(File).join(Client).join(Session).join(Protocol).\
+                filter(File.claimed_id != File.real_id).\
+                filter(Client.sgroup.in_(groups)).\
+                filter(Client.language.in_(languages)).\
+                filter(Protocol.name.in_(protocol)).\
+                filter(or_(Protocol.purpose == 'probe', Protocol.purpose == 'probeImpostor'))
+          if model_ids:
+            q = q.filter(File.claimed_id.in_(model_ids))
+          for k in q:
+            retval[k.id] = (make_path(k.path, directory, extension), k.claimed_id, k.claimed_id, k.real_id, k.path)
+
+    return retval
+
   def files(self, directory=None, extension=None, protocol=None,
       purposes=None, model_ids=None, groups=None, classes=None, 
       languages=None):
@@ -195,204 +323,11 @@ class Database(object):
     wish to save processing results later on.
     """
 
-    def make_path(stem, directory, extension):
-      import os
-      if not extension: extension = ''
-      if directory: return os.path.join(directory, stem + extension)
-      return stem + extension
-
-    type(groups)
-    groups = self.__group_replace_alias__(groups)
-    VALID_PROTOCOLS = ('Mc', 'Md', 'Ma', 'Ud', 'Ua', 'P', 'G')
-    VALID_PURPOSES = ('enrol', 'probe')
-    VALID_GROUPS = ('g1', 'g2', 'world')
-    VALID_LANGUAGES = ('en', 'fr', 'sp')
-    VALID_CLASSES = ('client', 'impostor')
-
-    protocol = self.__check_validity__(protocol, "protocol", VALID_PROTOCOLS)
-    purposes = self.__check_validity__(purposes, "purpose", VALID_PURPOSES)
-    groups = self.__check_validity__(groups, "group", VALID_GROUPS)
-    languages = self.__check_validity__(languages, "language", VALID_LANGUAGES)
-    classes = self.__check_validity__(classes, "class", VALID_CLASSES)
     retval = {}
-    
-    if 'world' in groups:
-      if not model_ids:
-        q = self.session.query(File).join(Client).\
-              filter(Client.sgroup == 'world').\
-              filter(Client.language.in_(languages)).\
-              order_by(File.real_id, File.session_id, File.claimed_id, File.shot)
-      else:
-        q = self.session.query(File).join(Client).\
-              filter(File.claimed_id.in_(model_ids)).\
-              filter(Client.sgroup == 'world').\
-              filter(Client.language.in_(languages)).\
-              order_by(File.real_id, File.session_id, File.claimed_id, File.shot) 
-      for k in q:
-        retval[k.id] = make_path(k.path, directory, extension)
-    
-    if ('g1' in groups or 'g2' in groups):
-      if('enrol' in purposes):
-        if not model_ids:
-          q = self.session.query(File).join(Client).join(Session).join(Protocol).\
-                filter(File.claimed_id == File.real_id).\
-                filter(Client.sgroup.in_(groups)).\
-                filter(Client.language.in_(languages)).\
-                filter(Protocol.name.in_(protocol)).\
-                filter(Protocol.purpose == 'enrol').\
-                order_by(File.claimed_id, File.session_id, File.real_id, File.shot)
-        else:
-          q = self.session.query(File).join(Client).join(Session).join(Protocol).\
-                filter(File.claimed_id == File.real_id).\
-                filter(File.claimed_id.in_(model_ids)).\
-                filter(Client.sgroup.in_(groups)).\
-                filter(Client.language.in_(languages)).\
-                filter(Protocol.name.in_(protocol)).\
-                filter(Protocol.purpose == 'enrol').\
-                order_by(File.claimed_id, File.session_id, File.real_id, File.shot)
-        for k in q:
-          retval[k.id] = make_path(k.path, directory, extension)
-      if('probe' in purposes):
-        if('client' in classes):
-          if not model_ids:
-            q = self.session.query(File).join(Client).join(Session).join(Protocol).\
-                  filter(File.claimed_id == File.real_id).\
-                  filter(Client.sgroup.in_(groups)).\
-                  filter(Client.language.in_(languages)).\
-                  filter(Protocol.name.in_(protocol)).\
-                  filter(Protocol.purpose == 'probe').\
-                  order_by(File.claimed_id, File.session_id, File.real_id, File.shot)
-          else:
-            q = self.session.query(File).join(Client).join(Session).join(Protocol).\
-                  filter(File.claimed_id.in_(model_ids)).\
-                  filter(File.claimed_id == File.real_id).\
-                  filter(Client.sgroup.in_(groups)).\
-                  filter(Client.language.in_(languages)).\
-                  filter(Protocol.name.in_(protocol)).\
-                  filter(Protocol.purpose == 'probe').\
-                  order_by(File.claimed_id, File.session_id, File.real_id, File.shot)
-          for k in q:
-            retval[k.id] = make_path(k.path, directory, extension)
-        if('impostor' in classes):
-          if not model_ids:
-            q = self.session.query(File).join(Client).join(Session).join(Protocol).\
-                  filter(File.claimed_id != File.real_id).\
-                  filter(Client.sgroup.in_(groups)).\
-                  filter(Client.language.in_(languages)).\
-                  filter(Protocol.name.in_(protocol)).\
-                  filter(or_(Protocol.purpose == 'probe', Protocol.purpose == 'probeImpostor')).\
-                  order_by(File.claimed_id, File.session_id, File.real_id, File.shot)
-          else:
-            q = self.session.query(File).join(Client).join(Session).join(Protocol).\
-                  filter(File.claimed_id.in_(model_ids)).\
-                  filter(File.claimed_id != File.real_id).\
-                  filter(Client.sgroup.in_(groups)).\
-                  filter(Client.language.in_(languages)).\
-                  filter(Protocol.name.in_(protocol)).\
-                  filter(or_(Protocol.purpose == 'probe', Protocol.purpose == 'probeImpostor')).\
-                  order_by(File.claimed_id, File.session_id, File.real_id, File.shot)
-          for k in q:
-            retval[k.id] = make_path(k.path, directory, extension)
-        
-    return retval
-
-  # TODO: dictionary interface
-  #def dictionary(self, directory=None, extension=None, protocol=None,
-  #    purpose=None, client_id=None, groups=None):
-    """Returns a multiple-layers dictionary for the specific query by the user.
-
-    Keyword Parameters:
-
-    directory
-      A directory name that will be prepended to the final filepath returned
-
-    extension
-      A filename extension that will be appended to the final filepath returned
-
-    protocol
-      One of the BANCA protocols ("P", "G", "Mc", "Md", "Ma", "Ud", "Ua").
-
-    purpose
-      The purpose required to be retrieved ("enrol", "probe", "world") or a tuple
-      with several of them. If 'None' is given (this is the default), it is 
-      considered the same as a tuple with all possible values.
-
-    client_ids
-      Only retrieves the files for the provided list of client ids. If 'None' is 
-      given (this is the default), no filter over the client_ids is performed.
-
-    groups
-      One of the two groups ("g1", "g2") or a tuple with several of them. 
-      If 'None' is given (this is the default), it is considered the same as a 
-      tuple with all possible values.
-
-    Returns: A multiple-layers dictionary containing the resolved filenames 
-    considering all the filtering criteria or a full Torch Arrayset if you 
-    allow me to check for the existence of files (set check=True).
-      * The first key belongs to ('world', 'g1', 'g2'), 
-      * The second key is the claimed client id
-      * The third key corresponds to the type of access ('client', 'impostor')
-      * The fourth key is the key id of the file
-    """
-
-    """
-    def make_path(stem, directory, extension):
-      import os
-      if not extension: extension = ''
-      if directory: return os.path.join(directory, stem + extension)
-      return stem + extension
-
-    VALID_PROTOCOLS = ('Mc', 'Md', 'Ma', 'Ud', 'Ua', 'P', 'G')
-    VALID_PURPOSES = ('enrol', 'probe', 'world')
-    # TODO: check client id, factorize valid groups ?
-    VALID_GROUPS = ('g1', 'g2')
-
-    protocol = self.__check_validity__(protocol, "protocol", VALID_PROTOCOLS)
-    purpose = self.__check_validity__(purpose, "purpose", VALID_PURPOSE)
-    groups = self.__check_validity__(groups, "group", VALID_GROUPS)
-    retval = {'world': {}, 'enrol': {}, 'probe': {} }
-
-    # Add world data if required
-    if 'world' in purpose:
-      q = self.session.query(File).join(Client).filter(File.real_id == Client.id).filter(Client.sgroup == 'world').order_by(File.real_id, File.session_id, File.claimed_id, File.shot)
-      for k in q: 
-        if not str(k.claimed_id) in retval['world']:
-          retval['world'][str(k.claimed_id)] = {'client': {}, 'impostor': {} }
-        if k.real_id == k.claimed_id:
-          retval['world'][str(k.claimed_id)]['client'][str(k.id)] = make_path(k.path, directory, extension)
-        else:
-          retval['world'][str(k.claimed_id)]['impostor'][str(k.id)] = make_path(k.path, directory, extension)
-
-    # List of the clients
-    if not client_ids:
-      client_ids = []
-      q = self.session.query(File).join(Client).filter(File.real_id == Client.id).filter(Client.sgroup.in_(groups)).order_by(Client.id)
-      for k in q:
-        if k.real_id not in client_ids:
-          client_ids.append(k.claimed_id)
-          retval['enrol'][str(k.claimed_id)] = {'client': {}, 'impostor': {} }
-          retval['probe'][str(k.claimed_id)] = {'client': {}, 'impostor': {} }
-    else:
-      for id in client_ids:
-        retval['enrol'][str(id)] = {'client': {}, 'impostor': {} }
-        retval['probe'][str(id)] = {'client': {}, 'impostor': {} }
-    
-    # Loop over the client_ids and add required data
-    if 'enrol' in purpose:
-      q = self.session.query(File).join(Client).join(Session).join(Protocol).filter(File.claimed_id.in_(client_ids)).filter(File.real_id == File.claimed_id).filter(Client.sgroup.in_(groups)).filter(File.session_id == Session.id).filter(Protocol.name.in_(protocol)).filter(Protocol.session_id == Session.id).filter(Protocol.purpose == 'enrol').order_by(File.real_id, File.session_id, File.claimed_id, File.shot)
-      for k in q:
-        retval['enrol'][str(k.claimed_id)]['client'][str(k.id)] = make_path(k.path, directory, extension)
-    
-    if 'probe' in purpose:
-      q = self.session.query(File).join(Client).join(Session).join(Protocol).filter(File.claimed_id.in_(client_ids)).filter(Client.sgroup.in_(groups)).filter(Protocol.session_id == File.session_id).filter(Protocol.name.in_(protocol)).filter(or_(Protocol.purpose == 'probe', and_(Protocol.purpose == 'probeImpostor', File.claimed_id != File.real_id))).order_by(File.real_id, File.session_id, File.claimed_id, File.shot)
-      for k in q:
-        if k.real_id == k.claimed_id:
-          retval['probe'][str(k.claimed_id)]['client'][str(k.id)] = make_path(k.path, directory, extension)
-        else:
-          retval['probe'][str(k.claimed_id)]['impostor'][str(k.id)] = make_path(k.path, directory, extension)
+    d = self.objects(directory, extension, protocol, purposes, model_ids, groups, classes, languages)
+    for k in d: retval[k] = d[k][0]
 
     return retval
-    """
 
   def save_one(self, id, obj, directory, extension):
     """Saves a single object supporting the torch save() protocol.
