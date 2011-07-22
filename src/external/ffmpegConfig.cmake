25d6e0c61ceeed527852c2cd686f406f1206198c
Andre Anjos <andre.anjos@idiap.ch>
2011-07-22 09:59:16 +0200

    Update dependence documentation (bis)

--- src/external/ffmpegConfig.cmake
+++ src/external/ffmpegConfig.cmake
@@ -3,8 +3,9 @@
 
 include(FindPkgConfig)
 
-# These are the versions of the several libraries shipped with ffmpeg. You have
-# to tune your requirements according to you needs.
+# These are the versions of the several libraries shipped with ffmpeg. You may
+# have to tune your requirements according to you needs.
+#
 # ffmpeg | avformat | avcodec | avutil  | swscale | old style | swscale GPL?
 # =======+==========+=========+=========+=========+===========+==============
 # 0.5    | 52.31.0  | 52.20.0 | 49.15.0 | 0.7.1   | yes       | yes
@@ -13,7 +14,8 @@ include(FindPkgConfig)
 # 0.5.3  | 52.31.0  | 52.20.1 | 49.15.0 | 0.7.1   | yes       | yes
 # 0.6    | 52.64.2  | 52.72.2 | 50.15.1 | 0.11.0  | no        | no
 # 0.6.1  | 52.64.2  | 52.72.2 | 50.15.1 | 0.11.0  | no        | no
-# 0.7.1  | 53.4.0   | 53.7.0  | 51.10.0 | 2.0.0   | no        | no
+# 0.7.1  | 52.122.0 | 52.110.0| 50.43.0 | 0.14.1  | no        | no
+# trunk  | 53.4.0   | 53.7.0  | 51.10.0 | 2.0.0   | no        | no
 
 # Our base build requires ffmpeg >= 0.5. This is available on most platforms,
 # but please note that if you link to anything <= 0.6, your code will become
