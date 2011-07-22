281272a085ac13db3402aa59a7b009e60cd89c09
Andre Anjos <andre.anjos@idiap.ch>
2011-07-22 12:40:12 +0200

    Be very precise about the location of Python header files

--- src/external/pythonConfig.cmake
+++ src/external/pythonConfig.cmake
@@ -13,10 +13,17 @@ set(Python_ADDITIONAL_VERSIONS ${PYTHON_VERSION})
 include(FindPythonLibs)
 include(FindPythonInterp)
 
+# This calculates the correct python installation prefix for the current
+# interpreter
+execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import sys; print sys.prefix" OUTPUT_VARIABLE PYTHON_PREFIX OUTPUT_STRIP_TRAILING_WHITESPACE)
+
 # This will calculate the include path for numpy
 execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import numpy; print numpy.get_include()" OUTPUT_VARIABLE PYTHON_NUMPY_INCLUDE_DIR OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-set(python_INCLUDE_DIRS "${PYTHON_INCLUDE_DIR};${PYTHON_NUMPY_INCLUDE_DIR}" CACHE INTERNAL "incdirs")
+# Do not use the include dir path found by FindPythonLibs as it does not
+# work properly on OSX (we end up getting the system path if another python
+# version is selected). This may cause compilation problems.
+set(python_INCLUDE_DIRS "${PYTHON_PREFIX}/include/python${PYTHON_VERSION};${PYTHON_NUMPY_INCLUDE_DIR}" CACHE INTERNAL "incdirs")
 get_filename_component(python_LIBRARY_DIRS ${PYTHON_LIBRARY} PATH CACHE)
   
 set(PYTHON_INSTALL_DIRECTORY ${CMAKE_INSTALL_PREFIX}/lib/python${PYTHON_VERSION}
