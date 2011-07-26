33dacf8ef7fa457acd1e9b91a9308e2c36930649
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2011-07-26 18:45:37 +0200

    Update JFAMachine python bindings

--- src/python/machine/src/jfa.cc
+++ src/python/machine/src/jfa.cc
@@ -5,9 +5,11 @@
  */
 
 #include <boost/python.hpp>
+#include <boost/shared_ptr.hpp>
 #include "core/python/exception.h"
 #include "core/python/vector.h"
 #include "machine/JFAMachine.h"
+#include "machine/GMMMachine.h"
 
 using namespace boost::python;
 namespace mach = Torch::machine;
@@ -16,20 +18,33 @@ namespace tp = Torch::core::python;
 
 
 void bind_machine_jfa() {
-  class_<mach::JFAMachine, boost::shared_ptr<mach::JFAMachine>
-    >("JFAMachine", "A JFAMachine", init<int, int, int, int>((arg("C"), arg("D"), arg("ru"), arg("rv")), "Builds a new JFAMachine."))
+  class_<mach::JFABaseMachine, boost::shared_ptr<mach::JFABaseMachine> >("JFABaseMachine", "A JFABaseMachine", init<boost::shared_ptr<mach::GMMMachine>, int, int>((arg("ubm"), arg("ru"), arg("rv")), "Builds a new JFABaseMachine."))
+    .def(init<io::HDF5File&>((arg("config")), "Constructs a new JFABaseMachine from a configuration file."))
+    .def(init<const mach::JFABaseMachine&>((arg("machine")), "Copy constructs a JFABaseMachine"))
+    .def("load", &mach::JFABaseMachine::load, (arg("self"), arg("config")), "Loads the configuration parameters from a configuration file.")
+    .add_property("ubm", &mach::JFABaseMachine::getUbm, &mach::JFABaseMachine::setUbm)
+    .add_property("U", make_function(&mach::JFABaseMachine::getU, return_internal_reference<>()), &mach::JFABaseMachine::setU)
+    .add_property("V", make_function(&mach::JFABaseMachine::getV, return_internal_reference<>()), &mach::JFABaseMachine::setV)
+    .add_property("D", make_function(&mach::JFABaseMachine::getD, return_internal_reference<>()), &mach::JFABaseMachine::setD)
+    .add_property("DimC", &mach::JFABaseMachine::getDimC)
+    .add_property("DimD", &mach::JFABaseMachine::getDimD)
+    .add_property("DimCD", &mach::JFABaseMachine::getDimCD)
+    .add_property("DimRu", &mach::JFABaseMachine::getDimRu)
+    .add_property("DimRv", &mach::JFABaseMachine::getDimRv)
+  ;
+
+  class_<mach::JFAMachine, boost::shared_ptr<mach::JFAMachine> >("JFAMachine", "A JFAMachine", init<boost::shared_ptr<mach::JFABaseMachine> >((arg("jfa_base")), "Builds a new JFAMachine."))
     .def(init<io::HDF5File&>((arg("config")), "Constructs a new JFAMachine from a configuration file."))
     .def(init<const mach::JFAMachine&>((arg("machine")), "Copy constructs a JFAMachine"))
     .def("load", &mach::JFAMachine::load, (arg("self"), arg("config")), "Loads the configuration parameters from a configuration file.")
-    .add_property("ubm_mean", make_function(&mach::JFAMachine::getUbmMean, return_internal_reference<>()), &mach::JFAMachine::setUbmMean)
-    .add_property("ubm_var", make_function(&mach::JFAMachine::getUbmVar, return_internal_reference<>()), &mach::JFAMachine::setUbmVar)
-    .add_property("U", make_function(&mach::JFAMachine::getU, return_internal_reference<>()), &mach::JFAMachine::setU)
-    .add_property("V", make_function(&mach::JFAMachine::getV, return_internal_reference<>()), &mach::JFAMachine::setV)
-    .add_property("D", make_function(&mach::JFAMachine::getD, return_internal_reference<>()), &mach::JFAMachine::setD)
+    .add_property("ubm", &mach::JFAMachine::getJFABase, &mach::JFAMachine::setJFABase)
+    .add_property("y", make_function(&mach::JFAMachine::getY, return_internal_reference<>()), &mach::JFAMachine::setY)
+    .add_property("z", make_function(&mach::JFAMachine::getZ, return_internal_reference<>()), &mach::JFAMachine::setZ)
     .add_property("DimC", &mach::JFAMachine::getDimC)
     .add_property("DimD", &mach::JFAMachine::getDimD)
     .add_property("DimCD", &mach::JFAMachine::getDimCD)
     .add_property("DimRu", &mach::JFAMachine::getDimRu)
     .add_property("DimRv", &mach::JFAMachine::getDimRv)
   ;
+
 }
