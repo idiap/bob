Fixes and updates for Spatial Gradients and base Optical Flow

src/python/ip/test/test_flow.py
@@ -26,35 +26,30 @@ def load_known_flow(relative_filename):
 def make_image_tripplet():
   """Creates two images for you to calculate the flow
   
-  10 10 10 10 10    10 10 10 10 10    10 10 10 10 10
-  10  5  5  5  5    10 10 10 10 10    10 10 10 10 10
-  10  5  5  5  5 => 10 10  5  5  5 => 10 10 10 10 10
-  10 10 10 10 10    10 10  5  5  5    10 10 10  5  5
-  10 10 10 10 10    10 10 10 10 10    10 10 10  5  5
+  255 255 255 255 255    255 255 255 255 255    255 255 255 255 255
+  255   0   0   0   0    255 255 255 255 255    255 255 255 255 255
+  255   0   0   0   0 => 255 255   0   0   0 => 255 255 255 255 255
+  255 255 255 255 255    255 255   0   0   0    255 255 255   0   0
+  255 255 255 255 255    255 255 255 255 255    255 255 255   0   0
   
   """
   im1 = torch.core.array.uint8_2(5,5)
-  im1.fill(10)
-  im1[1:3, 1:] = 5
+  im1.fill(255)
+  im1[1:3, 1:] = 0
   im2 = torch.core.array.uint8_2(5,5)
-  im2.fill(10)
-  im2[2:4, 2:] = 5
+  im2.fill(255)
+  im2[2:4, 2:] = 0
   im3 = torch.core.array.uint8_2(5,5)
-  im3.fill(10)
-  im3[3:, 3:] = 5
-  return im1.cast('float64'), im2.cast('float64'), im3.cast('float64')
+  im3.fill(255)
+  im3[3:, 3:] = 0
+  return im1.cast('float64')/255., im2.cast('float64')/255., im3.cast('float64')/255.
 
 def HornAndSchunckFlowPython(alpha, im1, im2, im3, u0, v0):
   """Calculates the H&S flow in pure python"""
   grad = torch.ip.HornAndSchunckGradient(im1.shape())
   ex, ey, et = grad(im1, im2)
-  ex *= 0.25
-  ey *= 0.25
-  et *= 0.25
-  #u = torch.ip.laplacian_12(u0) / -12.
-  #v = torch.ip.laplacian_12(v0) / -12.
-  u = torch.ip.laplacian_014(u0) / -0.25
-  v = torch.ip.laplacian_014(v0) / -0.25
+  u = torch.ip.laplacian_avg_hs(u0)
+  v = torch.ip.laplacian_avg_hs(v0)
   common_term = (ex*u + ey*v + et) / (ex**2 + ey**2 + alpha**2)
   return u - ex*common_term, v - ey*common_term
 
@@ -74,15 +69,15 @@ def compute_flow_opencv(alpha, iterations, ifile1, ifile2):
 class FlowTest(unittest.TestCase):
   """Performs various combined optical flow tests."""
 
-  def notest01_HornAndSchunckAgainstSyntheticPython(self):
+  def test01_HornAndSchunckAgainstSyntheticPython(self):
     
     # Tests and examplifies usage of the vanilla HS algorithm while comparing
     # the C++ implementation to a pythonic implementation of the same algorithm
 
     # We create a new estimator specifying the alpha parameter (first value)
     # and the number of iterations to perform (second value).
-    N = 100
-    alpha = 15. 
+    N = 1200
+    alpha = 1.5 
 
     # The OpticalFlow estimator always receives a blitz::Array<uint8_t,2> as
     # the image input. The output has the same rank and extents but is in
@@ -96,29 +91,31 @@ class FlowTest(unittest.TestCase):
     for i in range(N):
       flow(alpha, 1, i1, i2, u_cxx, v_cxx)
       u_py, v_py = HornAndSchunckFlowPython(alpha, i1, i2, i3, u_py, v_py)
-      #cxx_se2 = flow.evalEc2(u_cxx, v_cxx)
-      #cxx_be = flow.evalEb(i1, i2, u_cxx, v_cxx)
-      #py_se2 = flow.evalEc2(u_py, v_py)
-      #py_be = flow.evalEb(i1, i2, u_py, v_py)
-      #cxx_avg_err = (cxx_se2 * (alpha**2) + cxx_be**2).sum()
-      #py_avg_err = (py_se2 * (alpha**2) + py_be**2).sum()
-      #print "Error %2d: %.3e (%.3e) %.3e (%.3e) %.3e (%.3e)" % \
-      #    (
-      #     i, 
-      #     cxx_se2.sum()**0.5,
-      #     py_se2.sum()**0.5, 
-      #     cxx_be.sum(), 
-      #     py_be.sum(), 
-      #     cxx_avg_err**0.5,
-      #     py_avg_err**0.5
-      #    )
+      cxx_se2 = flow.evalEc2(u_cxx, v_cxx)
+      cxx_be = flow.evalEb(i1, i2, u_cxx, v_cxx)
+      py_se2 = flow.evalEc2(u_py, v_py)
+      py_be = flow.evalEb(i1, i2, u_py, v_py)
+      cxx_avg_err = (cxx_se2 * (alpha**2) + cxx_be**2).sum()
+      py_avg_err = (py_se2 * (alpha**2) + py_be**2).sum()
+      '''
+      print "Error %2d: %.3e (%.3e) %.3e (%.3e) %.3e (%.3e)" % \
+          (
+           i, 
+           cxx_se2.sum()**0.5,
+           py_se2.sum()**0.5, 
+           cxx_be.sum(), 
+           py_be.sum(), 
+           cxx_avg_err**0.5,
+           py_avg_err**0.5
+          )
+      '''
     self.assertTrue( u_cxx.numeq(u_py) )
     self.assertTrue( v_cxx.numeq(v_py) )
 
   def notest02_VanillaHornAndSchunckDemo(self):
     
     N = 64
-    alpha = 2 
+    alpha = 1.1
 
     if1 = os.path.join("rubberwhale", "frame10_gray.png")
     if2 = os.path.join("rubberwhale", "frame11_gray.png")
@@ -128,10 +125,21 @@ class FlowTest(unittest.TestCase):
     u = torch.core.array.float64_2(i1.shape()); u.fill(0)
     v = torch.core.array.float64_2(i1.shape()); v.fill(0)
     flow = torch.ip.VanillaHornAndSchunckFlow(i1.shape())
-    for k in range(N): 
+    for i in range(N): 
       flow(alpha, 1, i1, i2, u, v)
-      array = (255.0*torch.ip.flowutils.flow2hsv(u,v)).cast('uint8')
-      array.save("hs_rubberwhale-%d.png" % k)
+      #array = (255.0*torch.ip.flowutils.flow2hsv(u,v)).cast('uint8')
+      #array.save("hs_rubberwhale-%d.png" % i)
+      se2 = flow.evalEc2(u, v)
+      be = flow.evalEb(i1, i2, u, v)
+      avg_err = (se2 * (alpha**2) + be**2).sum()
+      print "Error %2d| Ec2: %.3e Eb: %.3e E2 (avg.): %.3e" % \
+          (
+           i, 
+           se2.sum()**0.5,
+           be.sum(), 
+           avg_err**0.5,
+          )
+      #print "error:", (torch.ip.flowError(i1, i2, u, v)**2).sum()
 
   def notest03_VanillaHornAndSchunckAgainstOpenCV(self):
     
@@ -144,7 +152,7 @@ class FlowTest(unittest.TestCase):
 
     # We create a new estimator specifying the alpha parameter (first value)
     # and the number of iterations to perform (second value).
-    N = 5 
+    N = 64 
     alpha = 15 
 
     # The OpticalFlow estimator always receives a blitz::Array<uint8_t,2> as
@@ -165,15 +173,6 @@ class FlowTest(unittest.TestCase):
     be = flow.evalEb(i1, i2, u_cxx, v_cxx)
     avg_err = (se2 * (alpha**2) + be**2).sum()
     print "Torch H&S Error (%2d iterations) : %.3e %.3e %.3e" % (N, se2.sum()**0.5, be.sum(), avg_err**0.5)
-
-    u_py = torch.core.array.float64_2(i1.shape()); u_py.fill(0)
-    v_py = torch.core.array.float64_2(i1.shape()); v_py.fill(0)
-    for i in range(N):
-      u_py, v_py = HornAndSchunckFlowPython(alpha, i1, i2, None, u_py, v_py)
-    se2 = flow.evalEc2(u_py, v_py)
-    be = flow.evalEb(i1, i2, u_py, v_py)
-    avg_err = (se2 * (alpha**2) + be**2).sum()
-    print "Python H&S Error (%2d iterations) : %.3e %.3e %.3e" % (N, se2.sum()**0.5, be.sum(), avg_err**0.5)
 
     u_ocv1, v_ocv1 = compute_flow_opencv(alpha, N/4, if1, if2)
     u_ocv2, v_ocv2 = compute_flow_opencv(alpha, N/2, if1, if2)