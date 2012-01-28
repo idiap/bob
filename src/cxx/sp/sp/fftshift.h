8e53b96fbf4f6dcf20d49f7949817d05e18e29b0
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2012-01-28 20:28:10 +0100

    fftshift: check for non existing quadrants

--- src/cxx/sp/sp/fftshift.h
+++ src/cxx/sp/sp/fftshift.h
@@ -47,44 +47,62 @@ namespace bob {
       void fftshiftNoCheck( const int center, const blitz::Array<T,1>& A, 
         blitz::Array<T,1>& B)
       {
-        // Process source left
-        blitz::Range  A_d1(0, center-1),
-                      B_d1(B.extent(0)-center, B.extent(0)-1);
-        B(B_d1) = A(A_d1);
-        // Process source right
-        blitz::Range  A_d2(center, A.extent(0)-1),
-                      B_d2(0, B.extent(0)-center-1);
-        B(B_d2) = A(A_d2);
+        if(center>0)
+        {
+          // Process source left
+          blitz::Range  A_d1(0, center-1),
+                        B_d1(B.extent(0)-center, B.extent(0)-1);
+          B(B_d1) = A(A_d1);
+        }
+        if(center<A.extent(0))
+        {
+         // Process source right
+         blitz::Range  A_d2(center, A.extent(0)-1),
+                       B_d2(0, B.extent(0)-center-1);
+         B(B_d2) = A(A_d2);
+        }
       }
 
       template <typename T>
       void fftshiftNoCheck( const int centerY, const int centerX, 
         const blitz::Array<T,2>& A, blitz::Array<T,2>& B)
       {
-        // Process source quadrant 1 (Top left)
-        blitz::Range  A_y1(0, centerY-1),
-                      A_x1(0, centerX-1),
-                      B_y1(B.extent(0)-centerY, B.extent(0)-1),
-                      B_x1(B.extent(1)-centerX, B.extent(1)-1);
-        B(B_y1,B_x1) = A(A_y1,A_x1);
-        // Process source quadrant 3 (Bottom right)
-        blitz::Range  A_y3(centerY, A.extent(0)-1),
-                      A_x3(centerX, A.extent(1)-1),
-                      B_y3(0, B.extent(0)-centerY-1),
-                      B_x3(0, B.extent(1)-centerX-1);
-        B(B_y3,B_x3) = A(A_y3,A_x3);
-        // Process source quadrant 2 (Top right)
-        blitz::Range  A_y2(0, centerY-1),
-                      A_x2(centerX, A.extent(1)-1),
-                      B_y2(B.extent(0)-centerY, B.extent(0)-1),
-                      B_x2(0, B.extent(1)-centerX-1);
-        B(B_y2,B_x2) = A(A_y2,A_x2);
-        // Process source quadrant 4 (Bottom left)
-        blitz::Range  A_y4(centerY, A.extent(0)-1),
-                      A_x4(0, centerX-1),
-                      B_y4(0, B.extent(0)-centerY-1),
-                      B_x4(B.extent(1)-centerX, B.extent(1)-1);
-        B(B_y4,B_x4) = A(A_y4,A_x4);
+        if(centerY>0 && centerX>0)
+        {
+          // Process source quadrant 1 (Top left)
+          blitz::Range  A_y1(0, centerY-1),
+                        A_x1(0, centerX-1),
+                        B_y1(B.extent(0)-centerY, B.extent(0)-1),
+                        B_x1(B.extent(1)-centerX, B.extent(1)-1);
+          B(B_y1,B_x1) = A(A_y1,A_x1);
+        }
+        if(centerY<A.extent(0) && centerX<A.extent(1))
+        {
+          // Process source quadrant 3 (Bottom right)
+          blitz::Range  A_y3(centerY, A.extent(0)-1),
+                        A_x3(centerX, A.extent(1)-1),
+                        B_y3(0, B.extent(0)-centerY-1),
+                        B_x3(0, B.extent(1)-centerX-1);
+          B(B_y3,B_x3) = A(A_y3,A_x3);
+        }
+        if(centerY>0 && centerX<A.extent(1))
+        {
+          // Process source quadrant 2 (Top right)
+          blitz::Range  A_y2(0, centerY-1),
+                        A_x2(centerX, A.extent(1)-1),
+                        B_y2(B.extent(0)-centerY, B.extent(0)-1),
+                        B_x2(0, B.extent(1)-centerX-1);
+          B(B_y2,B_x2) = A(A_y2,A_x2);
+        }
+        if(centerY<A.extent(0) && centerX>0)
+        {
+          // Process source quadrant 4 (Bottom left)
+          blitz::Range  A_y4(centerY, A.extent(0)-1),
+                        A_x4(0, centerX-1),
+                        B_y4(0, B.extent(0)-centerY-1),
+                        B_x4(B.extent(1)-centerX, B.extent(1)-1);
+          B(B_y4,B_x4) = A(A_y4,A_x4);
+        }
       }
     }
 
