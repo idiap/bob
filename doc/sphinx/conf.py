77dacb38580656acf56e57e97a0cefaf8fd6f453
Andre Anjos <andre.anjos@idiap.ch>
2012-01-31 11:21:00 +0100

    Modifications to the Sphinx generation to use the new logo

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -125,16 +125,16 @@ if sphinx.__version__ >= "1.0":
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+#html_short_title = 'bob'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = '../img/bob+reflection-sphinx.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = '../img/favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -203,7 +203,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = '../img/bob+reflection-sphinx.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
