7ef3c5fb5220896340f6611c0296f813a6402db3
Andre Anjos <andre.dos.anjos@gmail.com>
2012-02-14 22:47:49 +0100

    New file that integrates version numbers and build platforms

---
+++ python/lib/build.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Andre Anjos <andre.dos.anjos@gmail.com>
+# Tue 14 Feb 21:17:18 2012 
+
+"""Version information collected during Bob's build
+"""
+
+version = "@BOB_VERSION@"
+platform = "@BOB_PLATFORM@"
