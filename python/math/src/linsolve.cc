58d189e06efa0da3d49390cd02c2ed7b2f53d46c
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2012-03-05 12:48:07 +0100

    Minor documentation change

--- python/math/src/linsolve.cc
+++ python/math/src/linsolve.cc
@@ -28,50 +28,178 @@ using namespace boost::python;
 namespace tp = bob::python;
 namespace ca = bob::core::array;
 
-static const char* LINSOLVE_DOC = "Solve the linear system A*x=b and return the result as a blitz array. The solver is from the LAPACK library.";
-static const char* LINSOLVE_SYMPOS_DOC = "Solve the linear system A*x=b, where A is symmetric definite positive, and return the result as a blitz array. The solver is from the LAPACK library.";
+static const char* LINSOLVE_DOC = "Solve the linear system A*x=b and return the result as a blitz array. The solver is from the LAPACK library (use of dgesv).";
+static const char* LINSOLVE_SYMPOS_DOC = "Solve the linear system A*x=b, where A is symmetric definite positive, and return the result as a blitz array. The solver is from the LAPACK library (use of dposv).";
 static const char* LINSOLVE_CG_SYMPOS_DOC = "Solve the linear system A*x=b via conjugate gradients, where A is symmetric definite positive, and return the result as a blitz array.";
 
 static void script_linsolve(tp::const_ndarray A, tp::ndarray x, tp::const_ndarray b) {
-  blitz::Array<double,1> x_ = x.bz<double,1>();
-  bob::math::linsolve(A.bz<double,2>(), x_, 
-      b.bz<double,1>());
+  const ca::typeinfo& info_x = x.type();
+  const ca::typeinfo& info_b = b.type();
+  if(info_x.dtype == ca::t_float64 && info_b.dtype == ca::t_float64)
+  {
+    if(info_x.nd == info_b.nd)
+    {
+      if(info_x.nd == 1)
+      {
+        blitz::Array<double,1> x_ = x.bz<double,1>();
+        bob::math::linsolve(A.bz<double,2>(), x_, 
+          b.bz<double,1>());
+      }
+      else if(info_x.nd == 2)
+      {
+        blitz::Array<double,2> x_ = x.bz<double,2>();
+        bob::math::linsolve(A.bz<double,2>(), x_, 
+          b.bz<double,2>());
+      }
+      else
+        PYTHON_ERROR(TypeError, "Linear solver does not support more than 2 dimensions");
+    }
+    else
+      PYTHON_ERROR(TypeError, "x and b should have the same number of dimensions");
+  }
+  else
+    PYTHON_ERROR(TypeError, "Linear solver does only support float64 type");
 }
 
 static void script_linsolve_(tp::const_ndarray A, tp::ndarray x, tp::const_ndarray b) {
-  blitz::Array<double,1> x_ = x.bz<double,1>();
-  bob::math::linsolve_(A.bz<double,2>(), x_, 
-      b.bz<double,1>());
+  const ca::typeinfo& info_x = x.type();
+  const ca::typeinfo& info_b = b.type();
+  if(info_x.dtype == ca::t_float64 && info_b.dtype == ca::t_float64)
+  {
+    if(info_x.nd == info_b.nd)
+    {
+      if(info_x.nd == 1)
+      {
+        blitz::Array<double,1> x_ = x.bz<double,1>();
+        bob::math::linsolve_(A.bz<double,2>(), x_, 
+          b.bz<double,1>());
+      }
+      else if(info_x.nd == 2)
+      {
+        blitz::Array<double,2> x_ = x.bz<double,2>();
+        bob::math::linsolve_(A.bz<double,2>(), x_, 
+          b.bz<double,2>());
+      }
+      else
+        PYTHON_ERROR(TypeError, "Linear solver does not support more than 2 dimensions");
+    }
+    else
+      PYTHON_ERROR(TypeError, "x and b should have the same number of dimensions");
+  }
+  else
+    PYTHON_ERROR(TypeError, "Linear solver does only support float64 type");
 }
 
 static object py_script_linsolve(tp::const_ndarray A, tp::const_ndarray b) {
-  const ca::typeinfo& info = A.type();
-  tp::ndarray res(info.dtype, info.shape[0]);
-  blitz::Array<double,1> res_ = res.bz<double,1>();
-  bob::math::linsolve(A.bz<double,2>(), res_, 
-      b.bz<double,1>());
-  return res.self();
+  const ca::typeinfo& info_b = b.type();
+  if(info_b.dtype == ca::t_float64)
+  {
+    if(info_b.nd == 1)
+    {
+      tp::ndarray x(info_b.dtype, info_b.shape[0]);
+      blitz::Array<double,1> x_ = x.bz<double,1>();
+      bob::math::linsolve_(A.bz<double,2>(), x_, 
+        b.bz<double,1>());
+      return x.self();
+    }
+    else if(info_b.nd == 2)
+    {
+      tp::ndarray x(info_b.dtype, info_b.shape[0], info_b.shape[1]);
+      blitz::Array<double,2> x_ = x.bz<double,2>();
+      bob::math::linsolve_(A.bz<double,2>(), x_, 
+        b.bz<double,2>());
+      return x.self();
+    }
+    else
+      PYTHON_ERROR(TypeError, "Linear solver does not support more than 2 dimensions");
+  }
+  else
+    PYTHON_ERROR(TypeError, "Linear solver does only support float64 type");
 }
 
 static void script_linsolveSympos(tp::const_ndarray A, tp::ndarray x, tp::const_ndarray b) {
-  blitz::Array<double,1> x_ = x.bz<double,1>();
-  bob::math::linsolveSympos(A.bz<double,2>(), x_, 
-      b.bz<double,1>());
+  const ca::typeinfo& info_x = x.type();
+  const ca::typeinfo& info_b = b.type();
+  if(info_x.dtype == ca::t_float64 && info_b.dtype == ca::t_float64)
+  {
+    if(info_x.nd == info_b.nd)
+    {
+      if(info_x.nd == 1)
+      {
+        blitz::Array<double,1> x_ = x.bz<double,1>();
+        bob::math::linsolveSympos(A.bz<double,2>(), x_, 
+          b.bz<double,1>());
+      }
+      else if(info_x.nd == 2)
+      {
+        blitz::Array<double,2> x_ = x.bz<double,2>();
+        bob::math::linsolveSympos(A.bz<double,2>(), x_, 
+          b.bz<double,2>());
+      }
+      else
+        PYTHON_ERROR(TypeError, "Linear solver does not support more than 2 dimensions");
+    }
+    else
+      PYTHON_ERROR(TypeError, "x and b should have the same number of dimensions");
+  }
+  else
+    PYTHON_ERROR(TypeError, "Linear solver does only support float64 type");
 }
 
 static void script_linsolveSympos_(tp::const_ndarray A, tp::ndarray x, tp::const_ndarray b) {
-  blitz::Array<double,1> x_ = x.bz<double,1>();
-  bob::math::linsolveSympos_(A.bz<double,2>(), x_, 
-      b.bz<double,1>());
+  const ca::typeinfo& info_x = x.type();
+  const ca::typeinfo& info_b = b.type();
+  if(info_x.dtype == ca::t_float64 && info_b.dtype == ca::t_float64)
+  {
+    if(info_x.nd == info_b.nd)
+    {
+      if(info_x.nd == 1)
+      {
+        blitz::Array<double,1> x_ = x.bz<double,1>();
+        bob::math::linsolveSympos_(A.bz<double,2>(), x_, 
+          b.bz<double,1>());
+      }
+      else if(info_x.nd == 2)
+      {
+        blitz::Array<double,2> x_ = x.bz<double,2>();
+        bob::math::linsolveSympos_(A.bz<double,2>(), x_, 
+          b.bz<double,2>());
+      }
+      else
+        PYTHON_ERROR(TypeError, "Linear solver does not support more than 2 dimensions");
+    }
+    else
+      PYTHON_ERROR(TypeError, "x and b should have the same number of dimensions");
+  }
+  else
+    PYTHON_ERROR(TypeError, "Linear solver does only support float64 type");
 }
 
 static object py_script_linsolveSympos(tp::const_ndarray A, tp::const_ndarray b) {
-  const ca::typeinfo& info = b.type();
-  tp::ndarray res(info.dtype, info.shape[0]);
-  blitz::Array<double,1> res_ = res.bz<double,1>();
-  bob::math::linsolveSympos(A.bz<double,2>(), res_,
-      b.bz<double,1>());
-  return res.self();
+  const ca::typeinfo& info_b = b.type();
+  if(info_b.dtype == ca::t_float64)
+  {
+    if(info_b.nd == 1)
+    {
+      tp::ndarray x(info_b.dtype, info_b.shape[0]);
+      blitz::Array<double,1> x_ = x.bz<double,1>();
+      bob::math::linsolveSympos_(A.bz<double,2>(), x_, 
+        b.bz<double,1>());
+      return x.self();
+    }
+    else if(info_b.nd == 2)
+    {
+      tp::ndarray x(info_b.dtype, info_b.shape[0], info_b.shape[1]);
+      blitz::Array<double,2> x_ = x.bz<double,2>();
+      bob::math::linsolveSympos_(A.bz<double,2>(), x_, 
+        b.bz<double,2>());
+      return x.self();
+    }
+    else
+      PYTHON_ERROR(TypeError, "Linear solver does not support more than 2 dimensions");
+  }
+  else
+    PYTHON_ERROR(TypeError, "Linear solver does only support float64 type");
 }
 
 static void script_linsolveCGSympos(tp::const_ndarray A, tp::ndarray x, tp::const_ndarray b,
