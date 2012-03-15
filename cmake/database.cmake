7773be76b3e6301e09ccaaa3d8a933af6d457cae
Andre Anjos <andre.anjos@idiap.ch>
2012-03-15 12:22:39 +0100

    Re-activate verbosity for creating databases

--- cmake/database.cmake
+++ cmake/database.cmake
@@ -7,4 +7,4 @@ add_custom_target(db-try-download
   COMMAND ${CMAKE_SOURCE_DIR}/bin/dbdownload.py --verbose --server=${BOB_DATABASE_URL} --version=${BOB_DATABASE_VERSION} --destination=${CMAKE_SOURCE_DIR}/databases --try COMMENT "Trying to download/refresh databases from '${BOB_DATABASE_URL}/${BOB_DATABASE_VERSION}' to '${CMAKE_SOURCE_DIR}/databases'")
 
 add_custom_target(db-create
-  COMMAND ${CMAKE_BINARY_DIR}/bin/dbmanage.py all create --recreate COMMENT "Creating databases at '${CMAKE_BINARY_DIR}' from scratch")
+  COMMAND ${CMAKE_BINARY_DIR}/bin/dbmanage.py all create --recreate --verbose COMMENT "Creating databases at '${CMAKE_BINARY_DIR}' from scratch")
