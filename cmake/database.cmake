b7a9843b78777358bcaace6e3b286f4c10f95b15
Andre Anjos <andre.anjos@idiap.ch>
2012-03-15 09:28:06 +0100

    Simplify database management even more

--- cmake/database.cmake
+++ cmake/database.cmake
@@ -6,11 +6,5 @@ add_custom_target(db-download
 add_custom_target(db-try-download
   COMMAND ${CMAKE_SOURCE_DIR}/bin/dbdownload.py --verbose --server=${BOB_DATABASE_URL} --version=${BOB_DATABASE_VERSION} --destination=${CMAKE_SOURCE_DIR}/databases --try COMMENT "Trying to download/refresh databases from '${BOB_DATABASE_URL}/${BOB_DATABASE_VERSION}' to '${CMAKE_SOURCE_DIR}/databases'")
 
-add_custom_target(db-refresh DEPENDS db-try-download
-  COMMAND ${CMAKE_BINARY_DIR}/bin/dbmanage.py all copyfrom --verbose ${CMAKE_SOURCE_DIR}/databases COMMENT "Refreshing databases installed at '${CMAKE_BINARY_DIR}' using downloads at '${CMAKE_SOURCE_DIR}/databases'")
-
-add_custom_target(db-refresh-install DEPENDS db-try-download
-  COMMAND ${CMAKE_INSTALL_PREFIX}/bin/dbmanage.py all copyfrom --verbose ${CMAKE_SOURCE_DIR}/databases COMMENT "Refreshing databases installed at '${CMAKE_INSTALL_PREFIX}' using downloads at '${CMAKE_SOURCE_DIR}/databases'")
-
 add_custom_target(db-create
   COMMAND ${CMAKE_BINARY_DIR}/bin/dbmanage.py all create --recreate --verbose ${CMAKE_SOURCE_DIR}/databases COMMENT "Creating databases at '${CMAKE_BINARY_DIR}' from scratch")
