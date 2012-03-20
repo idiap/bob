a513f8b17612c8ffb12fbea06ca06937e2cecd73
Manuel Guenther <manuel.guenther@idiap.ch>
2012-03-20 19:44:01 +0100

    Corrected bug in the scale function; changed scale test to be more aggressive; generated new reference images.

--- cxx/ip/test/scale.cc
+++ cxx/ip/test/scale.cc
@@ -77,16 +77,6 @@ void checkBlitzEqual( blitz::Array<T,2>& t1, blitz::Array<U,2>& t2)
       BOOST_CHECK_EQUAL(t1(i,j), bob::core::cast<T>(t2(i,j)));
 }
 
-template<typename T, typename U>  
-void checkBlitzEqual( blitz::Array<T,3>& t1, blitz::Array<U,3>& t2) 
-{
-  check_dimensions( t1, t2);
-  for( int i=0; i<t1.extent(0); ++i)
-    for( int j=0; j<t1.extent(1); ++j)
-      for( int k=0; k<t1.extent(2); ++k)
-        BOOST_CHECK_EQUAL(t1(i,j,k), bob::core::cast<T>(t2(i,j,k)));
-}
-
 
 template<typename T, typename U>  
 void checkBlitzClose( blitz::Array<T,2>& t1, blitz::Array<U,2>& t2, 
@@ -96,35 +86,13 @@ void checkBlitzClose( blitz::Array<T,2>& t1, blitz::Array<U,2>& t2,
   int x_min = std::min( t1.extent(1), t2.extent(1));
   BOOST_CHECK_LE( abs(t1.extent(0)-t2.extent(0)), 1);
   BOOST_CHECK_LE( abs(t1.extent(1)-t2.extent(1)), 1);
-  double diff = 0.;
   for( int i=0; i<y_min; ++i)
     for( int j=0; j<x_min; ++j)
-      diff += abs( t1(i,j) - bob::core::cast<T>(t2(i,j)) );
-  diff = (diff/(y_min*x_min)) / 
-    (std::numeric_limits<T>::max()-std::numeric_limits<T>::min()+1);
-  BOOST_CHECK_SMALL( diff, eps );
+      BOOST_CHECK_EQUAL(t1(i,j), bob::core::cast<T>(t2(i,j)));
 }
 
-template<typename T, typename U>  
-void checkBlitzClose( blitz::Array<T,3>& t1, blitz::Array<U,3>& t2, 
-  const double eps )
-{
-  int p_min = std::min( t1.extent(0), t2.extent(0));
-  int y_min = std::min( t1.extent(1), t2.extent(1));
-  int x_min = std::min( t1.extent(2), t2.extent(2));
-  BOOST_CHECK_EQUAL( t1.extent(0), t2.extent(0) );
-  BOOST_CHECK_LE( abs(t1.extent(1)-t2.extent(1)), 1);
-  BOOST_CHECK_LE( abs(t1.extent(2)-t2.extent(2)), 1);
-  double diff = 0.;
-  for( int i=0; i<p_min; ++i)
-    for( int j=0; j<y_min; ++j)
-      for( int k=0; k<x_min; ++k)
-        diff += abs( t1(i,j,k) - bob::core::cast<T>(t2(i,j,k)) );
-  diff = (diff/(y_min*x_min*p_min)) / 
-    (std::numeric_limits<T>::max()-std::numeric_limits<T>::min()+1);
-  BOOST_CHECK_SMALL( diff, eps );
-}
 
+// #define REGENERATE_REFERENCE_IMAGES
 
 BOOST_FIXTURE_TEST_SUITE( test_setup, T )
 
@@ -153,27 +121,39 @@ BOOST_AUTO_TEST_CASE( test_scale_2d_generic_uint8 )
   bob::ip::scale( img, img_processed, bob::ip::Rescale::BilinearInterp);
   testdata_path_img = testdata_cpath;
   testdata_path_img /= "image_s137x137.pgm";
+#ifdef REGENERATE_REFERENCE_IMAGES
+  bob::io::Array (bob::core::cast<uint8_t>(img_processed)).save(testdata_path_img.string());
+#else // REGENERATE_REFERENCE_IMAGES
   bob::io::Array ar_img_s137(testdata_path_img.string());
   blitz::Array<uint8_t,2> img_ref_s137 = ar_img_s137.get<uint8_t,2>();
   checkBlitzClose( img_ref_s137, img_processed, eps);
+#endif // REGENERATE_REFERENCE_IMAGES
 
   // 77x77
   img_processed.resize(77,77); 
   bob::ip::scale( img, img_processed, bob::ip::Rescale::BilinearInterp);
   testdata_path_img = testdata_cpath;
   testdata_path_img /= "image_s77x77.pgm";
+#ifdef REGENERATE_REFERENCE_IMAGES
+  bob::io::Array (bob::core::cast<uint8_t>(img_processed)).save(testdata_path_img.string());
+#else // REGENERATE_REFERENCE_IMAGES
   bob::io::Array ar_img_s77(testdata_path_img.string());
   blitz::Array<uint8_t,2> img_ref_s77 = ar_img_s77.get<uint8_t,2>();
   checkBlitzClose( img_ref_s77, img_processed, eps);
+#endif // REGENERATE_REFERENCE_IMAGES
 
   // 125x75
   img_processed.resize(125,75);
   bob::ip::scale( img, img_processed, bob::ip::Rescale::BilinearInterp);
   testdata_path_img = testdata_cpath;
   testdata_path_img /= "image_s125x75.pgm";
+#ifdef REGENERATE_REFERENCE_IMAGES
+  bob::io::Array (bob::core::cast<uint8_t>(img_processed)).save(testdata_path_img.string());
+#else // REGENERATE_REFERENCE_IMAGES
   bob::io::Array ar_img_s125x75(testdata_path_img.string());
   blitz::Array<uint8_t,2> img_ref_s125x75 = ar_img_s125x75.get<uint8_t,2>();
   checkBlitzClose( img_ref_s125x75, img_processed, eps);
+#endif // REGENERATE_REFERENCE_IMAGES
 
   // 100x100
   img_processed.resize(100,100); 
