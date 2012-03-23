d97c5dba7c013ad8401ebbebdcbb2728612787ed
Manuel Guenther <manuel.guenther@idiap.ch>
2012-03-23 13:01:32 +0100

    corrected parameter name of convert function.

--- python/core/lib/test/convert.py
+++ python/core/lib/test/convert.py
@@ -36,20 +36,20 @@ class ConversionTest(unittest.TestCase):
   def xtest02_from_range(self):
   
     x = numpy.array(range(6), 'uint8').reshape(2,3)
-    c = bob.core.array.convert(x, 'uint16', sourceRange=(0,255))
+    c = bob.core.array.convert(x, 'uint16', source_range=(0,255))
     self.assertTrue( numpy.array_equal(x.astype('float64'), c) )
 
   def test03_to_range(self):
   
     x = numpy.array(range(6), 'uint8').reshape(2,3)
-    c = bob.core.array.convert(x, 'float64', destRange=(0.,255.))
+    c = bob.core.array.convert(x, 'float64', dest_range=(0.,255.))
     self.assertTrue( numpy.array_equal(x.astype('float64'), c) )
 
   def test04_from_and_to_range(self):
   
     x = numpy.array(range(6), 'uint8').reshape(2,3)
-    c = bob.core.array.convert(x, 'float64', sourceRange=(0,255),
-        destRange=(0.,255.))
+    c = bob.core.array.convert(x, 'float64', source_range=(0,255),
+        dest_range=(0.,255.))
     self.assertTrue( numpy.array_equal(x.astype('float64'), c) )
 
 # Instantiates our standard main module for unittests
