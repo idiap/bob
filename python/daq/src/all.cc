cc30bcada27fca398ef2a309c0d6b717f765b333
Manuel Guenther <manuel.guenther@idiap.ch>
2012-03-26 11:01:50 +0200

    Corrected python function and parameter names of the daq system.

--- python/daq/src/all.cc
+++ python/daq/src/all.cc
@@ -111,41 +111,41 @@ void bind_daq_all() {
          "model"))
     .def("start", &CaptureSystem::start,(arg("self")), "Start the capture system. This call is blocking.")
     .add_property("length", &CaptureSystem::getLength, &CaptureSystem::setLength, "Recording length in seconds (recording delay excluded)")
-    .add_property("recordingDelay",  &CaptureSystem::getRecordingDelay,  &CaptureSystem::setRecordingDelay, "Recording delay in seconds, i.e. amount of seconds before the recording begins")
-    .add_property("outputName", &CaptureSystem::getOutputName, &CaptureSystem::setOutputName, "Output name")
-    .add_property("outputDir", &CaptureSystem::getOutputDir, &CaptureSystem::setOutputDir, "Directory where captured images are written")
+    .add_property("recording_delay",  &CaptureSystem::getRecordingDelay,  &CaptureSystem::setRecordingDelay, "Recording delay in seconds, i.e. amount of seconds before the recording begins")
+    .add_property("output_name", &CaptureSystem::getOutputName, &CaptureSystem::setOutputName, "Output name")
+    .add_property("output_dir", &CaptureSystem::getOutputDir, &CaptureSystem::setOutputDir, "Directory where captured images are written")
     .add_property("thumbnail", &CaptureSystem::getThumbnail, &CaptureSystem::setThumbnail, "Path to an image, displayed as thumbnail in the GUI")
     .add_property("fullscreen", &CaptureSystem::getFullScreen, &CaptureSystem::setFullScreen, "GUI should be fullscreen")
-    .def("setDisplaySize", &CaptureSystem::setDisplaySize, (arg("self"), arg("width"), arg("height")), "GUI size (ignored if fullscreen)")
-    .def("setExecuteOnStartRecording", &CaptureSystem::setExecuteOnStartRecording, (arg("self"), arg("command")), "Shell command executed when the recording starts\n. Warning: The command blocks the GUI thread. You should execute time * consuming commands in a sub-shell (e.g. command params &)")
-    .def("setExecuteOnStopRecording", &CaptureSystem::setExecuteOnStopRecording, (arg("self"), arg("command")), "Shell command executed when the recording stops.\n Warning: See setExecuteOnStartRecording()")
-    .def("setText", &CaptureSystem::setText, (arg("self"), arg("text")), "Custom text displayed in the GUI");
+    .def("set_display_size", &CaptureSystem::setDisplaySize, (arg("self"), arg("width"), arg("height")), "GUI size (ignored if fullscreen)")
+    .def("set_execute_on_start_recording", &CaptureSystem::setExecuteOnStartRecording, (arg("self"), arg("command")), "Shell command executed when the recording starts\n. Warning: The command blocks the GUI thread. You should execute time * consuming commands in a sub-shell (e.g. command params &)")
+    .def("set_execute_on_stop_recording", &CaptureSystem::setExecuteOnStopRecording, (arg("self"), arg("command")), "Shell command executed when the recording stops.\n Warning: See setExecuteOnStartRecording()")
+    .def("set_text", &CaptureSystem::setText, (arg("self"), arg("text")), "Custom text displayed in the GUI");
 #endif
   
   /// Callbacks
   class_<ControllerCallback, boost::noncopyable>("ControllerCallback", "Callback provided by a Controller", no_init)
-    .def("imageReceived", &ControllerCallback::imageReceived, (arg("self"), arg("image"), arg("status")), "Image received by the Controller.\n 'image': pixel array in RGB 24 format.\n 'status': information about the frame");
+    .def("image_received", &ControllerCallback::imageReceived, (arg("self"), arg("image"), arg("status")), "Image received by the Controller.\n 'image': pixel array in RGB 24 format.\n 'status': information about the frame");
 
   class_<Stoppable, boost::noncopyable>("Stoppable", no_init)
     .def("stop", &Stoppable::stop);
 
   class_<Camera::CameraCallback, boost::noncopyable>("CameraCallback", "Callback provided by Camera", no_init)
     .def("imageReceived", &Camera::CameraCallback::imageReceived,
-      (arg("self"), arg("image"), arg("pixelFormat"), arg("width"), arg("height"), arg("stride"), arg("size"), arg("frameNb"), arg("timestamp")),
+      (arg("self"), arg("image"), arg("pixel_format"), arg("width"), arg("height"), arg("stride"), arg("size"), arg("frame_nb"), arg("timestamp")),
          "Image received by the camera. The implementation should be as short as"
          "possible since the capture thread is blocked during this call.\n"
          "\n"
          "'image': image buffer\n"
-         "'pixelFormat': pixel format of the image. \n"
+         "'pixel_format': pixel format of the image. \n"
          "'width': image width. \n"
          "'height': image height. \n"
          "'stride': image stride. \n"
          "'size': total image size (usually height*stride). \n"
-         "'frameNb': frame number. \n"
+         "'frame_nb': frame number. \n"
          "'timestamp': frame timestamp (in seconds). \n");
 
   class_<FaceLocalizationCallback, boost::noncopyable>("FaceLocalizationCallback", no_init)
-    .def("onDetection", &FaceLocalizationCallback::onDetection, (arg("boundingbox")));
+    .def("on_detection", &FaceLocalizationCallback::onDetection, (arg("boundingbox")));
     
   /// Cameras
   enum_<Camera::PixelFormat>("PixelFormat")
@@ -169,15 +169,15 @@ void bind_daq_all() {
     
   class_<Camera, bases<Stoppable>, boost::noncopyable>("Camera", "Camera is an abstract class which captures frames", no_init)
     .def("start", &Camera::start, (arg("self")))
-    .def("addCameraCallback", &Camera::addCameraCallback, (arg("self"), arg("callback")))
-    .def("removeCameraCallback", &Camera::removeCameraCallback, (arg("self"), arg("callback")))
-    .def("getSupportedPixelFormats", &getSupportedPixelFormats, (arg("self")), "Get the list of supported pixel formats")
-    .def("getSupportedFrameSizes", &getSupportedFrameSizes, (arg("self"), arg("pixelFormat")), "Get the list of supported frame sizes for a pixel format")
-    .def("getSupportedFrameIntervals", &getSupportedFrameIntervals, (arg("self"), arg("pixelFormat"), arg("frameSize")), "Get the list of supported frame intervals for a pixel format and a frame size")
-    .add_property("pixelFormat", &Camera::getPixelFormat, &Camera::setPixelFormat)
-    .add_property("frameSize", &Camera::getFrameSize, &Camera::setFrameSize)
-    .add_property("frameInterval", &Camera::getFrameInterval, &Camera::setFrameInterval)
-    .def("printSummary", &Camera::printSummary, (arg("self")), "Print information about the device");
+    .def("add_camera_callback", &Camera::addCameraCallback, (arg("self"), arg("callback")))
+    .def("remove_camera_callback", &Camera::removeCameraCallback, (arg("self"), arg("callback")))
+    .def("get_supported_pixel_formats", &getSupportedPixelFormats, (arg("self")), "Get the list of supported pixel formats")
+    .def("get_supported_frame_sizes", &getSupportedFrameSizes, (arg("self"), arg("pixel_format")), "Get the list of supported frame sizes for a pixel format")
+    .def("get_supported_frame_intervals", &getSupportedFrameIntervals, (arg("self"), arg("pixel_format"), arg("frame_size")), "Get the list of supported frame intervals for a pixel format and a frame size")
+    .add_property("pixel_format", &Camera::getPixelFormat, &Camera::setPixelFormat)
+    .add_property("frame_size", &Camera::getFrameSize, &Camera::setFrameSize)
+    .add_property("frame_interval", &Camera::getFrameInterval, &Camera::setFrameInterval)
+    .def("print_summary", &Camera::printSummary, (arg("self")), "Print information about the device");
 
 #ifdef V4L2
   class_<V4LCamera, bases<Camera>, boost::noncopyable>("V4LCamera", "Capture frames using Video for Linux 2",
@@ -185,16 +185,16 @@ void bind_daq_all() {
 #endif
   
   class_<VideoReaderCamera, bases<Camera>, boost::noncopyable>("VideoReaderCamera", "Read a video file from a bob::io::VideoReader",
-                                                               init<boost::shared_ptr<bob::io::VideoReader> >((arg("videoReader"))));
+                                                               init<boost::shared_ptr<bob::io::VideoReader> >((arg("video_reader"))));
 
 
   /// OutputWriters
   class_<OutputWriter, boost::noncopyable>("OutputWriter", "OutputWriter is an abstract class which provides a way to write frames on the hard drive.", no_init)
     .def("open", &OutputWriter::open, (arg("self")))
     .def("close", &OutputWriter::close, (arg("self")))
-    .def("writeFrame", &OutputWriter::writeFrame, (arg("self"), arg("image"), arg("frameNb"), arg("timestamp")), "Write a frame on the hard drive. \n'image': pixels in RGB24 format. \n'frameNb': frame number. \n'timestamp': frame timestamp in seconds.")
-    .def("setOutputDir", &OutputWriter::setOutputDir, (arg("self"), arg("dir")), "Set the directory where we want to output")
-    .def("setOutputName", &OutputWriter::setOutputName, (arg("self"), arg("name")), "Set the name used to identify the output files");
+    .def("writeFrame", &OutputWriter::writeFrame, (arg("self"), arg("image"), arg("frame_nb"), arg("timestamp")), "Write a frame on the hard drive. \n'image': pixels in RGB24 format. \n'frame_nb': frame number. \n'timestamp': frame timestamp in seconds.")
+    .def("set_output_dir", &OutputWriter::setOutputDir, (arg("self"), arg("dir")), "Set the directory where we want to output")
+    .def("set_output_name", &OutputWriter::setOutputName, (arg("self"), arg("name")), "Set the name used to identify the output files");
 
     class_<BobOutputWriter, bases<OutputWriter>, boost::noncopyable>("BobOutputWriter",
                                                                      "Write a video file using Bob. Two files are created: .avi (contains the video with a fixed fps) and .txt (contains the timestamps for each frame)");
@@ -205,13 +205,13 @@ void bind_daq_all() {
                                                                           "responsible to forward captured images to other classes, and have to convert "
                                                                           "it to RGB24 format if needed",
                                                                           no_init)
-    .def("addControllerCallback", &Controller::addControllerCallback, (arg("self"), arg("callback")))
-    .def("removeControllerCallback", &Controller::removeControllerCallback, (arg("self"), arg("callback")))
-    .def("addStoppable", &Controller::addStoppable, (arg("self"), arg("stoppable")), "Add classes that should be stopped in priority (i.e. before ControllerCallback classes)")
-    .def("removeStoppable", &Controller::removeStoppable, (arg("self"), arg("stoppable")))
-    .add_property("recordingDelay", &Controller::getRecordingDelay, &Controller::setRecordingDelay, "Recording delay in seconds, i.e. amount of seconds before the recording begins.")
+    .def("add_controller_callback", &Controller::addControllerCallback, (arg("self"), arg("callback")))
+    .def("remove_controller_callback", &Controller::removeControllerCallback, (arg("self"), arg("callback")))
+    .def("add_stoppable", &Controller::addStoppable, (arg("self"), arg("stoppable")), "Add classes that should be stopped in priority (i.e. before ControllerCallback classes)")
+    .def("remove_stoppable", &Controller::removeStoppable, (arg("self"), arg("stoppable")))
+    .add_property("recording_delay", &Controller::getRecordingDelay, &Controller::setRecordingDelay, "Recording delay in seconds, i.e. amount of seconds before the recording begins.")
     .add_property("length", &Controller::getLength, &Controller::setLength, "Recording length in seconds (recording delay excluded)")
-    .def("setOutputWriter", &Controller::setOutputWriter, (arg("self"), arg("outputWriter")));
+    .def("set_output_writer", &Controller::setOutputWriter, (arg("self"), arg("output_writer")));
 
     class_<SimpleController, bases<Controller>, boost::noncopyable>("SimpleController", "Default Controller implementation");
 
@@ -219,14 +219,14 @@ void bind_daq_all() {
     class_<bob::daq::Display, bases<ControllerCallback, FaceLocalizationCallback>, boost::noncopyable>("Display", "Display is an abstract class which is responsible to display an interface to the user", no_init)
     .def("start", &bob::daq::Display::start, (arg("self")), "Start the interface. This call should be blocking")
     .def("stop", &bob::daq::Display::stop, (arg("self")), "")
-    .def("addKeyPressCallback", &bob::daq::Display::addKeyPressCallback, (arg("self"), arg("callback")), "Add a callback which listen to user keyboard interactions.")
-    .def("removeKeyPressCallback", &bob::daq::Display::removeKeyPressCallback, (arg("self"), arg("callback")))
-    .def("setThumbnail", &bob::daq::Display::setThumbnail, (arg("self"), arg("path")), "Set path to an image, displayed as thumbnail in the GUI")
-    .def("setFullscreen", &bob::daq::Display::setFullscreen, (arg("self"), arg("fullscreen")), "Set whether GUI should be fullscreen")
-    .def("setDisplaySize", &bob::daq::Display::setDisplaySize, (arg("self"), arg("width"), arg("height")), "Set GUI size (ignored if fullscreen)")
-    .def("setExecuteOnStartRecording", &bob::daq::Display::setExecuteOnStartRecording, (arg("self"), arg("command")), "Set a shell command executed when the recording starts. \nWarning: The command blocks the GUI thread. You should execute time consuming commands in a sub-shell (e.g. \"command params &\")")
-    .def("setExecuteOnStopRecording", &bob::daq::Display::setExecuteOnStopRecording, (arg("self"), arg("command")), "Set a shell command executed when the recording stops. \nWarning: See setExecuteOnStartRecording()")
-    .def("setText", &bob::daq::Display::setText, (arg("self"), arg("text")), "Set custom text displayed in the GUI");
+    .def("add_key_press_callback", &bob::daq::Display::addKeyPressCallback, (arg("self"), arg("callback")), "Add a callback which listen to user keyboard interactions.")
+    .def("remove_key_press_callback", &bob::daq::Display::removeKeyPressCallback, (arg("self"), arg("callback")))
+    .def("set_thumbnail", &bob::daq::Display::setThumbnail, (arg("self"), arg("path")), "Set path to an image, displayed as thumbnail in the GUI")
+    .def("set_fullscreen", &bob::daq::Display::setFullscreen, (arg("self"), arg("fullscreen")), "Set whether GUI should be fullscreen")
+    .def("set_display_size", &bob::daq::Display::setDisplaySize, (arg("self"), arg("width"), arg("height")), "Set GUI size (ignored if fullscreen)")
+    .def("set_execute_on_start_recording", &bob::daq::Display::setExecuteOnStartRecording, (arg("self"), arg("command")), "Set a shell command executed when the recording starts. \nWarning: The command blocks the GUI thread. You should execute time consuming commands in a sub-shell (e.g. \"command params &\")")
+    .def("set_execute_on_stop_recording", &bob::daq::Display::setExecuteOnStopRecording, (arg("self"), arg("command")), "Set a shell command executed when the recording stops. \nWarning: See setExecuteOnStartRecording()")
+    .def("set_text", &bob::daq::Display::setText, (arg("self"), arg("text")), "Set custom text displayed in the GUI");
 
   class_<QtDisplay, bases<bob::daq::Display>, boost::noncopyable>("QtDisplay", "Display a GUI using Qt");
 
@@ -236,8 +236,8 @@ void bind_daq_all() {
   /// FaceLocalizations
   class_<FaceLocalization, bases<ControllerCallback>, boost::noncopyable>("FaceLocalization", "FaceLocalization is an abstract class which provides face localization", no_init)
     .def("start", &FaceLocalization::start, (arg("self")), "Start the face localization of incoming frames")
-    .def("addFaceLocalizationCallback", &FaceLocalization::addFaceLocalizationCallback, (arg("self"), arg("callback")))
-    .def("removeFaceLocalizationCallback", &FaceLocalization::removeFaceLocalizationCallback, (arg("self"), arg("callback")));
+    .def("add_face_localization_callback", &FaceLocalization::addFaceLocalizationCallback, (arg("self"), arg("callback")))
+    .def("remove_face_localization_callback", &FaceLocalization::removeFaceLocalizationCallback, (arg("self"), arg("callback")));
 
 #ifdef VISIONER
   class_<VisionerFaceLocalization, bases<FaceLocalization>, boost::noncopyable>("VisionerFaceLocalization", "Provide face localization using Visioner",
