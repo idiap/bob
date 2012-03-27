9b8963e4005c1e2e637d322034b9792d2f28598e
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2012-03-27 17:15:26 +0200

    Add message when VLFeat is not found (Ticket #38)

--- external/vlfeatConfig.cmake
+++ external/vlfeatConfig.cmake
@@ -6,6 +6,13 @@ if(VLFEAT_INCLUDE_DIR AND VLFEAT_LIBRARY)
   add_definitions("-D HAVE_VLFEAT=1")
   set(VLFEAT_FOUND TRUE)
   find_package_message(VLFEAT "Found VLFeat: ${VLFEAT_LIBRARY}" "[${VLFEAT_LIBRARY}][${VLFEAT_INCLUDE_DIR}]")
+else()
+  if(NOT VLFEAT_INCLUDE_DIR)
+    message( STATUS "VLFeat include file <vl/sift.h> not found!")
+  endif()
+  if(NOT VLFEAT_LIBRARY)
+    message( STATUS "VLFeat library not found!")
+  endif()
 endif()
 
 MARK_AS_ADVANCED(
