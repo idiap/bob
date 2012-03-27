84960fc6c94ca85480ded192b2e62e2ba0339036
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2012-03-27 17:24:58 +0200

    cmake: externals: Disable message if package not found (ticket #38)

--- external/vlfeatConfig.cmake
+++ external/vlfeatConfig.cmake
@@ -1,4 +1,4 @@
-find_path(VLFEAT_INCLUDE_DIR vl/sift.h)
+find_path(VLFEAT_INCLUDE_DIR vl/sift2.h)
 find_library(VLFEAT_LIBRARY NAMES vl)
 
 set(VLFEAT_FOUND FALSE)
@@ -6,13 +6,6 @@ if(VLFEAT_INCLUDE_DIR AND VLFEAT_LIBRARY)
   add_definitions("-D HAVE_VLFEAT=1")
   set(VLFEAT_FOUND TRUE)
   find_package_message(VLFEAT "Found VLFeat: ${VLFEAT_LIBRARY}" "[${VLFEAT_LIBRARY}][${VLFEAT_INCLUDE_DIR}]")
-else()
-  if(NOT VLFEAT_INCLUDE_DIR)
-    message( STATUS "VLFeat include file <vl/sift.h> not found!")
-  endif()
-  if(NOT VLFEAT_LIBRARY)
-    message( STATUS "VLFeat library not found!")
-  endif()
 endif()
 
 MARK_AS_ADVANCED(
