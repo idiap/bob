23901eb5778a0d0e89f69ba3094f449cee71db6e
Andre Anjos <andre.anjos@idiap.ch>
2012-07-09 14:59:39 +0200

    Another try on the fix to matio detection

--- external/matioConfig.cmake
+++ external/matioConfig.cmake
@@ -8,12 +8,10 @@ if(matio_FOUND)
   add_definitions("-DHAVE_MATIO=1")
 
   include(CheckCSourceCompiles)
-  set(CMAKE_REQUIRED_INCLUDES ${matio_INCLUDE_DIR})
+  set(CMAKE_REQUIRED_INCLUDES "${matio_INCLUDEDIR}")
   set(CMAKE_REQUIRED_LIBRARIES ${matio_LIBRARIES})
-  set(CMAKE_REQUIRED_FLAGS ${matio_LDFLAGS})
   CHECK_C_SOURCE_COMPILES("#include <matio.h> 
     int main() { struct ComplexSplit s; }" HAVE_MATIO_OLD_COMPLEXSPLIT)
-  set(CMAKE_REQUIRED_FLAGS)
   set(CMAKE_REQUIRED_LIBRARIES)
   set(CMAKE_REQUIRED_INCLUDES)
 
