7bfa03188f72b48e0e8ab4c3f6440fdb96e757bc
Manuel Guenther <manuel.guenther@idiap.ch>
2012-07-20 16:21:24 +0200

    Corrected bug in the bob.math.normalized_scalar_product function.

--- python/math/lib/__init__.py
+++ python/math/lib/__init__.py
@@ -2,6 +2,7 @@ from ._math import *
 
 # add some default distance functions
 import math
+import numpy
 
 def euclidean_distance(x1, x2):
   """Returns the Euclidean distance between the given vectors"""
@@ -9,9 +10,11 @@ def euclidean_distance(x1, x2):
 
 
 def normalized_scalar_product(x1, x2):
-  """Computes the normalized scalar product (also known as the cosine between the two vectors)"""
+  """Computes the normalized scalar product (also known as the cosine between the two vectors). 
+  This is a similarity measure, the results are in the range [-1,1], 
+  or [0,1] when both vectors only have positive coefficients."""
   # Since this is actually a similarity function, we return its negative
-  return 1. - numpy.dot(x1, x2) / math.sqrt(numpy.dot(x1,x1) * numpy.dot(x2,x2))
+  return numpy.dot(x1, x2) / math.sqrt(numpy.dot(x1,x1) * numpy.dot(x2,x2))
 
 
 __all__ = dir()
