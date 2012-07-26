7393a8baea643ea5184e9194eaa7e37f87cee399
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2012-07-26 21:54:44 +0200

    IP Gamma correction: add more pythonic versions

--- python/ip/src/gamma_correction.cc
+++ python/ip/src/gamma_correction.cc
@@ -27,33 +27,82 @@
 using namespace boost::python;
 
 template <typename T, int N>
-static void inner_gammaCorrection(bob::python::const_ndarray src, 
+static void inner_gammaCorrection_c(bob::python::const_ndarray src, 
   bob::python::ndarray dst, const double g) 
 {
   blitz::Array<double,N> dst_ = dst.bz<double,N>();
   bob::ip::gammaCorrection<T>(src.bz<T,N>(), dst_, g);
 }
 
-static void gamma_correction(bob::python::const_ndarray src, 
+static void py_gamma_correction_c(bob::python::const_ndarray src,
   bob::python::ndarray dst, const double g)
 {
   const bob::core::array::typeinfo& info = src.type();
 
-  if (info.nd != 2) PYTHON_ERROR(TypeError, "gamma_correction() does not support input array with '%ld' dimensions.", info.nd);
+  if (info.nd != 2) 
+    PYTHON_ERROR(TypeError, 
+      "bob.ip.gamma_correction() does not support input array with '%ld' \
+       dimensions.",
+      info.nd);
 
   switch (info.dtype) {
     case bob::core::array::t_uint8: 
-      return inner_gammaCorrection<uint8_t,2>(src, dst, g);
+      return inner_gammaCorrection_c<uint8_t,2>(src, dst, g);
     case bob::core::array::t_uint16:
-      return inner_gammaCorrection<uint16_t,2>(src, dst, g);
+      return inner_gammaCorrection_c<uint16_t,2>(src, dst, g);
     case bob::core::array::t_float64:
-      return inner_gammaCorrection<double,2>(src, dst, g);
+      return inner_gammaCorrection_c<double,2>(src, dst, g);
     default:
-      PYTHON_ERROR(TypeError, "gamma_correction() does not support input array of type '%s'.", info.str().c_str());
+      PYTHON_ERROR(TypeError, 
+        "bob.ip.gamma_correction() does not support input array of \
+         type '%s'.",
+        info.str().c_str());
   }
 }
 
+template <typename T, int N>
+static object inner_gammaCorrection_p(bob::python::const_ndarray src,
+  const double g) 
+{
+  const bob::core::array::typeinfo& info = src.type();
+  bob::python::ndarray dst(bob::core::array::t_float64, info.shape[0],
+    info.shape[1]);
+  blitz::Array<double,N> dst_ = dst.bz<double,N>();
+  bob::ip::gammaCorrection<T>(src.bz<T,N>(), dst_, g);
+  return dst.self();
+}
+
+static object py_gamma_correction_p(bob::python::const_ndarray src,
+  const double g)
+{
+  const bob::core::array::typeinfo& info = src.type();
+
+  if (info.nd != 2) 
+    PYTHON_ERROR(TypeError, 
+      "bob.ip.gamma_correction() does not support input array with '%ld' \
+       dimensions.",
+      info.nd);
+
+  switch (info.dtype) {
+    case bob::core::array::t_uint8: 
+      return inner_gammaCorrection_p<uint8_t,2>(src, g);
+    case bob::core::array::t_uint16:
+      return inner_gammaCorrection_p<uint16_t,2>(src, g);
+    case bob::core::array::t_float64:
+      return inner_gammaCorrection_p<double,2>(src, g);
+    default:
+      PYTHON_ERROR(TypeError, 
+        "bob.ip.gamma_correction() does not support input array of \
+         type '%s'.",
+        info.str().c_str());
+  }
+}
 void bind_ip_gamma_correction() 
 {
-  def("gamma_correction", &gamma_correction, (arg("src"), arg("dst"), arg("gamma")), "Performs a power-law gamma correction on a 2D blitz array/image.");
+  def("gamma_correction", 
+    &py_gamma_correction_c, (arg("src"), arg("dst"), arg("gamma")), 
+    "Performs a power-law gamma correction on a 2D blitz array/image.");
+  def("gamma_correction", 
+    &py_gamma_correction_p, (arg("src"), arg("gamma")), 
+    "Performs a power-law gamma correction on a 2D blitz array/image. The output is allocated and returned.");
 }
