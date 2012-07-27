eb8dfb76c71475dcb76fcea564a670a875d0cd33
Andre Anjos <andre.dos.anjos@gmail.com>
2012-07-27 17:25:33 +0200

    Adds proper header with licensing information for Bob modules

--- cxx/visioner/programs/trainer.cc
+++ cxx/visioner/programs/trainer.cc
@@ -1,3 +1,27 @@
+/**
+ * @file visioner/programs/trainer.cc
+ * @date Fri 27 Jul 13:58:57 2012 CEST
+ * @author Andre Anjos <andre.anjos@idiap.ch>
+ *
+ * @brief This file was part of Visioner and originally authored by "Cosmin
+ * Atanasoaei <cosmin.atanasoaei@idiap.ch>". It was only modified to conform to
+ * Bob coding standards and structure.
+ *
+ * Copyright (C) 2011-2012 Idiap Research Institute, Martigny, Switzerland
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "visioner/util/timer.h"
 #include "visioner/model/mdecoder.h"
 #include "visioner/model/sampler.h"
