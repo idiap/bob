743813ea07fbd696b10330cf7fdbd18aec03aacb
Andre Anjos <andre.anjos@idiap.ch>
2012-08-02 12:10:45 +0200

    Fix ignore files, re-include ctags

---
+++ bin/ctags.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+# Andre Anjos <andre.anjos@idiap.ch>
+# Thu 02 Aug 2012 11:52:36 CEST
+
+install_dir=$(readlink -f $(dirname $(dirname $0)));
+files=$(find ${install_dir}/cxx ${install_dir}/python -name '*.h' -or -name '*.cc' -or -name '*.c' -or -name '*.cxx' -or -name '*.cpp' -or -name '*.C' -or -name '*.CC')
+ctags --output=${install_dir}/ctags ${files}
