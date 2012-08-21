4fb65ac5c88815895e4c1233245cddafc59694d6
Andre Anjos <andre.dos.anjos@gmail.com>
2012-08-21 09:43:10 +0200

    Trial testing infrastructure

---
+++ python/io/lib/test/__init__.py
@@ -0,0 +1,5 @@
+from .video import *
+from .hdf5 import *
+from .file import *
+from .arrayset import *
+from .array import *
---
+++ python/math/lib/test/__init__.py
@@ -0,0 +1,5 @@
+from .histogram import *
+from .interiorpointLP import *
+from .linsolve import *
+from .norminv import *
+from .stats import *
