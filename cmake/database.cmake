7149ee7c12c0e58eb7d2855eabc12edc2a2a8673
Andre Anjos <andre.dos.anjos@gmail.com>
2012-08-23 12:14:47 +0200

    Remove all traces of downloadable databases in Bob

--- cmake/database.cmake
+++
@@ -1,7 +0,0 @@
-# New targets for building/downloading and cloning databases
-
-add_custom_target(db-download
-  COMMAND ${CMAKE_SOURCE_DIR}/bin/dbdownload.py --verbose --server=${BOB_DATABASE_URL} --version=${BOB_DATABASE_VERSION} --destination=${CMAKE_SOURCE_DIR}/databases COMMENT "Downloading/refreshing databases from '${BOB_DATABASE_URL}/${BOB_DATABASE_VERSION}' to '${CMAKE_SOURCE_DIR}/databases'")
-
-add_custom_target(db-try-download
-  COMMAND ${CMAKE_SOURCE_DIR}/bin/dbdownload.py --verbose --server=${BOB_DATABASE_URL} --version=${BOB_DATABASE_VERSION} --destination=${CMAKE_SOURCE_DIR}/databases --try COMMENT "Trying to download/refresh databases from '${BOB_DATABASE_URL}/${BOB_DATABASE_VERSION}' to '${CMAKE_SOURCE_DIR}/databases'")
