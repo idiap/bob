48de2dfad86e2c1487815e0096e45558e584fdd4
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2012-08-24 18:15:28 +0200

    Median filter: clean C++ tests (ticket #75)

--- cxx/ip/test/Median.cc
+++ cxx/ip/test/Median.cc
@@ -25,20 +25,7 @@
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
 #include <blitz/array.h>
-#include <stdint.h>
-#include "core/logging.h"
-#include "core/convert.h"
 #include "ip/Median.h"
-
-#include "io/Array.h"
-#include <algorithm>
-
-#include <random/discrete-uniform.h>
-#include <random/uniform.h>
-
-#include <boost/filesystem.hpp>
-
-#include <iostream>
 
 struct T {
   double eps;
@@ -63,54 +50,6 @@ void checkBlitzEqual( blitz::Array<T,2>& t1, blitz::Array<U,2>& t2)
   for( int i=0; i<t1.extent(0); ++i)
     for( int j=0; j<t1.extent(1); ++j)
       BOOST_CHECK_EQUAL(t1(i,j), bob::core::cast<T>(t2(i,j)));
-}
-
-template<typename T, typename U>  
-void checkBlitzEqual( blitz::Array<T,3>& t1, blitz::Array<U,3>& t2) 
-{
-  check_dimensions( t1, t2);
-  for( int i=0; i<t1.extent(0); ++i)
-    for( int j=0; j<t1.extent(1); ++j)
-      for( int k=0; k<t1.extent(2); ++k)
-        BOOST_CHECK_EQUAL(t1(i,j,k), bob::core::cast<T>(t2(i,j,k)));
-}
-
-
-template<typename T>  
-void checkBlitzClose( blitz::Array<T,2>& t1, blitz::Array<T,2>& t2, 
-  const double eps )
-{
-  int y_min = std::min( t1.extent(0), t2.extent(0));
-  int x_min = std::min( t1.extent(1), t2.extent(1));
-  BOOST_CHECK_LE( abs(t1.extent(0)-t2.extent(0)), 1);
-  BOOST_CHECK_LE( abs(t1.extent(1)-t2.extent(1)), 1);
-  double diff = 0.;
-  for( int i=0; i<y_min; ++i)
-    for( int j=0; j<x_min; ++j)
-      diff += abs( t1(i,j) - t2(i,j) );
-  diff = (diff/(y_min*x_min)) / 
-    (std::numeric_limits<T>::max()-std::numeric_limits<T>::min()+1);
-  BOOST_CHECK_SMALL( diff, eps );
-}
-
-template<typename T>  
-void checkBlitzClose( blitz::Array<T,3>& t1, blitz::Array<T,3>& t2, 
-  const double eps )
-{
-  int p_min = std::min( t1.extent(0), t2.extent(0));
-  int y_min = std::min( t1.extent(1), t2.extent(1));
-  int x_min = std::min( t1.extent(2), t2.extent(2));
-  BOOST_CHECK_EQUAL( t1.extent(0), t2.extent(0) );
-  BOOST_CHECK_LE( abs(t1.extent(1)-t2.extent(1)), 1);
-  BOOST_CHECK_LE( abs(t1.extent(2)-t2.extent(2)), 1);
-  double diff = 0.;
-  for( int i=0; i<p_min; ++i)
-    for( int j=0; j<y_min; ++j)
-      for( int k=0; k<x_min; ++k)
-        diff += abs( t1(i,j,k) - t2(i,j,k) );
-  diff = (diff/(y_min*x_min*p_min)) / 
-    (std::numeric_limits<T>::max()-std::numeric_limits<T>::min()+1);
-  BOOST_CHECK_SMALL( diff, eps );
 }
 
 
