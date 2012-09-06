6c7a4ae56026580ab4f7d8c52ef1d458396f75eb
Andre Anjos <andre.anjos@idiap.ch>
2012-09-06 12:48:55 +0200

    Update tags generation

--- bin/ctags.sh
+++ bin/ctags.sh
@@ -3,5 +3,5 @@
 # Thu 02 Aug 2012 11:52:36 CEST
 
 install_dir=$(readlink -f $(dirname $(dirname $0)));
-files=$(find ${install_dir}/cxx ${install_dir}/python -name '*.h' -or -name '*.cc' -or -name '*.c' -or -name '*.cxx' -or -name '*.cpp' -or -name '*.C' -or -name '*.CC')
+files=$(find ${install_dir}/src ${install_dir}/python -name '*.h' -or -name '*.cc' -or -name '*.c' -or -name '*.cxx' -or -name '*.cpp' -or -name '*.C' -or -name '*.CC')
 ctags --output=${install_dir}/tags ${files}
