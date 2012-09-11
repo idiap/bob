ebdf9a75d69c88458031eeefdbb169e31240a1bf
Andre Anjos <andre.anjos@idiap.ch>
2012-09-11 18:40:08 +0200

    Adds the possibility to create a read-only db session, if the python implementation permits it

--- python/bob/db/utils.py
+++ python/bob/db/utils.py
@@ -11,6 +11,7 @@ import sys
 import errno
 import stat
 import logging
+import sqlite3
 
 class null(object):
   """A look-alike stream that discards the input"""
@@ -25,24 +26,91 @@ class null(object):
 
     pass
 
+def sqlite3_accepts_uris():
+  """Checks lock-ability for SQLite on the current file system"""
+
+  try:
+    sqlite3.connect(':memory:', uri=True)
+    return True
+  except TypeError:
+    return False
+
+class SQLiteConnector(object):
+  '''An object that handles the connection to SQLite databases.'''
+
+  SQLITE3_WITH_URIS = sqlite3_accepts_uris()
+
+  def __init__(self, filename, readonly=False, lock=None):
+
+    opts = {}
+    if readonly: opts['mode'] = 'ro'
+    if isinstance(lock, (str, unicode)): opts['vfs'] =  lock
+
+    if opts and not self.SQLITE3_WITH_URIS:
+      import warnings
+      warnings.warn('Got a request for a connection string to an SQLite session with options, but SQLite connection options are not supported at the installed version of Python (check http://bugs.python.org/issue13773 for a discussion and a patch). Returning a standard connection string.' % ('.'.join(sqlite_version),))
+      opts = {}
+
+    if self.SQLITE3_WITH_URIS:
+      ostr = ''
+      if opts:
+        ostr = '?' + '&'.join([k + '=' + v for (k, v) in opts.iteritems()])
+      self.uri = 'file:' + filename + ostr
+    else:
+      self.uri = filename
+    
+  def __call__(self):
+
+    if self.SQLITE3_WITH_URIS:
+      return sqlite3.connect(self.uri, uri=True)
+
+    return sqlite3.connect(self.uri)
+
 def session(dbtype, dbfile, echo=False):
   """Creates a session to an SQLite database"""
 
   from sqlalchemy import create_engine
   from sqlalchemy.orm import sessionmaker
 
-  engine = create_engine(connection_string(dbtype, dbfile), echo=echo)
+  url = connection_string(dbtype, dbfile)
+  engine = create_engine(url, echo=echo)
   Session = sessionmaker(bind=engine)
   return Session()
 
-def connection_string(dbtype, dbfile):
-  """Returns a connection string for supported platforms"""
+def session_readonly(dbtype, dbfile, echo=False):
+  """Creates a session to an SQLite database.
+  
+  Raises a RuntimeError if the file does not exist.
+  """
 
-  if dbtype == 'sqlite': return 'sqlite:///' + dbfile
+  if dbtype != 'sqlite':
+    raise NotImplemented, "Read-only sessions are only currently supported for SQLite databases"
+
+  if not os.path.exists(dbfile):
+    raise RuntimeError, "Cannot open **read-only** SQLite session to a file that does not exist (%s)" % dbfile
+
+  from sqlalchemy import create_engine
+  from sqlalchemy.orm import sessionmaker
+
+  engine = create_engine('sqlite://', creator=SQLiteConnector(dbfile, readonly=True, lock='unix-none'))
+  Session = sessionmaker(bind=engine)
+  return Session()
+
+def connection_string(dbtype, dbfile, opts={}):
+  """Returns a connection string for supported platforms
+  
+  Keyword parameters
+
+  dbtype
+    The type of database (only 'sqlite' is supported for the time being)
+
+  dbfile
+    The location of the file to be used
+  """
+
+  from sqlalchemy.engine.url import URL
+  return URL(dbtype, database=dbfile)
     
-  # otherwise, we just raise
-  raise RuntimeError, "Cannot create connection strings for database types different than 'sqlite' and you have type='%s'" % (dbtype,)
-
 def makedirs_safe(fulldir, dryrun=False):
   """Creates a directory if it does not exists, with concurrent access support"""
   try:
