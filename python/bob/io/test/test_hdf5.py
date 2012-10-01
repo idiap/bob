e707e47502c8a6b42f166bdffef922be01dd5a99
Andre Anjos <andre.dos.anjos@gmail.com>
2012-10-01 18:31:43 +0200

    Add specific attribute tests

--- python/bob/io/test/test_hdf5.py
+++ python/bob/io/test/test_hdf5.py
@@ -74,62 +74,66 @@ class HDF5FileTest(unittest.TestCase):
   """Performs various tests for the bob::io::HDF5File type."""
  
   def test01_CanCreate(self):
+
     # This test demonstrates how to create HDF5 files from scratch,
     # starting from blitz::Arrays
 
-    # We start by creating some arrays to play with. Please note that in normal
-    # cases you are either generating these arrays or reading from other binary
-    # files or datasets.
-    N = 2 
-    SHAPE = (3, 2) #6 elements
-    NELEMENT = SHAPE[0] * SHAPE[1]
-    arrays = []
-    for k in range(N):
-      data = [int(random.uniform(0,10)) for z in range(NELEMENT)]
-      arrays.append(numpy.array(data, 'int32').reshape(SHAPE))
+    try:
 
-    # Now we create a new binary output file in a temporary location and save
-    # the data there.
-    tmpname = get_tempfilename()
-    outfile = bob.io.HDF5File(tmpname, 'w')
-    outfile.append('testdata', arrays)
+      # We start by creating some arrays to play with. Please note that in
+      # normal cases you are either generating these arrays or reading from
+      # other binary files or datasets.
+      N = 2 
+      SHAPE = (3, 2) #6 elements
+      NELEMENT = SHAPE[0] * SHAPE[1]
+      arrays = []
+      for k in range(N):
+        data = [int(random.uniform(0,10)) for z in range(NELEMENT)]
+        arrays.append(numpy.array(data, 'int32').reshape(SHAPE))
 
-    # Data that is thrown in the file is immediately accessible, so you can
-    # interleave read and write operations without any problems.
-    # There is a single variable in the file, which is a bob arrayset:
-    self.assertEqual(outfile.paths(), ['/testdata'])
-    
-    # And all the data is *exactly* the same recorded, bit by bit
-    back = outfile.lread('testdata') # this is how to read the whole data back
-    for i, b in enumerate(back):
-      self.assertTrue( numpy.array_equal(b, arrays[i]) )
+      # Now we create a new binary output file in a temporary location and save
+      # the data there.
+      tmpname = get_tempfilename()
+      outfile = bob.io.HDF5File(tmpname, 'w')
+      outfile.append('testdata', arrays)
 
-    # If you want to immediately close the HDF5 file, just delete the object
-    del outfile
+      # Data that is thrown in the file is immediately accessible, so you can
+      # interleave read and write operations without any problems.
+      # There is a single variable in the file, which is a bob arrayset:
+      self.assertEqual(outfile.paths(), ['/testdata'])
+      
+      # And all the data is *exactly* the same recorded, bit by bit
+      back = outfile.lread('testdata') # this is how to read the whole data back
+      for i, b in enumerate(back):
+        self.assertTrue( numpy.array_equal(b, arrays[i]) )
 
-    # You can open the file in read-only mode using the 'r' flag. Writing
-    # operations on this file will fail.
-    readonly = bob.io.HDF5File(tmpname, 'r')
+      # If you want to immediately close the HDF5 file, just delete the object
+      del outfile
 
-    # There is a single variable in the file, which is a bob arrayset:
-    self.assertEqual(readonly.paths(), ['/testdata'])
+      # You can open the file in read-only mode using the 'r' flag. Writing
+      # operations on this file will fail.
+      readonly = bob.io.HDF5File(tmpname, 'r')
 
-    # You can get an overview of what is in the HDF5 dataset using the
-    # describe() method
-    description = readonly.describe('testdata')
+      # There is a single variable in the file, which is a bob arrayset:
+      self.assertEqual(readonly.paths(), ['/testdata'])
 
-    self.assertTrue(description[0].type.compatible(arrays[0]))
-    self.assertEqual(description[0].size, N)
+      # You can get an overview of what is in the HDF5 dataset using the
+      # describe() method
+      description = readonly.describe('testdata')
 
-    # Test that writing will really fail
-    self.assertRaises(RuntimeError, readonly.append, "testdata", arrays[0])
+      self.assertTrue(description[0].type.compatible(arrays[0]))
+      self.assertEqual(description[0].size, N)
 
-    # And all the data is *exactly* the same recorded, bit by bit
-    back = readonly.lread('testdata') # this is how to read the whole data back
-    for i, b in enumerate(back):
-      self.assertTrue( numpy.array_equal(b, arrays[i]) )
+      # Test that writing will really fail
+      self.assertRaises(RuntimeError, readonly.append, "testdata", arrays[0])
 
-    os.unlink(tmpname)
+      # And all the data is *exactly* the same recorded, bit by bit
+      back = readonly.lread('testdata') # how to read the whole data back
+      for i, b in enumerate(back):
+        self.assertTrue( numpy.array_equal(b, arrays[i]) )
+
+    finally:
+      os.unlink(tmpname)
 
   def test02_TypeSupport(self):
 
@@ -137,118 +141,126 @@ class HDF5FileTest(unittest.TestCase):
     # from to HDF5 files. One single file will hold all data for this test.
     # This is also supported with HDF5: multiple variables in a single file.
 
-    N = 100 
+    try:
 
-    tmpname = get_tempfilename()
-    outfile = bob.io.HDF5File(tmpname, 'w')
+      N = 100 
 
-    data = [bool(int(random.uniform(0,2))) for z in range(N)]
-    self.readWriteTest(outfile, 'bool_data', data)
-    data = [int(random.uniform(0,100)) for z in range(N)]
-    self.readWriteTest(outfile, 'int_data', data)
-    self.readWriteTest(outfile, 'int8_data', data, numpy.int8)
-    self.readWriteTest(outfile, 'uint8_data', data, numpy.uint8)
-    self.readWriteTest(outfile, 'int16_data', data, numpy.int16)
-    self.readWriteTest(outfile, 'uint16_data', data, numpy.uint16)
-    self.readWriteTest(outfile, 'int32_data', data, numpy.int32)
-    self.readWriteTest(outfile, 'uint32_data', data, numpy.uint32)
+      tmpname = get_tempfilename()
+      outfile = bob.io.HDF5File(tmpname, 'w')
 
-    data = [long(random.uniform(0,1000000000)) for z in range(N)]
-    self.readWriteTest(outfile, 'long_data', data)
-    self.readWriteTest(outfile, 'int64_data', data, numpy.int64)
-    self.readWriteTest(outfile, 'uint64_data', data, numpy.uint64)
+      data = [bool(int(random.uniform(0,2))) for z in range(N)]
+      self.readWriteTest(outfile, 'bool_data', data)
+      data = [int(random.uniform(0,100)) for z in range(N)]
+      self.readWriteTest(outfile, 'int_data', data)
+      self.readWriteTest(outfile, 'int8_data', data, numpy.int8)
+      self.readWriteTest(outfile, 'uint8_data', data, numpy.uint8)
+      self.readWriteTest(outfile, 'int16_data', data, numpy.int16)
+      self.readWriteTest(outfile, 'uint16_data', data, numpy.uint16)
+      self.readWriteTest(outfile, 'int32_data', data, numpy.int32)
+      self.readWriteTest(outfile, 'uint32_data', data, numpy.uint32)
 
-    data = [float(random.uniform(0,1)) for z in range(N)]
-    self.readWriteTest(outfile, 'float_data', data, float)
-    #Note that because of double => float precision issues, the next test will
-    #fail. Python floats are actually double precision.
-    #self.readWriteTest(outfile, 'float32_data', data, numpy.float32)
-    self.readWriteTest(outfile, 'float64_data', data, numpy.float64)
-    #The next construction is not supported by bob
-    #self.readWriteTest(outfile, 'float128_data', data, numpy.float128)
+      data = [long(random.uniform(0,1000000000)) for z in range(N)]
+      self.readWriteTest(outfile, 'long_data', data)
+      self.readWriteTest(outfile, 'int64_data', data, numpy.int64)
+      self.readWriteTest(outfile, 'uint64_data', data, numpy.uint64)
 
-    data = [complex(random.uniform(0,1),random.uniform(-1,0)) for z in range(N)]
-    self.readWriteTest(outfile, 'complex_data', data, complex)
-    #Note that because of double => float precision issues, the next test will
-    #fail. Python floats are actually double precision.
-    #self.readWriteTest(outfile, 'complex64_data', data, numpy.complex64)
-    self.readWriteTest(outfile, 'complex128_data', data, numpy.complex128)
-    #The next construction is not supported by bob
-    #self.readWriteTest(outfile, 'complex256_data', data, numpy.complex256)
+      data = [float(random.uniform(0,1)) for z in range(N)]
+      self.readWriteTest(outfile, 'float_data', data, float)
+      #Note that because of double => float precision issues, the next test will
+      #fail. Python floats are actually double precision.
+      #self.readWriteTest(outfile, 'float32_data', data, numpy.float32)
+      self.readWriteTest(outfile, 'float64_data', data, numpy.float64)
+      #The next construction is not supported by bob
+      #self.readWriteTest(outfile, 'float128_data', data, numpy.float128)
 
-    self.readWriteTestArray(outfile, numpy.int8)
-    self.readWriteTestArray(outfile, numpy.int16)
-    self.readWriteTestArray(outfile, numpy.int32)
-    self.readWriteTestArray(outfile, numpy.int64)
-    self.readWriteTestArray(outfile, numpy.uint8)
-    self.readWriteTestArray(outfile, numpy.uint16)
-    self.readWriteTestArray(outfile, numpy.uint32)
-    self.readWriteTestArray(outfile, numpy.uint64)
-    self.readWriteTestArray(outfile, numpy.float32)
-    self.readWriteTestArray(outfile, numpy.float64)
-    #self.readWriteTestArray(outfile, numpy.float128) #no numpy conversion
-    self.readWriteTestArray(outfile, numpy.complex64)
-    self.readWriteTestArray(outfile, numpy.complex128)
-    #self.readWriteTestArray(outfile, numpy.complex256) #no numpy conversion
+      data = [complex(random.uniform(0,1),random.uniform(-1,0)) for z in range(N)]
+      self.readWriteTest(outfile, 'complex_data', data, complex)
+      #Note that because of double => float precision issues, the next test will
+      #fail. Python floats are actually double precision.
+      #self.readWriteTest(outfile, 'complex64_data', data, numpy.complex64)
+      self.readWriteTest(outfile, 'complex128_data', data, numpy.complex128)
+      #The next construction is not supported by bob
+      #self.readWriteTest(outfile, 'complex256_data', data, numpy.complex256)
 
-    os.unlink(tmpname)
+      self.readWriteTestArray(outfile, numpy.int8)
+      self.readWriteTestArray(outfile, numpy.int16)
+      self.readWriteTestArray(outfile, numpy.int32)
+      self.readWriteTestArray(outfile, numpy.int64)
+      self.readWriteTestArray(outfile, numpy.uint8)
+      self.readWriteTestArray(outfile, numpy.uint16)
+      self.readWriteTestArray(outfile, numpy.uint32)
+      self.readWriteTestArray(outfile, numpy.uint64)
+      self.readWriteTestArray(outfile, numpy.float32)
+      self.readWriteTestArray(outfile, numpy.float64)
+      #self.readWriteTestArray(outfile, numpy.float128) #no numpy conversion
+      self.readWriteTestArray(outfile, numpy.complex64)
+      self.readWriteTestArray(outfile, numpy.complex128)
+      #self.readWriteTestArray(outfile, numpy.complex256) #no numpy conversion
+
+    finally:
+      os.unlink(tmpname)
 
   def test03_DatasetManagement(self):
 
-    # This test examplifies dataset management within HDF5 files and how to
-    # copy, delete and move data around.
+    try:
 
-    # Let's just create some dummy data to play with
-    N = 100 
+      # This test examplifies dataset management within HDF5 files and how to
+      # copy, delete and move data around.
 
-    tmpname = get_tempfilename()
-    outfile = bob.io.HDF5File(tmpname, 'w')
+      # Let's just create some dummy data to play with
+      N = 100 
 
-    data = [int(random.uniform(0,N)) for z in range(N)]
-    outfile.append('int_data', data)
+      tmpname = get_tempfilename()
+      outfile = bob.io.HDF5File(tmpname, 'w')
 
-    # This is how to rename a dataset.
-    outfile.rename('int_data', 'MyRenamedDataset')
-    
-    # You can move the Dataset to any other hierarchy in the HDF5 file. The
-    # directory structure within the file (i.e. the HDF5 groups) will be
-    # created on demand.
-    outfile.rename('MyRenamedDataset', 'NewDirectory1/Dir2/MyDataset')
+      data = [int(random.uniform(0,N)) for z in range(N)]
+      outfile.append('int_data', data)
 
-    # Let's move the MyDataset dataset to another directory
-    outfile.rename('NewDirectory1/Dir2/MyDataset', 'Test2/Bla')
+      # This is how to rename a dataset.
+      outfile.rename('int_data', 'MyRenamedDataset')
+      
+      # You can move the Dataset to any other hierarchy in the HDF5 file. The
+      # directory structure within the file (i.e. the HDF5 groups) will be
+      # created on demand.
+      outfile.rename('MyRenamedDataset', 'NewDirectory1/Dir2/MyDataset')
 
-    # So, now the original dataset name does not exist anymore
-    self.assertEqual(outfile.paths(), ['/Test2/Bla'])
+      # Let's move the MyDataset dataset to another directory
+      outfile.rename('NewDirectory1/Dir2/MyDataset', 'Test2/Bla')
 
-    # We can also unlink the dataset from the file. Please note this will not
-    # erase the data in the file, just make it inaccessible
-    outfile.unlink('Test2/Bla')
-    
-    # Finally, nothing is there anymore
-    self.assertEqual(outfile.paths(), [])
+      # So, now the original dataset name does not exist anymore
+      self.assertEqual(outfile.paths(), ['/Test2/Bla'])
 
-    os.unlink(tmpname)
+      # We can also unlink the dataset from the file. Please note this will not
+      # erase the data in the file, just make it inaccessible
+      outfile.unlink('Test2/Bla')
+      
+      # Finally, nothing is there anymore
+      self.assertEqual(outfile.paths(), [])
+
+    finally:
+      os.unlink(tmpname)
 
   def test04_resizeAndPreserve(self):
     
     # This test checks that non-contiguous C-style array can be saved
     # into an HDF5 file.
 
-    # Let's just create some dummy data to play with
-    SHAPE = (2, 3) #6 elements
-    NELEMENT = SHAPE[0] * SHAPE[1]
-    data = [int(random.uniform(0,10)) for z in range(NELEMENT)]
-    array = numpy.array(data, 'int32').reshape(SHAPE)
+    try:
+      # Let's just create some dummy data to play with
+      SHAPE = (2, 3) #6 elements
+      NELEMENT = SHAPE[0] * SHAPE[1]
+      data = [int(random.uniform(0,10)) for z in range(NELEMENT)]
+      array = numpy.array(data, 'int32').reshape(SHAPE)
 
-    # Try to save a slice
-    tmpname = get_tempfilename()
-    bob.io.save(array[:,0], tmpname)
+      # Try to save a slice
+      tmpname = get_tempfilename()
+      bob.io.save(array[:,0], tmpname)
 
-    os.unlink(tmpname)
+    finally:
+      os.unlink(tmpname)
 
   def test05_canLoadMatlab(self):
-
+    
     # shows we can load a 2D matlab array and interpret it as a bunch of 1D
     # arrays, correctly
 
@@ -284,10 +296,41 @@ class HDF5FileTest(unittest.TestCase):
     mfile = bob.io.load(F('test7_unlimited.hdf5'))
     self.assertTrue ( mfile.ndim == 2 )
 
-  def test08_attribute_simple(self):
+  def test08_attribute_version(self):
 
-    tmpname = get_tempfilename()
-    outfile = bob.io.HDF5File(tmpname, 'w')
-    outfile.set_attribute('version', 32)
-    self.assertEqual(outfile.get_attribute('version'), 32)
-    os.unlink(tmpname)
+    try:
+      tmpname = get_tempfilename()
+      outfile = bob.io.HDF5File(tmpname, 'w')
+      outfile.set_attribute('version', 32)
+      self.assertEqual(outfile.get_attribute('version'), 32)
+
+    finally:
+      os.unlink(tmpname)
+  
+  def test09_attribute_complex(self):
+
+    try:
+      tmpname = get_tempfilename()
+      outfile = bob.io.HDF5File(tmpname, 'w')
+      attribute = numpy.array([complex(32,29), complex(55,21), complex(-22,44.5), complex(17,19)]).reshape(2,2)
+      outfile.set_attribute('complicated', attribute)
+      recovered = outfile.get_attribute('complicated')
+      self.assertTrue(numpy.array_equal(recovered, attribute))
+
+    finally:
+      os.unlink(tmpname)
+
+  def test10_attribute_dataset(self):
+    
+    try:
+      tmpname = get_tempfilename()
+      outfile = bob.io.HDF5File(tmpname, 'w')
+      data = numpy.array([1,3,2,4,7,9.2]).reshape(3,2)
+      outfile.set('data', data)
+      attribute = numpy.array([complex(32,29), complex(55,21), complex(-22,44.5), complex(17,19)]).reshape(2,2)
+      outfile.set_attribute('complicated', attribute, 'data')
+      recovered = outfile.get_attribute('complicated', 'data')
+      self.assertTrue(numpy.array_equal(recovered, attribute))
+
+    finally:
+      os.unlink(tmpname)
