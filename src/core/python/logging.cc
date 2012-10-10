84af249a8ac97b448b58b935fd8152d797a02125
Andre Anjos <andre.dos.anjos@gmail.com>
2012-10-10 09:47:50 +0200

    Fix logging destruction issue
    * Found when building for Ubuntu 12.10 (Quantal)
    * This fix is certainly related to issue #83
    * This fix definetely also closes issues related to Idiap's bug: https://secure.idiap.ch/bugzilla3/show_bug.cgi?id=7763

--- src/core/python/logging.cc
+++ src/core/python/logging.cc
@@ -23,6 +23,7 @@
 #include <pthread.h>
 #include <boost/python.hpp>
 
+#include "bob/core/python/ndarray.h"
 #include "bob/core/python/gil.h"
 #include "bob/core/logging.h"
 
@@ -43,17 +44,28 @@ struct PythonLoggingOutputDevice: public bob::core::OutputDevice {
      * @param callable A python callable object. Can be a function or an object
      * that implements the __call__() slot.
      */
-    PythonLoggingOutputDevice(object callable): m_callable(callable) { }
+    PythonLoggingOutputDevice(object callable): 
+      m_callable(callable) { }
 
     /**
      * D'tor
      */
-    virtual ~PythonLoggingOutputDevice() {}
+    virtual ~PythonLoggingOutputDevice() {
+      close();
+    }
+
+    /**
+     * Closes this stream for good
+     */
+    virtual void close() {
+      m_callable = object(); //set to None
+    }
 
     /**
      * Writes a message to the callable.
      */
     virtual inline std::streamsize write(const char* s, std::streamsize n) {
+      if (TPY_ISNONE(m_callable)) return 0;
       std::string value(s);
       if (std::isspace(value[n-1])) { //remove accidental newlines in the end
         value = value.substr(0, n-1);
@@ -74,14 +86,24 @@ static void log_message(bob::core::OutputStream& s, const std::string& message) 
   s << message << std::endl;
 }
 
+/**
+ * Gets rid of the Python stuff before we destroy the Python interpreter
+ */
+static void outputstream_del(bob::core::OutputStream& os) {
+  os.reset("null");
+}
+
 void bind_core_logging() {
   class_<bob::core::OutputDevice, boost::shared_ptr<bob::core::OutputDevice>, boost::noncopyable>("OutputDevice", "OutputDevices act like sinks for the messages emitted from within C++", no_init); 
 
-  class_<PythonLoggingOutputDevice, boost::shared_ptr<PythonLoggingOutputDevice>, bases<bob::core::OutputDevice> >("PythonLoggingOutputDevice", "The PythonLoggingOutputDevice is the default logging class for bob.core.OutputStream objects to be used in python. It diverges the output of logged messages in C++ into the pythonic logging module.", init<object>("Initializes the PythonLoggingOutputDevice with a new callable that will be used to emit messages."));
+  class_<PythonLoggingOutputDevice, boost::shared_ptr<PythonLoggingOutputDevice>, bases<bob::core::OutputDevice> >("PythonLoggingOutputDevice", "The PythonLoggingOutputDevice is the default logging class for bob.core.OutputStream objects to be used in python. It diverges the output of logged messages in C++ into the pythonic logging module.", init<object>("Initializes the PythonLoggingOutputDevice with a new callable that will be used to emit messages."))
+    .def("__del__", &PythonLoggingOutputDevice::close, (arg("self")), "Resets this stream before calling the C++ destructor")
+    ;
 
   class_<bob::core::OutputStream, boost::shared_ptr<bob::core::OutputStream> >("OutputStream", "The OutputStream object represents a normal C++ stream and is used as the basis for configuring the message output re-direction inside bob.", init<>("Constructs a new OutputStream using no parameters. Ignores any input received."))
     .def(init<const std::string&>((arg("configuration")), "Initializes this stream with one of the default C++ methods available: stdout, stderr, null or a filename (if the filename ends in '.gz', it will be compressed on the fly)."))
     .def(init<boost::shared_ptr<bob::core::OutputDevice> >((arg("device")), "Constructs a new OutputStream using the given existing OutputDevice."))
+    .def("__del__", &outputstream_del, (arg("self")), "Resets the OutputStream before the C++ destructor goes into action")
     .def("reset", &bob::core::OutputStream::reset<const std::string>, (arg("self"), arg("configuration")), "Resets the current stream to use a new method for output instead of the currently configured.")
     .def("reset", &bob::core::OutputStream::reset<boost::shared_ptr<bob::core::OutputDevice> >, (arg("self"), arg("device")), "Resets the current stream to use a new method for output instead of the currently configured. This version of the API allows you to pass an existing OutputDevice to be used for output data.")
     .def("log", &log_message, (arg("self"), arg("message")), "This method logs an arbitrary message to the current log stream")
