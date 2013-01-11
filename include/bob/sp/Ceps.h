24e69d2fcc2f2b20a5d046813b3b20cf0b8eb8e5
Elie Khoury <ekhoury@idiap.ch>
2013-01-11 17:05:37 +0100

    Ceps: fixing 1 warning, and fixing a bug occuring during test on an old version of blitz

--- include/bob/sp/Ceps.h
+++ include/bob/sp/Ceps.h
@@ -52,118 +52,118 @@ class CepsTest;
 class Ceps
 {
 public:
-	/**
-	 * @brief Constructor: Initialize working arrays
-	 */
-	Ceps( double sf, int win_length_ms, int win_shift_ms, int n_filters, int n_ceps,
-			double f_min, double f_max, double delta_win, const blitz::Array<double,1>& data_array);
+  /**
+   * @brief Constructor: Initialize working arrays
+   */
+  Ceps( double sf, int win_length_ms, int win_shift_ms, int n_filters, int n_ceps,
+      double f_min, double f_max, double delta_win, const blitz::Array<double,1>& data_array);
 
-	/**
-	 * @brief Get the Cepstral Shape
-	 */
-	blitz::TinyVector<int,2> getCepsShape(int n_size) const;
+  /**
+   * @brief Get the Cepstral Shape
+   */
+  blitz::TinyVector<int,2> getCepsShape(int n_size) const;
 
-	/**
-	 * @brief Compute Cepstral features
-	 */
-	void CepsAnalysis(int n_size, blitz::Array<double,2>& ceps_2D);
+  /**
+   * @brief Compute Cepstral features
+   */
+  void CepsAnalysis(int n_size, blitz::Array<double,2>& ceps_2D);
 
-	/**
-	 * @brief Reinitialize member variables
-	 */
-	void reinit(double dct_norm, bool fb_linear, bool withEnergy, bool withDelta,
-			bool withDeltaDelta, bool withDeltaEnergy, bool withDeltaDeltaEnergy);
+  /**
+   * @brief Reinitialize member variables
+   */
+  void reinit(double dct_norm, bool fb_linear, bool withEnergy, bool withDelta,
+      bool withDeltaDelta, bool withDeltaEnergy, bool withDeltaDeltaEnergy);
 
-	/**
-	 * @brief Destructor
-	 */
-	virtual ~Ceps();
+  /**
+   * @brief Destructor
+   */
+  virtual ~Ceps();
 
 
 private:
-	/**
-	 * @brief Compute the first derivatives
-	 */
-	void addDelta(blitz::Array<double,2>& frames, int win_size, int n_frames, int frame_size);
-	/**
-	 * @brief Compute the second derivatives
-	 */
-	void addDeltaDelta(blitz::Array<double,2>& frames, int win_size, int n_frames, int frame_size);
-	/**
-	 * @brief Mean Normalisation of the features
-	 */
-	blitz::Array<double,2> dataZeroMean(blitz::Array<double,2>& frames, bool norm_energy, int n_frames, int frame_size);
+  /**
+   * @brief Compute the first derivatives
+   */
+  void addDelta(blitz::Array<double,2>& frames, int win_size, int n_frames, int frame_size);
+  /**
+   * @brief Compute the second derivatives
+   */
+  void addDeltaDelta(blitz::Array<double,2>& frames, int win_size, int n_frames, int frame_size);
+  /**
+   * @brief Mean Normalisation of the features
+   */
+  blitz::Array<double,2> dataZeroMean(blitz::Array<double,2>& frames, bool norm_energy, int n_frames, int frame_size);
 
-	static double mel(double f);
-	static double MelInv(double f);
-	void emphasis(blitz::Array<double,1> &data,int n,double a);
-	void hammingWindow(blitz::Array<double,1> &data);
-	void logFilterBank(blitz::Array<double,1>& x);
-	void logTriangularFBank(blitz::Array<double,1>& data);
-	double logEnergy(blitz::Array<double,1> &data);
-	void transformDCT();
-	void initWindowSize();
-	void initCache();
+  static double mel(double f);
+  static double MelInv(double f);
+  void emphasis(blitz::Array<double,1> &data,int n,double a);
+  void hammingWindow(blitz::Array<double,1> &data);
+  void logFilterBank(blitz::Array<double,1>& x);
+  void logTriangularFBank(blitz::Array<double,1>& data);
+  double logEnergy(blitz::Array<double,1> &data);
+  void transformDCT();
+  void initWindowSize();
+  void initCache();
 
-	double m_sf;
-	int m_win_length;
-	int m_win_shift;
-	int m_win_size;
-	int m_nfilters;
-	int m_nceps;
-	double m_f_min;
-	double m_f_max;
-	int m_delta_win;
-	blitz::Array<double,1> m_data_array;
-	bool m_fb_linear;
-	double m_dct_norm;
-	bool m_withEnergy;
-	bool m_withDelta;
-	bool m_withDeltaDelta;
-	bool m_withDeltaEnergy;
-	bool m_withDeltaDeltaEnergy;
-	blitz::Array<double,2> m_dct_kernel;
-	blitz::Array<double,1> m_hamming_kernel;
-	blitz::Array<double,1> m_frame;
-	blitz::Array<double,1> m_filters;
-	blitz::Array<double,1> m_ceps_coeff;
-	blitz::Array<int,1>  m_p_index;
+  double m_sf;
+  int m_win_length;
+  int m_win_shift;
+  int m_win_size;
+  int m_nfilters;
+  int m_nceps;
+  double m_f_min;
+  double m_f_max;
+  int m_delta_win;
+  blitz::Array<double,1> m_data_array;
+  bool m_fb_linear;
+  double m_dct_norm;
+  bool m_withEnergy;
+  bool m_withDelta;
+  bool m_withDeltaDelta;
+  bool m_withDeltaEnergy;
+  bool m_withDeltaDeltaEnergy;
+  blitz::Array<double,2> m_dct_kernel;
+  blitz::Array<double,1> m_hamming_kernel;
+  blitz::Array<double,1> m_frame;
+  blitz::Array<double,1> m_filters;
+  blitz::Array<double,1> m_ceps_coeff;
+  blitz::Array<int,1>  m_p_index;
 
-	friend class TestCeps;
+  friend class TestCeps;
 };
 
 class TestCeps
 {
 public:
-	TestCeps(Ceps& ceps);
-	Ceps& m_ceps;
+  TestCeps(Ceps& ceps);
+  Ceps& m_ceps;
 
-	// Methods to test
-	double mel(double f) { return m_ceps.mel(f); }
-	double MelInv(double f) { return m_ceps.MelInv(f); }
-	blitz::TinyVector<int,2> getCepsShape(int n_size) const
-													{ return m_ceps.getCepsShape( n_size); }
-	blitz::Array<double,1> getFilter(void) {return m_ceps.m_filters;}
-	blitz::Array<double,1> getFeatures(void) {return m_ceps.m_ceps_coeff;}
+  // Methods to test
+  double mel(double f) { return m_ceps.mel(f); }
+  double MelInv(double f) { return m_ceps.MelInv(f); }
+  blitz::TinyVector<int,2> getCepsShape(int n_size) const
+                          { return m_ceps.getCepsShape( n_size); }
+  blitz::Array<double,1> getFilter(void) {return m_ceps.m_filters;}
+  blitz::Array<double,1> getFeatures(void) {return m_ceps.m_ceps_coeff;}
 
-	void CepsAnalysis(  int n_size, blitz::Array<double,2>& ceps_2D)
-	{m_ceps.CepsAnalysis(n_size, ceps_2D);
-	}
-	void hammingWindow(blitz::Array<double,1>& data){ m_ceps.hammingWindow(data);}
-	void emphasis(blitz::Array<double,1>& data,int n,double a){m_ceps.emphasis(data, n, a);}
-	void logFilterBank(blitz::Array<double,1>& x){m_ceps.logFilterBank(x);}
-	void logTriangularFBank(blitz::Array<double,1>& data){m_ceps.logTriangularFBank(data);}
-	double logEnergy(blitz::Array<double,1> &data){return m_ceps.logEnergy(data);}
-	void transformDCT(){m_ceps.transformDCT();}
-	void addDelta(blitz::Array<double,2>& frames, int win_size, int n_frames, int frame_size){
-		m_ceps.addDelta(frames, win_size, n_frames, frame_size);
-	}
-	void addDeltaDelta(blitz::Array<double,2>& frames, int win_size, int n_frames, int frame_size){
-		m_ceps.addDeltaDelta(frames, win_size, n_frames, frame_size);
-	}
-	blitz::Array<double,2> dataZeroMean(blitz::Array<double,2>& frames, bool norm_energy, int n_frames, int frame_size){
-		return m_ceps.dataZeroMean(frames,norm_energy, n_frames, frame_size);
-	}
+  void CepsAnalysis(  int n_size, blitz::Array<double,2>& ceps_2D)
+  {m_ceps.CepsAnalysis(n_size, ceps_2D);
+  }
+  void hammingWindow(blitz::Array<double,1>& data){ m_ceps.hammingWindow(data);}
+  void emphasis(blitz::Array<double,1>& data,int n,double a){m_ceps.emphasis(data, n, a);}
+  void logFilterBank(blitz::Array<double,1>& x){m_ceps.logFilterBank(x);}
+  void logTriangularFBank(blitz::Array<double,1>& data){m_ceps.logTriangularFBank(data);}
+  double logEnergy(blitz::Array<double,1> &data){return m_ceps.logEnergy(data);}
+  void transformDCT(){m_ceps.transformDCT();}
+  void addDelta(blitz::Array<double,2>& frames, int win_size, int n_frames, int frame_size){
+    m_ceps.addDelta(frames, win_size, n_frames, frame_size);
+  }
+  void addDeltaDelta(blitz::Array<double,2>& frames, int win_size, int n_frames, int frame_size){
+    m_ceps.addDeltaDelta(frames, win_size, n_frames, frame_size);
+  }
+  blitz::Array<double,2> dataZeroMean(blitz::Array<double,2>& frames, bool norm_energy, int n_frames, int frame_size){
+    return m_ceps.dataZeroMean(frames,norm_energy, n_frames, frame_size);
+  }
 
 };
 
