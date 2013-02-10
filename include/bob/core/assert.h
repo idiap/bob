a6a746296713c991c54cfe44ebb7e8a6206d25ae
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-02-10 22:07:24 +0100

    Add assertSameShape() variant, reindent code, remove useless headers

--- include/bob/core/assert.h
+++ include/bob/core/assert.h
@@ -20,14 +20,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef BOB_CORE_ARRAY_ASSERT_H
-#define BOB_CORE_ARRAY_ASSERT_H
+#ifndef BOB_CORE_ASSERT_H
+#define BOB_CORE_ASSERT_H
 
-#include "bob/core/check.h"
-#include "bob/core/array_exception.h"
-#if !defined (HAVE_BLITZ_TINYVEC2_H)
-#include <blitz/tinyvec-et.h>
-#endif
+#include <bob/core/check.h>
+#include <bob/core/array_exception.h>
 
 namespace bob {
 /**
@@ -35,127 +32,140 @@ namespace bob {
  * @{
  *
  */
-  namespace core { namespace array {
-    /**
-     * @brief Checks that a blitz array has zero base indices, and throws
-     * a NonZeroBaseError exception if this is not the case.
-     */
-    template<typename T, int D>
-    void assertZeroBase(const blitz::Array<T,D>& src)
-    {
-      for( int i=0; i<src.rank(); ++i)
-        if( src.base(i)!=0 )
-          throw NonZeroBaseError( i, src.base(i));
-    }
+namespace core { namespace array {
 
-    /**
-     * @brief Checks that a blitz array has one base indices, and throws
-     * a NonOneBaseError exception if this is not the case.
-     */
-    template<typename T, int D>
-    void assertOneBase(const blitz::Array<T,D>& src)
-    {
-      for( int i=0; i<src.rank(); ++i)
-        if( src.base(i)!=1)
-          throw NonOneBaseError( i, src.base(i));
-    }
+/**
+ * @brief Checks that a blitz array has zero base indices, and throws
+ * a NonZeroBaseError exception if this is not the case.
+ */
+template<typename T, int D>
+void assertZeroBase(const blitz::Array<T,D>& src)
+{
+  for( int i=0; i<src.rank(); ++i)
+    if( src.base(i)!=0 )
+      throw NonZeroBaseError( i, src.base(i));
+}
 
-    /**
-     * @brief Checks that two blitz arrays have the same base, and throws an
-     * DifferentBaseError exception if this is not the case.
-     */
-    template<typename T, typename U, int D>
-    void assertSameBase(const blitz::Array<T,D>& a, 
-      const blitz::Array<U,D>& b)
-    {
-      if( !hasSameBase(a,b) )
-        throw DifferentBaseError();
-    }
+/**
+ * @brief Checks that a blitz array has one base indices, and throws
+ * a NonOneBaseError exception if this is not the case.
+ */
+template<typename T, int D>
+void assertOneBase(const blitz::Array<T,D>& src)
+{
+  for( int i=0; i<src.rank(); ++i)
+    if( src.base(i)!=1)
+      throw NonOneBaseError( i, src.base(i));
+}
 
-    /**
-     * @brief Checks that a blitz array is a C-style array stored contiguously
-     * in memory, and throws a NonCContiguousError exception if this is not 
-     * the case.
-     */
-    template<typename T, int D>
-    void assertCContiguous(const blitz::Array<T,D>& src)
-    {
-      if( !isCContiguous(src) )
-        throw NonCContiguousError();
-    }
+/**
+ * @brief Checks that two blitz arrays have the same base, and throws an
+ * DifferentBaseError exception if this is not the case.
+ */
+template<typename T, typename U, int D>
+void assertSameBase(const blitz::Array<T,D>& a, 
+  const blitz::Array<U,D>& b)
+{
+  if( !hasSameBase(a,b) )
+    throw DifferentBaseError();
+}
 
-    /**
-     * @brief Checks that a blitz array is a Fortran-style array stored 
-     * contiguously in memory, and throws a NonCContiguousError exception if 
-     * this is not the case.
-     */
-    template<typename T, int D>
-    void assertFortranContiguous(const blitz::Array<T,D>& src)
-    {
-      if( !isFortranContiguous(src) )
-        throw NonFortranContiguousError();
-    }
+/**
+ * @brief Checks that a blitz array is a C-style array stored contiguously
+ * in memory, and throws a NonCContiguousError exception if this is not 
+ * the case.
+ */
+template<typename T, int D>
+void assertCContiguous(const blitz::Array<T,D>& src)
+{
+  if( !isCContiguous(src) )
+    throw NonCContiguousError();
+}
 
-    /**
-     * @brief Checks that a blitz array is a C-style array stored contiguously
-     * in memory with zero base indices, and throws a 
-     * NonCContiguousError/NonZeroBaseError exception if this is not the case.
-     */
-    template<typename T, int D>
-    void assertCZeroBaseContiguous(const blitz::Array<T,D>& src)
-    {
-      assertZeroBase(src);
-      assertCContiguous(src);
-    }
+/**
+ * @brief Checks that a blitz array is a Fortran-style array stored 
+ * contiguously in memory, and throws a NonCContiguousError exception if 
+ * this is not the case.
+ */
+template<typename T, int D>
+void assertFortranContiguous(const blitz::Array<T,D>& src)
+{
+  if( !isFortranContiguous(src) )
+    throw NonFortranContiguousError();
+}
 
-    /**
-     * @brief Checks that a blitz array is a Fortran-style array stored 
-     * contiguously in memory with one base indices, and throws a 
-     * NonFortranContiguousError/NonZeroBaseError exception if this 
-     * is not the case.
-     */
-    template<typename T, int D>
-    void assertFortranOneBaseContiguous(const blitz::Array<T,D>& src)
-    {
-      assertOneBase(src);
-      assertFortranContiguous(src);
-    }
+/**
+ * @brief Checks that a blitz array is a C-style array stored contiguously
+ * in memory with zero base indices, and throws a 
+ * NonCContiguousError/NonZeroBaseError exception if this is not the case.
+ */
+template<typename T, int D>
+void assertCZeroBaseContiguous(const blitz::Array<T,D>& src)
+{
+  assertZeroBase(src);
+  assertCContiguous(src);
+}
 
-    /**
-     * @brief Checks that a blitz array has the expected shape, and throws an
-     * UnexpectedShapeError exception if this is not the case.
-     */
-    template<typename T, int D>
-    void assertSameShape(const blitz::Array<T,D>& ar, 
-      const blitz::TinyVector<int, D>& shape)
-    {
-      if( !hasSameShape(ar,shape) )
-        throw UnexpectedShapeError();
-    }
+/**
+ * @brief Checks that a blitz array is a Fortran-style array stored 
+ * contiguously in memory with one base indices, and throws a 
+ * NonFortranContiguousError/NonZeroBaseError exception if this 
+ * is not the case.
+ */
+template<typename T, int D>
+void assertFortranOneBaseContiguous(const blitz::Array<T,D>& src)
+{
+  assertOneBase(src);
+  assertFortranContiguous(src);
+}
 
-    /**
-     * @brief Checks that two blitz arrays have the same shape, and throws an
-     * UnexpectedShapeError exception if this is not the case.
-     */
-    template<typename T, typename U, int D>
-    void assertSameShape(const blitz::Array<T,D>& a, 
-      const blitz::Array<U,D>& b)
-    {
-      if( !hasSameShape(a,b) )
-        throw UnexpectedShapeError();
-    }
+/**
+ * @brief Checks that a blitz array has the expected shape, and throws an
+ * UnexpectedShapeError exception if this is not the case.
+ */
+template<typename T, int D>
+void assertSameShape(const blitz::Array<T,D>& ar, 
+  const blitz::TinyVector<int, D>& shape)
+{
+  if( !hasSameShape(ar,shape) )
+    throw UnexpectedShapeError();
+}
 
-    /**
-     * @brief Checks that two dimensions (values) have the same length (value),
-     * and throws an UnexpectedShapeError exception if this is not the case.
-     */
-    inline void assertSameDimensionLength(const int d1, const int d2)
-    {
-      if( d1!=d2 )
-        throw UnexpectedShapeError();
-    }
+/**
+ * @brief Checks that a blitz array has the expected shape, and throws an
+ * UnexpectedShapeError exception if this is not the case.
+ */
+template<typename T, int D>
+void assertSameShape(const blitz::TinyVector<int,D>& shape, 
+  const blitz::Array<T,D>& ar)
+{
+  if( !hasSameShape(shape,ar) )
+    throw UnexpectedShapeError();
+}
 
-  }}
+/**
+ * @brief Checks that two blitz arrays have the same shape, and throws an
+ * UnexpectedShapeError exception if this is not the case.
+ */
+template<typename T, typename U, int D>
+void assertSameShape(const blitz::Array<T,D>& a, 
+  const blitz::Array<U,D>& b)
+{
+  if( !hasSameShape(a,b) )
+    throw UnexpectedShapeError();
+}
+
+/**
+ * @brief Checks that two dimensions (values) have the same length (value),
+ * and throws an UnexpectedShapeError exception if this is not the case.
+ */
+inline void assertSameDimensionLength(const int d1, const int d2)
+{
+  if( d1!=d2 )
+    throw UnexpectedShapeError();
+}
+
+}}
 /**
  * @}
  */
