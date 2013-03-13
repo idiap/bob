7f1df9e3028c937932ae432c308e985276e4fb74
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-03-13 18:57:38 +0100

    Remove namespace abbreviations (cf. #27)

--- src/io/cxx/TensorFile.cc
+++ src/io/cxx/TensorFile.cc
@@ -20,24 +20,20 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "bob/core/logging.h"
-#include "bob/core/array_type.h"
+#include <bob/core/logging.h>
+#include <bob/core/array_type.h>
 
-#include "bob/io/TensorFile.h"
-#include "bob/io/reorder.h"
+#include <bob/io/TensorFile.h>
+#include <bob/io/reorder.h>
 
-namespace io = bob::io;
-namespace core = bob::core;
-namespace ca = bob::core::array;
-
-io::TensorFile::TensorFile(const std::string& filename, 
-    io::TensorFile::openmode flag):
+bob::io::TensorFile::TensorFile(const std::string& filename, 
+    bob::io::TensorFile::openmode flag):
   m_header_init(false),
   m_current_array(0),
   m_n_arrays_written(0),
   m_openmode(flag)
 {
-  if((flag & io::TensorFile::out) && (flag & io::TensorFile::in)) {
+  if((flag & bob::io::TensorFile::out) && (flag & bob::io::TensorFile::in)) {
     m_stream.open(filename.c_str(), std::ios::in | std::ios::out | 
         std::ios::binary);
     if(m_stream)
@@ -47,14 +43,14 @@ io::TensorFile::TensorFile(const std::string& filename,
       m_header_init = true;
       m_n_arrays_written = m_header.m_n_samples;
 
-      if (flag & io::TensorFile::append) {
+      if (flag & bob::io::TensorFile::append) {
         m_stream.seekp(0, std::ios::end);
         m_current_array = m_header.m_n_samples;
       }
     }
   }
-  else if(flag & io::TensorFile::out) {  
-    if(m_stream && (flag & io::TensorFile::append)) {
+  else if(flag & bob::io::TensorFile::out) {  
+    if(m_stream && (flag & bob::io::TensorFile::append)) {
       m_stream.open(filename.c_str(), std::ios::out | std::ios::in |
           std::ios::binary);
       m_header.read(m_stream);
@@ -67,7 +63,7 @@ io::TensorFile::TensorFile(const std::string& filename,
     else
       m_stream.open(filename.c_str(), std::ios::out | std::ios::binary);
   }
-  else if(flag & io::TensorFile::in) {
+  else if(flag & bob::io::TensorFile::in) {
     m_stream.open(filename.c_str(), std::ios::in | std::ios::binary);
     if(m_stream) {
       m_header.read(m_stream);
@@ -75,36 +71,36 @@ io::TensorFile::TensorFile(const std::string& filename,
       m_header_init = true;
       m_n_arrays_written = m_header.m_n_samples;
 
-      if (flag & io::TensorFile::append) {
-        core::error << "Cannot append data in read only mode." << std::endl;
-        throw core::Exception();
+      if (flag & bob::io::TensorFile::append) {
+        bob::core::error << "Cannot append data in read only mode." << std::endl;
+        throw bob::core::Exception();
       }
     }
   }
   else
   {
-    core::error << "Invalid combination of flags." << std::endl;
-    throw core::Exception();
+    bob::core::error << "Invalid combination of flags." << std::endl;
+    throw bob::core::Exception();
   }
 }
 
-io::TensorFile::~TensorFile() {
+bob::io::TensorFile::~TensorFile() {
   close();
 }
 
-void io::TensorFile::peek(ca::typeinfo& info) const {
+void bob::io::TensorFile::peek(bob::core::array::typeinfo& info) const {
   info = m_header.m_type;
 }
 
-void io::TensorFile::close() {
+void bob::io::TensorFile::close() {
   // Rewrite the header and update the number of samples
   m_header.m_n_samples = m_n_arrays_written;
-  if(m_openmode & io::TensorFile::out) m_header.write(m_stream);
+  if(m_openmode & bob::io::TensorFile::out) m_header.write(m_stream);
 
   m_stream.close();
 }
 
-void io::TensorFile::initHeader(const ca::typeinfo& info) {
+void bob::io::TensorFile::initHeader(const bob::core::array::typeinfo& info) {
   // Check that data have not already been written
   if (m_n_arrays_written > 0 ) {
     bob::core::error << "Cannot init the header of an output stream in which data have already been written." << std::endl;
@@ -113,7 +109,7 @@ void io::TensorFile::initHeader(const ca::typeinfo& info) {
 
   // Initialize header
   m_header.m_type = info;
-  m_header.m_tensor_type = io::arrayTypeToTensorType(info.dtype);
+  m_header.m_tensor_type = bob::io::arrayTypeToTensorType(info.dtype);
   m_header.write(m_stream);
 
   // Temporary buffer to help with data transposition...
@@ -122,9 +118,9 @@ void io::TensorFile::initHeader(const ca::typeinfo& info) {
   m_header_init = true;
 }
 
-void io::TensorFile::write(const ca::interface& data) {
+void bob::io::TensorFile::write(const bob::core::array::interface& data) {
 
-  const ca::typeinfo& info = data.type();
+  const bob::core::array::typeinfo& info = data.type();
 
   if (!m_header_init) initHeader(info);
   else {
@@ -133,7 +129,7 @@ void io::TensorFile::write(const ca::interface& data) {
       throw std::runtime_error("buffer does not conform to expected type");
   }
 
-  io::row_to_col_order(data.ptr(), m_buffer.get(), info);
+  bob::io::row_to_col_order(data.ptr(), m_buffer.get(), info);
           
   m_stream.write(static_cast<const char*>(m_buffer.get()), info.buffer_size());
 
@@ -142,7 +138,7 @@ void io::TensorFile::write(const ca::interface& data) {
   if (m_current_array>m_n_arrays_written) ++m_n_arrays_written;
 }
 
-void io::TensorFile::read (ca::interface& buf) {
+void bob::io::TensorFile::read (bob::core::array::interface& buf) {
   
   if(!m_header_init) throw Uninitialized();
   if(!buf.type().is_compatible(m_header.m_type)) buf.set(m_header.m_type);
@@ -150,12 +146,12 @@ void io::TensorFile::read (ca::interface& buf) {
   m_stream.read(reinterpret_cast<char*>(m_buffer.get()), 
       m_header.m_type.buffer_size());
   
-  io::col_to_row_order(m_buffer.get(), buf.ptr(), m_header.m_type);
+  bob::io::col_to_row_order(m_buffer.get(), buf.ptr(), m_header.m_type);
 
   ++m_current_array;
 }
 
-void io::TensorFile::read (size_t index, ca::interface& buf) {
+void bob::io::TensorFile::read (size_t index, bob::core::array::interface& buf) {
   
   // Check that we are reaching an existing array
   if( index > m_header.m_n_samples ) {
