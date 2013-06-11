c25521c01e987cb42ef65d6fca0fdbf36fa56d72
Andre Anjos <andre.dos.anjos@gmail.com>
2013-06-11 14:36:56 +0200

    [MLP/Cost] Add more tests

--- python/bob/trainer/test/test_cost.py
+++ python/bob/trainer/test/test_cost.py
@@ -10,6 +10,7 @@ import numpy
 import math
 
 from .. import SquareError, CrossEntropyLoss, gradient
+from ...machine import LogisticActivation, IdentityActivation
 
 def test_square_error():
   
@@ -38,6 +39,18 @@ def test_square_error_derivative():
     absdiff = abs(op.f_prime(p,q)-gradient.estimate(op.f,p,args=(q,)))
     assert absdiff < 1e-4, 'SquareError derivative and estimation do not match to 10^-4: |%g-%g| = %g' % (op.f_prime(p,q), gradient.estimate(op.f,p,args=(q,)), absdiff)
 
+def test_square_error_error():
+  
+  op = SquareError()
+  x = numpy.random.rand(10) #10 random numbers between 0 and 1
+  y = numpy.random.rand(10) #10 random numbers between 0 and 1
+  act = LogisticActivation()
+
+  # go for an exact match
+  for p,q in zip(x,y):
+    expected = p*(1-p)*(p-q)
+    assert op.error(p,q,act) == expected, 'SquareError error does not perform as expected %g != %g' % (op.error(p,q,act), expected)
+
 def test_cross_entropy():
   
   op = CrossEntropyLoss()
@@ -62,8 +75,8 @@ def test_cross_entropy_derivative():
 
   # go for approximation
   for p,q in zip(x,y):
-    absdiff = abs(op.f_prime(p,q)-gradient.estimate(op.f,p,args=(q,)))
-    assert absdiff < 1e-4, 'SquareError derivative and estimation do not match to 10^-4: |%g-%g| = %g' % (op.f_prime(p,q), gradient.estimate(op.f,p,args=(q,)), absdiff)
+    reldiff = abs((op.f_prime(p,q)-gradient.estimate(op.f,p,args=(q,))) / op.f_prime(p,q))
+    assert reldiff < 1e-3, 'SquareError derivative and estimation do not match to 10^-4: |%g-%g| = %g' % (op.f_prime(p,q), gradient.estimate(op.f,p,args=(q,)), reldiff)
 
 def test_square_error_equality():
 
@@ -78,3 +91,27 @@ def test_cross_entropy_equality():
   op2 = CrossEntropyLoss()
 
   assert op1 == op2
+
+def test_cross_entropy_error_with_logistic():
+  
+  op = CrossEntropyLoss()
+  x = numpy.random.rand(10) #10 random numbers between 0 and 1
+  y = numpy.random.rand(10) #10 random numbers between 0 and 1
+  act = LogisticActivation()
+
+  # go for an exact match
+  for p,q in zip(x,y):
+    expected = p-q
+    assert op.error(p,q,act) == expected, 'CrossEntropyLoss+LogisticActivation error does not perform as expected %g != %g' % (op.error(p,q,act), expected)
+
+def test_cross_entropy_error_without_logistic():
+  
+  op = CrossEntropyLoss(logistic_activation=False)
+  x = numpy.random.rand(10) #10 random numbers between 0 and 1
+  y = numpy.random.rand(10) #10 random numbers between 0 and 1
+  act = IdentityActivation()
+
+  # go for an exact match
+  for p,q in zip(x,y):
+    expected = (p-q)/(p*(1-p))
+    assert op.error(p,q,act) == expected, 'CrossEntropyLoss+IdentityActivation error does not perform as expected %g != %g' % (op.error(p,q,act), expected)
