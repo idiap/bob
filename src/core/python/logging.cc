4572a1d231e910507ce52b5d45acb313bf2cf249
Andre Anjos <andre.anjos@idiap.ch>
2013-07-09 18:19:52 +0200

    [logging] New pthread locking for concurrent access

--- src/core/python/logging.cc
+++ src/core/python/logging.cc
@@ -6,26 +6,40 @@
  * @brief Bindings to re-inject C++ messages into the Python logging module
  *
  * Copyright (C) 2011-2013 Idiap Research Institute, Martigny, Switzerland
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <pthread.h>
 #include <boost/python.hpp>
+#include <boost/shared_array.hpp>
 
 #include <bob/python/ndarray.h>
 #include <bob/python/gil.h>
 #include <bob/core/logging.h>
+
+/**
+ * MT "lock" support was only introduced in Boost 1.35. Before copying this
+ * very ugly hack, make sure we are still using Boost 1.34. This will no longer
+ * be the case starting January 2011.
+ */
+#include <boost/version.hpp>
+#include <boost/thread/mutex.hpp>
+#if ((BOOST_VERSION / 100) % 1000) > 34
+#include <boost/thread/locks.hpp>
+#else
+#warning Disabling MT locks because Boost < 1.35!
+#endif
 
 using namespace boost::python;
 
@@ -44,7 +58,7 @@ struct PythonLoggingOutputDevice: public bob::core::OutputDevice {
      * @param callable A python callable object. Can be a function or an object
      * that implements the __call__() slot.
      */
-    PythonLoggingOutputDevice(object callable): 
+    PythonLoggingOutputDevice(object callable):
       m_callable(callable) { }
 
     /**
@@ -65,12 +79,16 @@ struct PythonLoggingOutputDevice: public bob::core::OutputDevice {
      * Writes a message to the callable.
      */
     virtual inline std::streamsize write(const char* s, std::streamsize n) {
+      static boost::mutex mutex;
+#if   ((BOOST_VERSION / 100) % 1000) > 35
+      boost::lock_guard<boost::mutex> lock(mutex);
+#endif
+      bob::python::gil gil;
       if (TPY_ISNONE(m_callable)) return 0;
       std::string value(s);
       if (std::isspace(value[n-1])) { //remove accidental newlines in the end
         value = value.substr(0, n-1);
       }
-      bob::python::gil lock;
       m_callable(value);
       return n;
     }
@@ -79,11 +97,70 @@ struct PythonLoggingOutputDevice: public bob::core::OutputDevice {
     object m_callable; ///< the callable we use to stream the data out.
 };
 
+struct message_info_t {
+  bob::core::OutputStream& s;
+  std::string message;
+  bool exit;
+};
+
+void* log_message_inner(void* cookie) {
+  unsigned int thread_id = (unsigned int)pthread_self();
+  if (PyEval_ThreadsInitialized()) {
+    printf("(0x%x) python threads initialized\n", thread_id);
+  }
+  else {
+    printf("(0x%x) python threads NOT initialized\n", thread_id);
+  }
+  message_info_t* mi = (message_info_t*)cookie;
+  mi->s << "\"" << mi->message << "\"" << std::endl;
+  if (mi->exit) {
+    printf("(0x%x) exiting thread\n", thread_id);
+    pthread_exit(0);
+  }
+  printf("(0x%x) returning 0\n", thread_id);
+  return 0;
+}
+
 /**
- * A test function for your python bindings 
+ * A test function for your python bindings
  */
-static void log_message(bob::core::OutputStream& s, const std::string& message) {
-  s << message << std::endl;
+static void log_message(bob::core::OutputStream& s, const char* message) {
+  bob::python::no_gil unlock;
+  unsigned int thread_id = (unsigned int)pthread_self();
+  message_info_t mi = {s, message, false};
+  log_message_inner((void*)&mi);
+  printf("(0x%x) returning to caller\n", thread_id);
+}
+
+/**
+ * Logs a number of messages from a separate thread
+ */
+static void log_message_mt(unsigned int nthreads, bob::core::OutputStream& s, const char* message) {
+  bob::python::no_gil unlock;
+  unsigned int thread_id = (unsigned int)pthread_self();
+
+  printf("(0x%x) writing message sample\n", thread_id);
+  message_info_t mi_sample = {s, message, false};
+  log_message_inner((void*)&mi_sample);
+  printf("(0x%x) sampled - now launching %u thread(s)\n", thread_id, nthreads);
+
+  boost::shared_array<pthread_t> threads(new pthread_t[nthreads]);
+  message_info_t mi = {s, message, true};
+  printf("(0x%x) launching %u thread(s)\n", thread_id, nthreads);
+
+  for (unsigned int i=0; i<nthreads; ++i) {
+    printf("(0x%x) launch thread %d: `%s'\n", thread_id, i, mi.message.c_str());
+    pthread_create(&threads[i], NULL, &log_message_inner, (void*)&mi);
+    printf("(0x%x) thread %d = 0x%x launched\n", thread_id, i, (unsigned int)threads[i]);
+  }
+
+  void* status;
+  printf("(0x%x) waiting %u thread(s)\n", thread_id, nthreads);
+  for (unsigned int i=0; i<nthreads; ++i) {
+    pthread_join(threads[i], &status);
+    printf("(0x%x) waiting on thread 0x%x\n", thread_id, (unsigned int)threads[i]);
+  }
+  printf("(0x%x) returning to caller\n", thread_id);
 }
 
 /**
@@ -94,7 +171,7 @@ static void outputstream_del(bob::core::OutputStream& os) {
 }
 
 void bind_core_logging() {
-  class_<bob::core::OutputDevice, boost::shared_ptr<bob::core::OutputDevice>, boost::noncopyable>("OutputDevice", "OutputDevices act like sinks for the messages emitted from within C++", no_init); 
+  class_<bob::core::OutputDevice, boost::shared_ptr<bob::core::OutputDevice>, boost::noncopyable>("OutputDevice", "OutputDevices act like sinks for the messages emitted from within C++", no_init);
 
   class_<PythonLoggingOutputDevice, boost::shared_ptr<PythonLoggingOutputDevice>, bases<bob::core::OutputDevice> >("PythonLoggingOutputDevice", "The PythonLoggingOutputDevice is the default logging class for bob.core.OutputStream objects to be used in python. It diverges the output of logged messages in C++ into the pythonic logging module.", init<object>("Initializes the PythonLoggingOutputDevice with a new callable that will be used to emit messages."))
     .def("__del__", &PythonLoggingOutputDevice::close, (arg("self")), "Resets this stream before calling the C++ destructor")
@@ -116,4 +193,37 @@ void bind_core_logging() {
   scope().attr("error") = object(ptr(&bob::core::error));
 
   //a test function
+  def("__log_message__", &log_message, (arg("stream"), arg("message")),
+      "Logs a message into Bob's logging system from C++.\n" \
+      "\n" \
+      "This method is included for testing purposes only and should not be considered part of the Python API for Bob.\n" \
+      "\n" \
+      "Keyword parameters:\n" \
+      "\n" \
+      "stream\n" \
+      "  The stream to use for logging the message. Choose from the streams available at this module.\n" \
+      "\n" \
+      "message\n" \
+      "  A string containing the message to be logged.\n" \
+      "\n"
+     );
+
+  //a test function
+  def("__log_message_mt__", &log_message_mt, (arg("nthreads"), arg("stream"), arg("message")),
+      "Logs a message into Bob's logging system from C++ in a separate thread.\n" \
+      "\n" \
+      "This method is included for testing purposes only and should not be considered part of the Python API for Bob.\n" \
+      "\n" \
+      "Keyword parameters:\n" \
+      "\n" \
+      "nthreads\n" \
+      "  The total number of threads from which to write messages to the logging system using the C++->Python API.\n" \
+      "\n" \
+      "stream\n" \
+      "  The stream to use for logging the message. Choose from the streams available at this module.\n" \
+      "\n" \
+      "message\n" \
+      "  A string containing the message to be logged.\n" \
+      "\n"
+     );
 }
