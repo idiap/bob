9f219a604785b59e8d405a83ada49be85c342a91
Andre Anjos <andre.anjos@idiap.ch>
2013-08-08 12:49:41 +0200

    [python3] Test first for boost_python presence

--- src/boost.cmake
+++ src/boost.cmake
@@ -6,6 +6,10 @@ include(FindBoost)
 # Compiles against mt versions
 set(Boost_USE_MULTITHREADED ON)
 
+# Specific python support, only if installed
+execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import sys; print('%d%d' % (sys.version_info[0], sys.version_info[1]))" OUTPUT_VARIABLE PY_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+find_package(Boost 1.40.0 QUIET COMPONENTS python-py${PY_VERSION})
+
 # Determine here the components you need so the system can verify
 find_package(Boost 1.40.0 REQUIRED
   COMPONENTS
@@ -20,10 +24,6 @@ find_package(Boost 1.40.0 REQUIRED
     system
     regex
   )
-
-# Specific python support, only if installed
-execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import sys; print('%d%d' % (sys.version_info[0], sys.version_info[1]))" OUTPUT_VARIABLE PY_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
-find_package(Boost 1.40.0 QUIET COMPONENTS python-py${PY_VERSION})
 
 # Renaming so all works automagically
 set(boost_INCLUDE_DIRS ${Boost_INCLUDE_DIRS} CACHE INTERNAL "incdirs")
