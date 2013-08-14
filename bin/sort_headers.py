a9f5965e4e0f5c65cd85b0ae302603e6e3e81365
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-08-14 15:05:27 +0200

    Add copyright to some missing files

--- bin/sort_headers.py
+++ bin/sort_headers.py
@@ -2,6 +2,20 @@
 # vim: set fileencoding=utf-8 :
 # Andre Anjos <andre.dos.anjos@gmail.com>
 # Tue  4 Sep 21:43:59 2012
+#
+# Copyright (C) 2011-2013 Idiap Research Institute, Martigny, Switzerland
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """Sort headers in the following order:
 
--- cmake/bob-config.cmake
+++ cmake/bob-config.cmake
@@ -2,6 +2,20 @@
 # Sat  1 Sep 22:04:04 2012 CEST
 # <patch> Flavio Tarsetti <Tarsetti.Flavio@gmail.com>
 # Mon  1 Jul 10:00:02 2013 CEST
+#
+# Copyright (C) 2011-2013 Idiap Research Institute, Martigny, Switzerland
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 # Support settings for external code built on Bob-cxx
 
