ea1bf35eab0a2f975e9f08f4213cdcb47f2877fe
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-08-14 15:05:27 +0200

    Add copyright to missing files

--- bin/apiversion.py
+++ bin/apiversion.py
@@ -2,6 +2,20 @@
 # vim: set fileencoding=utf-8 :
 # Andre Anjos <andre.dos.anjos@gmail.com>
 # Sat 29 Jun 10:03:11 2013 
+#
+# Copyright (C) 2011-2013 Idiap Research Institute, Martigny, Switzerland
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 if len(sys.argv) != 2:
--- bin/ctags.sh
+++ bin/ctags.sh
@@ -1,6 +1,20 @@
 #!/bin/bash 
 # Andre Anjos <andre.anjos@idiap.ch>
 # Thu 02 Aug 2012 11:52:36 CEST
+#
+# Copyright (C) 2011-2013 Idiap Research Institute, Martigny, Switzerland
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 install_dir=$(readlink -f $(dirname $(dirname $0)));
 files=$(find ${install_dir}/src ${install_dir}/python -name '*.h' -or -name '*.cc' -or -name '*.c' -or -name '*.cxx' -or -name '*.cpp' -or -name '*.C' -or -name '*.CC')
