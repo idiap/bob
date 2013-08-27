d725239b22c1d3ab3fcfa0f31b7f9084c31eca89
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-08-27 13:33:09 +0200

    IP scaling: Fix #161 on the 1.2 branch (also related to #139)

---
+++ python/bob/ip/test/test_scale.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
+# Tue Aug 27 12:40:18 CEST 2013
+#
+# Copyright (C) 2011-2013 Idiap Research Institute, Martigny, Switzerland
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Test the Scaling function
+"""
+
+import unittest
+import bob
+import numpy
+
+src = numpy.array([[  0,   2,   4,   6],
+                   [  2,   4,   8,  12],
+                   [  4,   8,  16,  24],
+                   [  8,  16,  32,  48]], dtype=numpy.uint8)
+
+# Reference values
+dst_ref_2by2 = numpy.array([[0, 6], [8, 48]], dtype=numpy.float64)
+dst_ref_8by8 = numpy.array([[  0.        ,  0.85714286,  1.71428571,  2.57142857,  3.42857143,  4.28571429,  5.14285714,  6.        ],
+                            [  0.85714286,  1.71428571,  2.57142857,  3.67346939,  4.89795918,  6.12244898,  7.34693878,  8.57142857],
+                            [  1.71428571,  2.57142857,  3.42857143,  4.7755102 ,  6.36734694,  7.95918367,  9.55102041, 11.14285714],
+                            [  2.57142857,  3.67346939,  4.7755102 ,  6.6122449 ,  8.81632653, 11.02040816, 13.2244898 , 15.42857143],
+                            [  3.42857143,  4.89795918,  6.36734694,  8.81632653, 11.75510204, 14.69387755, 17.63265306, 20.57142857],
+                            [  4.57142857,  6.53061224,  8.48979592, 11.75510204, 15.67346939, 19.59183673, 23.51020408, 27.42857143],
+                            [  6.28571429,  8.97959184, 11.67346939, 16.16326531, 21.55102041, 26.93877551, 32.32653061, 37.71428571],
+                            [  8.        , 11.42857143, 14.85714286, 20.57142857, 27.42857143, 34.28571429, 41.14285714, 48.        ]], dtype=numpy.float64)
+
+# Precision when checking correctness
+eps = 1e-7
+
+class ScaleTest(unittest.TestCase):
+  """Performs various tests for the scale function."""
+
+  def test01_scale_regular(self):
+    """Use scaling where both output and input are arguments"""
+    dst_2by2 = numpy.zeros(shape=(2,2), dtype=numpy.float)
+    bob.ip.scale(src, dst_2by2)
+    numpy.allclose(dst_2by2, dst_ref_2by2, atol=eps)
+
+    dst_8by8 = numpy.zeros(shape=(8,8), dtype=numpy.float)
+    bob.ip.scale(src, dst_8by8)
+    numpy.allclose(dst_8by8, dst_ref_8by8, atol=eps)
+
+  def test02_scale_factor(self):
+    """Use scaling where the output size is provided as a scaling factor"""
+    dst_2by2 = numpy.zeros(shape=(2,2), dtype=numpy.float)
+    dst_2by2 = bob.ip.scale_as(src, 0.5)
+    numpy.allclose(dst_2by2, dst_ref_2by2, atol=eps)
+
+    dst_8by8 = numpy.zeros(shape=(8,8), dtype=numpy.float)
+    dst_8by8 = bob.ip.scale_as(src, 2.)
+    numpy.allclose(dst_8by8, dst_ref_8by8, atol=eps)
+
--- src/ip/python/scale.cc
+++ src/ip/python/scale.cc
@@ -20,6 +20,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <boost/python.hpp>
 #include <bob/python/ndarray.h>
 #include <bob/ip/scale.h>
 
@@ -74,8 +75,9 @@ static void scale(bob::python::const_ndarray src, bob::python::ndarray dst,
 
 BOOST_PYTHON_FUNCTION_OVERLOADS(scale_overloads, scale, 2, 3) 
 
+
 template <typename T, int N>
-static void inner_scale2(bob::python::const_ndarray src, 
+static void inner_scale_mask(bob::python::const_ndarray src, 
   bob::python::const_ndarray smask, 
   bob::python::ndarray dst, bob::python::ndarray dmask, 
   bob::ip::Rescale::Algorithm algo) 
@@ -85,7 +87,7 @@ static void inner_scale2(bob::python::const_ndarray src,
   bob::ip::scale(src.bz<T,N>(), smask.bz<bool,N>(), dst_, dmask_, algo);
 }
 
-static void scale2(bob::python::const_ndarray src, 
+static void scale_mask(bob::python::const_ndarray src, 
   bob::python::const_ndarray smask,
   bob::python::ndarray dst, bob::python::ndarray dmask,
   bob::ip::Rescale::Algorithm algo=bob::ip::Rescale::BilinearInterp) 
@@ -98,11 +100,11 @@ static void scale2(bob::python::const_ndarray src,
       switch(info.dtype) 
       {
         case bob::core::array::t_uint8: 
-          return inner_scale2<uint8_t,2>(src, smask, dst, dmask, algo);
+          return inner_scale_mask<uint8_t,2>(src, smask, dst, dmask, algo);
         case bob::core::array::t_uint16:
-          return inner_scale2<uint16_t,2>(src, smask, dst, dmask, algo);
+          return inner_scale_mask<uint16_t,2>(src, smask, dst, dmask, algo);
         case bob::core::array::t_float64:
-          return inner_scale2<double,2>(src, smask, dst, dmask, algo);
+          return inner_scale_mask<double,2>(src, smask, dst, dmask, algo);
         default:
           PYTHON_ERROR(TypeError, "bob.ip.scale() does not support array of type '%s'.", info.str().c_str());
       }
@@ -111,11 +113,11 @@ static void scale2(bob::python::const_ndarray src,
       switch(info.dtype) 
       {
         case bob::core::array::t_uint8: 
-          return inner_scale2<uint8_t,3>(src, smask, dst, dmask, algo);
+          return inner_scale_mask<uint8_t,3>(src, smask, dst, dmask, algo);
         case bob::core::array::t_uint16:
-          return inner_scale2<uint16_t,3>(src, smask, dst, dmask, algo);
+          return inner_scale_mask<uint16_t,3>(src, smask, dst, dmask, algo);
         case bob::core::array::t_float64:
-          return inner_scale2<double,3>(src, smask, dst, dmask, algo);
+          return inner_scale_mask<double,3>(src, smask, dst, dmask, algo);
         default:
           PYTHON_ERROR(TypeError, "bob.ip.scale() does not support array of type '%s'.", info.str().c_str());
       }
@@ -125,10 +127,27 @@ static void scale2(bob::python::const_ndarray src,
   }
 }
 
-template <typename T, int N>
-static object inner_scale_as(bob::python::const_ndarray src, const double f) 
+BOOST_PYTHON_FUNCTION_OVERLOADS(scale_mask_overloads, scale_mask, 4, 5)
+
+
+template <typename T>
+static object inner_scale_as_2d(bob::python::const_ndarray src, const double f) 
 {
-  return object(bob::ip::scaleAs(src.bz<T,N>(), f)); //copying!
+  blitz::Array<T,2> dstT = bob::ip::scaleAs(src.bz<T,2>(), f);
+  bob::python::ndarray dst(bob::core::array::t_float64, dstT.extent(0), dstT.extent(1));
+  blitz::Array<double,2> dst_ = dst.bz<double,2>();
+  bob::ip::scale(src.bz<T,2>(), dst_);
+  return dst.self();
+}
+
+template <typename T>
+static object inner_scale_as_3d(bob::python::const_ndarray src, const double f) 
+{
+  blitz::Array<T,3> dstT = bob::ip::scaleAs(src.bz<T,3>(), f);
+  bob::python::ndarray dst(bob::core::array::t_float64, dstT.extent(0), dstT.extent(1), dstT.extent(2));
+  blitz::Array<double,3> dst_ = dst.bz<double,3>();
+  bob::ip::scale(src.bz<T,3>(), dst_);
+  return dst.self();
 }
 
 template <typename T>
@@ -137,10 +156,10 @@ static object inner_scale_as_dim(bob::python::const_ndarray src, const double f)
   const bob::core::array::typeinfo& info = src.type();
   switch (info.nd) 
   {
-    case 2: return inner_scale_as<T,2>(src, f);
-    case 3: return inner_scale_as<T,3>(src, f);
+    case 2: return inner_scale_as_2d<T>(src, f);
+    case 3: return inner_scale_as_3d<T>(src, f);
     default:
-      PYTHON_ERROR(TypeError, "bob.ip.scale() does not support array with " SIZE_T_FMT " dimensions.", info.nd);
+      PYTHON_ERROR(TypeError, "bob.ip.scale_as() does not support array with " SIZE_T_FMT " dimensions.", info.nd);
   }
 }
 
@@ -155,11 +174,10 @@ static object scale_as(bob::python::const_ndarray src, double f) {
     case bob::core::array::t_float64:
       return inner_scale_as_dim<double>(src, f);
     default:
-      PYTHON_ERROR(TypeError, "bob.ip.scale() does not support array of type '%s'.", info.str().c_str());
+      PYTHON_ERROR(TypeError, "bob.ip.scale_as() does not support array of type '%s'.", info.str().c_str());
   }
 }
 
-BOOST_PYTHON_FUNCTION_OVERLOADS(scale2_overloads, scale2, 4, 5)
 
 void bind_ip_scale() 
 {
@@ -168,7 +186,7 @@ void bind_ip_scale()
     .value("BilinearInterp", bob::ip::Rescale::BilinearInterp)
     ;
 
-  def("scale", &scale, scale_overloads((arg("src"), arg("dst"), arg("algorithm")=bob::ip::Rescale::BilinearInterp), "Scales a 2D array/image to the dimensions given by the destination array."));
-  def("scale", &scale2, scale2_overloads((arg("src"), arg("src_mask"), arg("dst"), arg("dst_mask"), arg("algorithm")=bob::ip::Rescale::BilinearInterp), "Scales a 2D array/image to the dimensions given by the destination array, taking boolean mask into account."));
-  def("scale_as", &scale_as, (arg("original"), arg("scale_factor")), "Gives back a scaled version of the original 2D or 3D array/image.");
+  def("scale", &scale, scale_overloads((arg("src"), arg("dst"), arg("algorithm")=bob::ip::Rescale::BilinearInterp), "Scales an image to the dimensions given by the allocated destination image. This function supports 2D and 3D input array/image (NumPy array) of type numpy.uint8, numpy.uint16 and numpy.float64. The output image must be a 2D or 3D array/image (NumPy array) of type numpy.float64."));
+  def("scale", &scale_mask, scale_mask_overloads((arg("src"), arg("src_mask"), arg("dst"), arg("dst_mask"), arg("algorithm")=bob::ip::Rescale::BilinearInterp), "Scales an imageto the dimensions given by the destination array, taking boolean mask into account. This function supports 2D and 3D input array/image (NumPy array) of type numpy.uint8, numpy.uint16 and numpy.float64. The output image must be a 2D or 3D array/image (NumPy array) of type numpy.float64 and the output mask should be a boolean NumPy array of the same dimensions as the output image."));
+  def("scale_as", &scale_as, (arg("src"), arg("scaling_factor")), "Scales an image according to the provided scaling factor. This function supports 2D and 3D input array/image (NumPy array) of type numpy.uint8, numpy.uint16 and numpy.float64. This will allocate and return a scaled 2D or 3D array/image of type numpy.float64. The scaling technique used is bilinear interpolation.");
 }
