fd2037c2e96191f27bf69109f401ff26a46a4ee7
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-11-14 23:52:57 +0100

    Add kiss FFT based FFT/DCT (python bindings) #

--- src/sp/python/main.cc
+++ src/sp/python/main.cc
@@ -25,7 +25,9 @@
 void bind_sp_version();
 void bind_sp_extrapolate();
 void bind_sp_dct();
+void bind_sp_dct_kiss();
 void bind_sp_fft();
+void bind_sp_fft_kiss();
 void bind_sp_convolution();
 void bind_sp_convolution();
 void bind_sp_quantization();
@@ -37,7 +39,9 @@ BOOST_PYTHON_MODULE(_sp) {
   bind_sp_version();
   bind_sp_extrapolate();
   bind_sp_dct();
+  bind_sp_dct_kiss();
   bind_sp_fft();
+  bind_sp_fft_kiss();
   bind_sp_convolution();
   bind_sp_quantization();
 }
