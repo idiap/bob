e89414eedd8a7a2501606103509ba2d32c0808ce
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-11-14 23:51:43 +0100

    Add kiss FFT based FFT/DCT

---
+++ src/sp/cxx/DCT2DKiss.cc
@@ -0,0 +1,220 @@
+/**
+ * @file sp/cxx/DCT2DKiss.cc
+ * @date Thu Nov 14 22:59:47 CET 2013
+ * @author Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
+ *
+ * @brief Implement a 2D Discrete Cosine Transform using
+ * a 1D DCT implementation.
+ *
+ * Copyright (C) 2011-2013 Idiap Research Institute, Martigny, Switzerland
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <bob/sp/DCT2DKiss.h>
+#include <bob/core/assert.h>
+
+bob::sp::DCT2DKissAbstract::DCT2DKissAbstract(
+    const size_t height, const size_t width):
+  m_height(height), m_width(width),
+  m_buffer_hw(height, width), m_buffer_h(height),
+  m_buffer_h2(height)
+{
+}
+
+bob::sp::DCT2DKissAbstract::DCT2DKissAbstract(
+    const bob::sp::DCT2DKissAbstract& other):
+  m_height(other.m_height), m_width(other.m_width),
+  m_buffer_hw(other.m_height, other.m_width), m_buffer_h(other.m_height),
+  m_buffer_h2(other.m_height)
+{
+}
+
+bob::sp::DCT2DKissAbstract::~DCT2DKissAbstract()
+{
+}
+
+bob::sp::DCT2DKissAbstract& 
+bob::sp::DCT2DKissAbstract::operator=(const DCT2DKissAbstract& other)
+{
+  if (this != &other) {
+    setHeight(other.m_height);
+    setWidth(other.m_width);
+    m_buffer_hw.resize(other.m_height, other.m_width);
+    m_buffer_h.resize(other.m_height);
+    m_buffer_h2.resize(other.m_height);
+  }
+  return *this;
+}
+
+bool bob::sp::DCT2DKissAbstract::operator==(const bob::sp::DCT2DKissAbstract& b) const
+{
+  return (this->m_height == b.m_height && this->m_width == b.m_width);
+}
+
+bool bob::sp::DCT2DKissAbstract::operator!=(const bob::sp::DCT2DKissAbstract& b) const
+{
+  return !(this->operator==(b));
+}
+
+void bob::sp::DCT2DKissAbstract::operator()(const blitz::Array<double,2>& src, 
+  blitz::Array<double,2>& dst) const
+{
+  // Check input, inclusive dimension
+  bob::core::array::assertZeroBase(src);
+  const blitz::TinyVector<int,2> shape(m_height,m_width);
+  bob::core::array::assertSameShape(src, shape);
+
+  // Check output
+  bob::core::array::assertCZeroBaseContiguous(dst);
+  bob::core::array::assertSameShape(dst, src);
+
+  // Process
+  processNoCheck(src, dst);
+}
+
+void bob::sp::DCT2DKissAbstract::setHeight(const size_t height)
+{
+  m_height = height;
+  m_buffer_hw.resize(m_height, m_width);
+  m_buffer_h.resize(m_height);
+  m_buffer_h2.resize(m_height);
+}
+
+void bob::sp::DCT2DKissAbstract::setWidth(const size_t width)
+{
+  m_width = width;
+  m_buffer_hw.resize(m_height, m_width);
+  m_buffer_h.resize(m_height);
+  m_buffer_h2.resize(m_height);
+}
+ 
+
+bob::sp::DCT2DKiss::DCT2DKiss(const size_t height, const size_t width):
+  bob::sp::DCT2DKissAbstract::DCT2DKissAbstract(height, width),
+  m_dct_h(height),
+  m_dct_w(width)
+{
+}
+
+bob::sp::DCT2DKiss::DCT2DKiss(const bob::sp::DCT2DKiss& other):
+  bob::sp::DCT2DKissAbstract(other),
+  m_dct_h(other.m_height),
+  m_dct_w(other.m_width)
+{
+}
+
+bob::sp::DCT2DKiss::~DCT2DKiss()
+{
+}
+
+bob::sp::DCT2DKiss& 
+bob::sp::DCT2DKiss::operator=(const DCT2DKiss& other)
+{
+  if (this != &other) {
+    bob::sp::DCT2DKissAbstract::operator=(other);
+    m_dct_h.setLength(other.m_height);
+    m_dct_w.setLength(other.m_width);
+  }
+  return *this;
+}
+
+void bob::sp::DCT2DKiss::setHeight(const size_t height)
+{
+  bob::sp::DCT2DKissAbstract::setHeight(height);
+  m_dct_h.setLength(height);
+}
+
+void bob::sp::DCT2DKiss::setWidth(const size_t width)
+{
+  bob::sp::DCT2DKissAbstract::setWidth(width);
+  m_dct_w.setLength(width);
+}
+ 
+void bob::sp::DCT2DKiss::processNoCheck(const blitz::Array<double,2>& src, 
+  blitz::Array<double,2>& dst) const
+{
+  blitz::Range rall = blitz::Range::all();
+  // Compute the DCT
+  for (int i=0; i<(int)m_height; ++i) {
+    const blitz::Array<double,1> srci = src(i, rall);
+    blitz::Array<double,1> bufi = m_buffer_hw(i, rall);
+    m_dct_w(srci, bufi);
+  }
+  for (int j=0; j<(int)m_width; ++j) {
+    m_buffer_h = m_buffer_hw(rall, j);
+    m_dct_h(m_buffer_h, m_buffer_h2);
+    blitz::Array<double,1> dstj = dst(rall, j);
+    dstj = m_buffer_h2;
+  }
+}
+
+
+bob::sp::IDCT2DKiss::IDCT2DKiss(const size_t height, const size_t width):
+  bob::sp::DCT2DKissAbstract::DCT2DKissAbstract(height, width),
+  m_idct_h(height),
+  m_idct_w(width)
+{
+}
+
+bob::sp::IDCT2DKiss::IDCT2DKiss(const bob::sp::IDCT2DKiss& other):
+  bob::sp::DCT2DKissAbstract(other),
+  m_idct_h(other.m_height),
+  m_idct_w(other.m_width)
+{
+}
+
+bob::sp::IDCT2DKiss::~IDCT2DKiss()
+{
+}
+
+bob::sp::IDCT2DKiss& 
+bob::sp::IDCT2DKiss::operator=(const IDCT2DKiss& other)
+{
+  if (this != &other) {
+    bob::sp::DCT2DKissAbstract::operator=(other);
+    m_idct_h.setLength(other.m_height);
+    m_idct_w.setLength(other.m_width);
+  }
+  return *this;
+}
+
+void bob::sp::IDCT2DKiss::setHeight(const size_t height)
+{
+  bob::sp::DCT2DKissAbstract::setHeight(height);
+  m_idct_h.setLength(height);
+}
+
+void bob::sp::IDCT2DKiss::setWidth(const size_t width)
+{
+  bob::sp::DCT2DKissAbstract::setWidth(width);
+  m_idct_w.setLength(width);
+}
+
+void bob::sp::IDCT2DKiss::processNoCheck(const blitz::Array<double,2>& src, 
+  blitz::Array<double,2>& dst) const
+{
+  blitz::Range rall = blitz::Range::all();
+  // Compute the DCT
+  for (int i=0; i<(int)m_height; ++i) {
+    const blitz::Array<double,1> srci = src(i, rall);
+    blitz::Array<double,1> bufi = m_buffer_hw(i, rall);
+    m_idct_w(srci, bufi);
+  }
+  for (int j=0; j<(int)m_width; ++j) {
+    m_buffer_h = m_buffer_hw(rall, j);
+    m_idct_h(m_buffer_h, m_buffer_h2);
+    blitz::Array<double,1> dstj = dst(rall, j);
+    dstj = m_buffer_h2;
+  }
+}
---
+++ src/sp/cxx/FFT1DKiss.cc
@@ -0,0 +1,160 @@
+/**
+ * @file sp/cxx/FFT1DKiss.cc
+ * @date Wed Apr 13 23:08:13 2011 +0200
+ * @author Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
+ *
+ * @brief Implement a naive 1D Fast Fourier Transform
+ *
+ * Copyright (C) 2011-2013 Idiap Research Institute, Martigny, Switzerland
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <bob/sp/FFT1DKiss.h>
+#include <bob/core/assert.h>
+
+bob::sp::FFT1DKissAbstract::FFT1DKissAbstract(const size_t length):
+  m_length(length)
+{
+}
+
+bob::sp::FFT1DKissAbstract::FFT1DKissAbstract(
+    const bob::sp::FFT1DKissAbstract& other):
+  m_length(other.m_length)
+{
+}
+
+bob::sp::FFT1DKissAbstract::~FFT1DKissAbstract()
+{
+}
+
+bob::sp::FFT1DKissAbstract& 
+bob::sp::FFT1DKissAbstract::operator=(const FFT1DKissAbstract& other)
+{
+  if (this != &other) {
+    m_length = other.m_length;
+  }
+  return *this;
+}
+
+bool bob::sp::FFT1DKissAbstract::operator==(const bob::sp::FFT1DKissAbstract& b) const
+{
+  return (this->m_length == b.m_length);
+}
+
+bool bob::sp::FFT1DKissAbstract::operator!=(const bob::sp::FFT1DKissAbstract& b) const
+{
+  return !(this->operator==(b));
+}
+
+void bob::sp::FFT1DKissAbstract::operator()(const blitz::Array<std::complex<double>,1>& src, 
+  blitz::Array<std::complex<double>,1>& dst) const
+{
+  // Check input, inclusive dimension
+  bob::core::array::assertCZeroBaseContiguous(src);
+  const blitz::TinyVector<int,1> shape(m_length);
+  bob::core::array::assertSameShape(src, shape);
+
+  // Check output
+  bob::core::array::assertCZeroBaseContiguous(dst);
+  bob::core::array::assertSameShape( dst, src);
+
+  // Process
+  processNoCheck(src, dst);
+}
+
+void bob::sp::FFT1DKissAbstract::setLength(const size_t length)
+{
+  m_length = length;
+}
+  
+
+bob::sp::FFT1DKiss::FFT1DKiss(const size_t length):
+  bob::sp::FFT1DKissAbstract(length),
+  m_kissfft(new kissfft<double>(length, false))
+{
+}
+
+bob::sp::FFT1DKiss::FFT1DKiss(const bob::sp::FFT1DKiss& other):
+  bob::sp::FFT1DKissAbstract(other),
+  m_kissfft(new kissfft<double>(other.m_length, false))
+{
+}
+
+bob::sp::FFT1DKiss::~FFT1DKiss()
+{
+}
+
+bob::sp::FFT1DKiss& 
+bob::sp::FFT1DKiss::operator=(const FFT1DKiss& other)
+{
+  if (this != &other) {
+    bob::sp::FFT1DKissAbstract::operator=(other);
+    m_kissfft.reset(new kissfft<double>(other.m_length, false));
+  }
+  return *this;
+}
+
+void bob::sp::FFT1DKiss::setLength(const size_t length)
+{
+  bob::sp::FFT1DKissAbstract::setLength(length);
+  m_kissfft.reset(new kissfft<double>(length, false));
+}
+  
+void bob::sp::FFT1DKiss::processNoCheck(const blitz::Array<std::complex<double>,1>& src, 
+  blitz::Array<std::complex<double>,1>& dst) const
+{
+  // Compute the FFT
+  m_kissfft->transform(src.data(), dst.data());
+}
+
+
+bob::sp::IFFT1DKiss::IFFT1DKiss(const size_t length):
+  bob::sp::FFT1DKissAbstract(length),
+  m_kissfft(new kissfft<double>(length, true))
+{
+}
+
+bob::sp::IFFT1DKiss::IFFT1DKiss(const bob::sp::IFFT1DKiss& other):
+  bob::sp::FFT1DKissAbstract(other),
+  m_kissfft(new kissfft<double>(other.m_length, true))
+{
+}
+
+bob::sp::IFFT1DKiss::~IFFT1DKiss()
+{
+}
+
+bob::sp::IFFT1DKiss& 
+bob::sp::IFFT1DKiss::operator=(const IFFT1DKiss& other)
+{
+  if (this != &other) {
+    bob::sp::FFT1DKissAbstract::operator=(other);
+    m_kissfft.reset(new kissfft<double>(other.m_length, true));
+  }
+  return *this;
+}
+
+
+void bob::sp::IFFT1DKiss::setLength(const size_t length)
+{
+  bob::sp::FFT1DKissAbstract::setLength(length);
+  m_kissfft.reset(new kissfft<double>(length, true));
+}
+  
+void bob::sp::IFFT1DKiss::processNoCheck(const blitz::Array<std::complex<double>,1>& src, 
+  blitz::Array<std::complex<double>,1>& dst) const
+{
+  m_kissfft->transform(src.data(), dst.data());
+  dst /= (double)m_length;
+}
