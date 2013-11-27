a2973e0b97e32e01c2c23b0581f32ce343874c1d
Manuel Guenther <manuel.guenther@idiap.ch>
2013-11-27 19:00:18 +0100

    Implemented integral square image.

--- include/bob/ip/integral.h
+++ include/bob/ip/integral.h
@@ -26,13 +26,13 @@ namespace bob {
 
     namespace detail {
       /**
-        * @brief Function which computes the integral image of a 2D 
+        * @brief Function which computes the integral image of a 2D
         *   blitz::array/image of a given type.
         *   The first dimension is the height (y-axis), whereas the second
         *   one is the width (x-axis).
         * @warning No check is performed wrt. the array dimensions.
-        * @param src The input blitz array
-        * @param dst The output blitz array
+        * @param src The input image
+        * @param dst The output integral image
         */
       template<typename T, typename U>
       void integralNoCheck(const blitz::Array<T,2>& src,
@@ -47,7 +47,7 @@ namespace bob {
         {
           dst(y,0) = dst(y-1,0) + bob::core::cast<U>(src(y,0));
           U row_sum_cur = src(y,0);
-          for(int x=1; x<src.extent(1); ++x) 
+          for(int x=1; x<src.extent(1); ++x)
           {
             row_sum_cur += bob::core::cast<U>(src(y,x));
             dst(y,x) = dst(y-1,x) + row_sum_cur;
@@ -58,16 +58,16 @@ namespace bob {
 
 
     /**
-      * @brief Function which computes the integral image of a 2D 
+      * @brief Function which computes the integral image of a 2D
       *   blitz::array/image of a given type.
       *   The first dimension is the height (y-axis), whereas the second
       *   one is the width (x-axis).
       * @warning It is the user responsability to select a suitable type
-      *   for the destination array. Using a type with the same range of 
+      *   for the destination array. Using a type with the same range of
       *   value might cause out of range problems.
-      * @param src The input blitz array
-      * @param dst The output blitz array
-      * @param addZeroBorder This requires the dst array to be 1 pixel 
+      * @param src The input image
+      * @param dst The output integral image
+      * @param addZeroBorder This requires the dst array to be 1 pixel
       *   larger in each dimension. Besides, an extra zero pixel will be
       *   added at the beginning of each row and column
       */
@@ -94,12 +94,108 @@ namespace bob {
           dst(y,0) = 0;
         for(int x=1; x<dst.extent(1); ++x)
           dst(0,x) = 0;
-        blitz::Array<U,2> dst_c = 
+        blitz::Array<U,2> dst_c =
           dst(blitz::Range(1,src.extent(0)), blitz::Range(1,src.extent(1)));
         detail::integralNoCheck(src, dst_c);
       }
       else
         detail::integralNoCheck(src, dst);
+    }
+
+
+    namespace detail {
+      /**
+        * @brief Function which computes the integral image and the
+        *   integral square image of a 2D blitz::array/image of a given type.
+        * @warning No check is performed wrt. the array dimensions.
+        * @param src The input image
+        * @param dst The output integral image
+        * @param sqr The output integral square image
+        */
+      template<typename T, typename U>
+      void integralNoCheck(const blitz::Array<T,2>& src,
+        blitz::Array<U,2>& dst, blitz::Array<U,2>& sqr)
+      {
+        const U v = bob::core::cast<U>(src(0,0));
+        dst(0,0) = v;
+        sqr(0,0) = v*v;
+
+        // Compute first row
+        for(int x=1; x<src.extent(1); ++x){
+          const U v = bob::core::cast<U>(src(0,x));
+          dst(0,x) = dst(0,x-1) + v;
+          sqr(0,x) = sqr(0,x-1) + v*v;
+        }
+
+        // Compute remaining part
+        for(int y=1; y<src.extent(0); ++y)
+        {
+          U row_sum_cur = bob::core::cast<U>(src(y,0));
+          U row_sum_sqr = row_sum_cur * row_sum_cur;
+
+          dst(y,0) = dst(y-1,0) + row_sum_cur;
+          sqr(y,0) = sqr(y-1,0) + row_sum_sqr;
+
+          for(int x=1; x<src.extent(1); ++x)
+          {
+            const U v = bob::core::cast<U>(src(y,x));
+            row_sum_cur += v;
+            row_sum_sqr += v*v;
+
+            dst(y,x) = dst(y-1,x) + row_sum_cur;
+            sqr(y,x) = sqr(y-1,x) + row_sum_sqr;
+          }
+        }
+      }
+    }
+
+    /**
+      * @brief Function which computes the integral image and the
+      *   integral square image of a 2D blitz::array/image of a given type.
+      * @warning It is the user responsibility to select a suitable type
+      *   for the destination array. Using a type with the same range of
+      *   value might cause out of range problems.
+      * @param src The input image
+      * @param dst The output integral image
+      * @param sqr The output integral square image
+      * @param addZeroBorder This requires the dst and sqr arrays to be 1
+      *   pixel larger in each dimension. Besides, an extra zero pixel will
+      *   be added at the beginning of each row and column
+      */
+    template<typename T, typename U>
+    void integral(const blitz::Array<T,2>& src, blitz::Array<U,2>& dst,
+      blitz::Array<U,2>& sqr, const bool addZeroBorder=false)
+    {
+      // Checks that the src/dst arrays have zero base indices
+      bob::core::array::assertZeroBase(src);
+      bob::core::array::assertZeroBase(dst);
+      bob::core::array::assertZeroBase(sqr);
+      if(addZeroBorder)
+      {
+        blitz::TinyVector<int,2> shape = src.shape();
+        shape += 1;
+        bob::core::array::assertSameShape(dst,shape);
+        bob::core::array::assertSameShape(sqr,shape);
+      }
+      else{
+        bob::core::array::assertSameShape(src,dst);
+        bob::core::array::assertSameShape(src,sqr);
+      }
+      // Compute the integral image of the 2D array
+      if(addZeroBorder)
+      {
+        for(int y=0; y<dst.extent(0); ++y)
+          dst(y,0) = sqr(y,0) = 0;
+        for(int x=1; x<dst.extent(1); ++x)
+          dst(0,x) = sqr(0,x) = 0;
+        blitz::Array<U,2> dst_c =
+          dst(blitz::Range(1,src.extent(0)), blitz::Range(1,src.extent(1)));
+        blitz::Array<U,2> sqr_c =
+          sqr(blitz::Range(1,src.extent(0)), blitz::Range(1,src.extent(1)));
+        detail::integralNoCheck(src, dst_c, sqr_c);
+      }
+      else
+        detail::integralNoCheck(src, dst, sqr);
     }
 
   }
--- src/ip/python/integral.cc
+++ src/ip/python/integral.cc
@@ -40,7 +40,7 @@ static void integral2 (bob::python::const_ndarray src, bob::python::ndarray dst,
 
 }
 
-static void integral (bob::python::const_ndarray src, bob::python::ndarray dst, bool b=false) {
+static void integral (bob::python::const_ndarray src, bob::python::ndarray dst, bool b) {
   const bob::core::array::typeinfo& info = src.type();
 
   switch (info.dtype) {
@@ -53,8 +53,49 @@ static void integral (bob::python::const_ndarray src, bob::python::ndarray dst, 
 
 }
 
-BOOST_PYTHON_FUNCTION_OVERLOADS(integral_overloads, integral, 2, 3)
+
+template <typename T, typename U, int N>
+static void inner_integral_square (bob::python::const_ndarray src, bob::python::ndarray dst, bob::python::ndarray sqr, bool b) {
+  blitz::Array<U,N> dst_ = dst.bz<U,N>();
+  bob::ip::integral(src.bz<T,N>(), dst_, b);
+}
+
+template <typename T, int N>
+static void integral2_square (bob::python::const_ndarray src, bob::python::ndarray dst, bob::python::ndarray sqr, bool b) {
+  const bob::core::array::typeinfo& info = dst.type();
+
+  switch (info.dtype) {
+    case bob::core::array::t_int8: return inner_integral_square<T,int8_t,N>(src, dst, sqr, b);
+    case bob::core::array::t_int16: return inner_integral_square<T,int16_t,N>(src, dst, sqr, b);
+    case bob::core::array::t_int32: return inner_integral_square<T,int32_t,N>(src, dst, sqr, b);
+    case bob::core::array::t_int64: return inner_integral_square<T,int64_t,N>(src, dst, sqr, b);
+    case bob::core::array::t_uint8: return inner_integral_square<T,uint8_t,N>(src, dst, sqr, b);
+    case bob::core::array::t_uint16: return inner_integral_square<T,uint16_t,N>(src, dst, sqr, b);
+    case bob::core::array::t_uint32: return inner_integral_square<T,uint32_t,N>(src, dst, sqr, b);
+    case bob::core::array::t_uint64: return inner_integral_square<T,uint64_t,N>(src, dst, sqr, b);
+    case bob::core::array::t_float32: return inner_integral_square<T,float,N>(src, dst, sqr, b);
+    case bob::core::array::t_float64: return inner_integral_square<T,double,N>(src, dst, sqr, b);
+    default:
+      PYTHON_ERROR(TypeError, "integral image operator does not support output type '%s'", info.str().c_str());
+  }
+
+}
+
+static void integral_square (bob::python::const_ndarray src, bob::python::ndarray dst, bob::python::ndarray sqr, bool b) {
+  const bob::core::array::typeinfo& info = src.type();
+
+  switch (info.dtype) {
+    case bob::core::array::t_uint8: return integral2_square<uint8_t,2>(src, dst, sqr, b);
+    case bob::core::array::t_uint16: return integral2_square<uint16_t,2>(src, dst, sqr, b);
+    case bob::core::array::t_float64: return integral2_square<double,2>(src, dst, sqr, b);
+    default:
+      PYTHON_ERROR(TypeError, "integral image operator does not support input type '%s'", info.str().c_str());
+  }
+
+}
+
 
 void bind_ip_integral() {
-  def(BOOST_PP_STRINGIZE(integral), &integral, integral_overloads((arg("src"), arg("dst"), arg("add_zero_border")=false), "Compute the integral image of a 2D blitz array (image). It is the responsibility of the user to select an appropriate type for the numpy array which will contain the integral image. By default, src and dst should have the same size. If add_zero_border is set to true, then dst should be one pixel larger than src in each dimension.")); 
+  def("integral", &integral, (arg("src"), arg("dst"), arg("add_zero_border")=false), "Compute the integral image of a 2D blitz array (image). It is the responsibility of the user to select an appropriate type for the numpy array which will contain the integral image. By default, src and dst should have the same size. If add_zero_border is set to true, then dst should be one pixel larger than src in each dimension.");
+  def("integral", &integral_square, (arg("src"), arg("dst"), arg("sqr"), arg("add_zero_border")=false), "Compute the integral image and the integral square image of a 2D blitz array (image). It is the responsibility of the user to select an appropriate type for the numpy array which will contain the integral image and the integral square image. By default, src, dst and sqr should have the same size. If add_zero_border is set to true, then dst and sqr should be one pixel larger than src in each dimension.");
 }
