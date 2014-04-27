4e02c534bd9397d1d1c5e6c743b606fffddbeed5
Andre Anjos <andre.dos.anjos@gmail.com>
2014-04-27 19:20:32 +0200

    Try to make a more generic ffmpeg version detector that can work with avconv

--- bin/ffmpeg-version.sh
+++ bin/ffmpeg-version.sh
@@ -1,9 +1,9 @@
-#!/bin/bash 
+#!/bin/bash
 # Andre Anjos <andre.anjos@idiap.ch>
-# Tue 29 Nov 2011 13:36:43 CET
+# Sun 27 Apr 19:19:41 2014 CEST
 #
-# Copyright (C) 2011-2013 Idiap Research Institute, Martigny, Switzerland
+# Copyright (C) 2011-2014 Idiap Research Institute, Martigny, Switzerland
 
 export LD_LIBRARY_PATH=$2;
-string=`$1 -version 2>&1 | grep -i 'ffmpeg version'`;
+string=`$1 -version 2>&1 | grep -i "$1 version"`;
 python -c "print('${string}'.split(' ')[2].strip(','))"
