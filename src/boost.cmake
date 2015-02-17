d8cd31d4d8bccff09e075b2c8956e3f7b1497d0b
Manuel Guenther <manuel.guenther@idiap.ch>
2015-02-17 14:29:51 +0100

    Boost is now also found in the CMAKE_PREFIX_PATH

--- src/boost.cmake
+++ src/boost.cmake
@@ -6,9 +6,21 @@ include(FindBoost)
 # Compiles against mt versions
 set(Boost_USE_MULTITHREADED ON)
 
+# first try to find boost ONLY on the CMAKE_PREFIX_PATH
+if (CMAKE_PREFIX_PATH)
+  set(Boost_NO_SYSTEM_PATHS On)
+endif()
+
 # Specific python support, only if installed
 execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import sys; print('%d%d' % (sys.version_info[0], sys.version_info[1]))" OUTPUT_VARIABLE PY_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
 find_package(Boost 1.40.0 QUIET COMPONENTS python-py${PY_VERSION})
+
+# if the CMAKE_PREFIX_PATH does not contain boost, try to find it somewhere else
+if (NOT Found_BOOST)
+  set(Boost_NO_SYSTEM_PATHS Off)
+  find_package(Boost 1.40.0 QUIET COMPONENTS python-py${PY_VERSION})
+endif()
+
 
 # Determine here the components you need so the system can verify
 find_package(Boost 1.40.0 REQUIRED
