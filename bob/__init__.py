1b6c079d0816a59c47702ec0f4030197b8b6a4eb
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2015-07-24 22:49:31 +0200

    Attempt to fix travis

--- bob/__init__.py
+++ bob/__init__.py
@@ -1,2 +1,9 @@
 #see http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
 __import__('pkg_resources').declare_namespace(__name__)
+
+
+def get_config():
+  """Returns a string containing the configuration information.
+  """
+  import bob.extension
+  return bob.extension.get_config(__name__)
