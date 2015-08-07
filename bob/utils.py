2bc14968143d16560fb8c832fc33f20d864afca3
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2015-08-07 16:35:35 +0200

    Fixed the documentation

--- bob/utils.py
+++ bob/utils.py
@@ -28,16 +28,16 @@ def download(url, output_dir="."):
   
   """
 
-  import urllib2
+  import six
   import os
   
   file_name = url.split('/')[-1] #Getting only the file name without the version
   file_name = os.path.join(output_dir,file_name)
-  u = urllib2.urlopen(url)
+  u = six.moves.urllib.request.urlopen(url)
   f = open(file_name, 'wb')
   meta = u.info()
-  file_size = int(meta.getheaders("Content-Length")[0])
-  print "Downloading: %s Bytes: %s" % (file_name, file_size)
+  file_size = int(meta.get("Content-Length")[0])
+  print ("Downloading: %s Bytes: %s" % (file_name, file_size))
 
   file_size_dl = 0
   block_sz = 8192
@@ -50,7 +50,7 @@ def download(url, output_dir="."):
     f.write(buffer)
     status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
     status = status + chr(8)*(len(status)+1)
-    print status,
+    print (status)
 
   f.close()
   return file_name
@@ -75,11 +75,11 @@ def download_packages(requirements,  output_dir="./temp"):
   bob.io.base.create_directories_safe(output_dir)
 
   urls = []
-  print "Fetching urls!!!"
+  print ("Fetching urls!!!")
   for r in requirements:
     try:
       package_name = r.split("=")[0]
-      print "  Fetching {0}".format(package_name)      
+      print ("  Fetching {0}".format(package_name))
       urls.append(get_url(package_name))
     except HTTPError as exc:
       # url request failed with a something else than 404 Error
@@ -88,7 +88,7 @@ def download_packages(requirements,  output_dir="./temp"):
   for u in urls:
     file_name = download(u, output_dir)
 
-    print "Unziping {0}".format(u)    
+    print ("Unziping {0}".format(u)    )
     f = open(file_name, 'rb')
     z = zipfile.ZipFile(f)
     z.extractall(os.path.dirname(file_name))
